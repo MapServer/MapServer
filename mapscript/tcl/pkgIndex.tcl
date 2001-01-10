a7a7f03ea81f9b58a7b48a71f3c99b23533a217c
Tom Poindexter <tpoindex@nyx.net>
2001-01-10 03:42:43 +0000

    support for windows, ensure that shared object and supp tcl files are built using 'file join'.
    git-svn-id: http://svn.osgeo.org/mapserver/trunk@251 7532c77e-422f-0410-93f4-f0b67bdd69e2

--- mapscript/tcl/pkgIndex.tcl
+++ mapscript/tcl/pkgIndex.tcl
@@ -3,9 +3,9 @@
 # load shared library, and source supplementaly ref procs
 
 package ifneeded Mapscript 1.0 " 
-    load [file join $dir libMapscript[info sharedlibextension]]
-    # source the object reference helps and convenience procs
-    source [file join $dir mapscriptsupp.tcl]
+    load [list [file join $dir libMapscript[info sharedlibextension]]]
+    # source the object reference helpers and convenience procs
+    source [list [file join $dir mapscriptsupp.tcl]]
     package provide Mapscript 1.0
 "
 
