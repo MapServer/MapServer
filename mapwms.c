ad830e9be5a0bf80a35c5310868f3738c51c24e9
Steve Lime <sdlime@gmail.com>
2001-03-13 04:11:35 +0000

    Sketched some function prototypes for WMS compatability.
    git-svn-id: http://svn.osgeo.org/mapserver/trunk@346 7532c77e-422f-0410-93f4-f0b67bdd69e2

--- mapwms.c
+++ mapwms.c
@@ -4,3 +4,28 @@
 /* 
 ** Future home of the MapServer OpenGIS WMS module.
 */
+int msWMSProcessForm(mapObj *map, char **entries, int numentries) {
+  return(MS_SUCCESS);
+}
+
+int msWMSCapabilities(mapObj *map) {
+
+  printf("Content-type: application/xml%c%c",10,10);
+
+  if(!map) {
+    // return a default capabilties document
+    return(MS_SUCCESS);
+  }
+
+  // use the given map object to construct a capabilities document
+
+  return(MS_SUCCESS);
+}
+
+int msWMSGetMap(mapObj *map) {
+  return(MS_SUCCESS);
+}
+
+int msWMSFeatureInfo(mapObj *map) {
+  return(MS_SUCCESS);
+}
