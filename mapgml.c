7df4d999ea048eb1e60a51442379f43683dab1ce
Steve Lime <sdlime@gmail.com>
2001-03-28 15:50:48 +0000

    Initial outline for mapgml.c. Raju will be doing most of the coding. ;-)
    git-svn-id: http://svn.osgeo.org/mapserver/trunk@420 7532c77e-422f-0410-93f4-f0b67bdd69e2

---
+++ mapgml.c
@@ -0,0 +1,26 @@
+#include "map.h"
+#include "maperror.h"
+
+/*
+** Function to write the initial portion of a GML document.
+*/
+int msGMLStart(FILE *stream, const char *gmlversion) 
+{
+
+}
+
+/*
+** Function to write the final portion of a GML document.
+*/
+int msGMLFinish(FILE *stream, const char *gmlversion) 
+{
+
+}
+
+/*
+** Function to write a shapeObj (see mapprimative.h) as a GML feature.
+*/
+int msGMLWriteShape(FILE *stream, shapeObj *shape, const char *gmlversion) 
+{
+
+}
