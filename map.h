00814bb63f15b4f110864b5d38deb58e661474b7
Assefa Yewondwossen <assefa@dmsolutions.ca>
2002-09-24 18:52:15 +0000

    Remove msDrawWMSLayerSWF prototype.
    git-svn-id: http://svn.osgeo.org/mapserver/trunk@1477 7532c77e-422f-0410-93f4-f0b67bdd69e2

--- map.h
+++ map.h
@@ -1197,9 +1197,10 @@ void msDrawMarkerSymbolSWF(symbolSetObj *symbolset, imageObj *image,
 int msDrawRasterLayerSWF(mapObj *map, layerObj *layer, imageObj *image);
 int msDrawVectorLayerAsRasterSWF(mapObj *map, layerObj *layer, imageObj*image);
 
-int msDrawWMSLayerSWF(int nLayerId, httpRequestObj *pasReqInfo, 
+  /*int msDrawWMSLayerSWF(int nLayerId, httpRequestObj *pasReqInfo, 
                       int numRequests, 
                       mapObj *map, layerObj *layer, imageObj *image);
+  */
 
 void msTransformShapeSWF(shapeObj *shape, rectObj extent, double cellsize);
 
