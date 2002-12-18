7476174165d7b3588bb2a435da48d9b1d9f60650
Daniel Morissette <dmorissette@mapgears.com>
2002-12-18 22:18:34 +0000

    Added support for wfs_srs in msGetEPSGProj()
    git-svn-id: http://svn.osgeo.org/mapserver/trunk@1685 7532c77e-422f-0410-93f4-f0b67bdd69e2

--- mapproject.c
+++ mapproject.c
@@ -380,8 +380,9 @@ static int msTestNeedWrap( pointObj pt1, pointObj pt2, pointObj pt2_geo,
 ** msGetEPSGProj()
 **
 ** Extract projection code for this layer/map.  
-** First look for a wms_srs metadata, then gml_srs.  If not found then look for an EPSG
-** code in projectionObj, and if not found then return NULL.
+** First look for a wms_srs metadata, then wfs_srs and gml_srs.  If not 
+** found then look for an EPSG code in projectionObj, and if not found then 
+** return NULL.
 **
 ** If bReturnOnlyFirstOne=TRUE and metadata contains multiple EPSG codes
 ** then only the first one (which is assumed to be the layer's default
@@ -393,19 +394,9 @@ const char *msGetEPSGProj(projectionObj *proj, hashTableObj metadata, int bRetur
   static char epsgCode[20] ="";
   static char *value;
 
-  if ((value = msLookupHashTable(metadata, "wms_srs")) != NULL)
-  {
-    // Metadata value should already be in format "EPSG:n" or "AUTO:..."
-    if (!bReturnOnlyFirstOne)
-        return value;
-
-    // Caller requested only first projection code.
-    strncpy(epsgCode, value, 19);
-    epsgCode[19] = '\0';
-    if ((value=strchr(epsgCode, ' ')) != NULL)
-        *value = '\0';
-    return epsgCode;
-  } else if ((value = msLookupHashTable(metadata, "gml_srs")) != NULL)
+  if ((value = msLookupHashTable(metadata, "wms_srs")) != NULL ||
+      (value = msLookupHashTable(metadata, "wfs_srs")) != NULL ||
+      (value = msLookupHashTable(metadata, "gml_srs")) != NULL)
   {
     // Metadata value should already be in format "EPSG:n" or "AUTO:..."
     if (!bReturnOnlyFirstOne)
