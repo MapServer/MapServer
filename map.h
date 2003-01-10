506b5145656f34cccb5d1592331909856779fc97
Frank Warmerdam <warmerdam@pobox.com>
2003-01-10 15:08:06 +0000

    fixed MS_TRANSPARENT_COLOR macro return values
    git-svn-id: http://svn.osgeo.org/mapserver/trunk@1747 7532c77e-422f-0410-93f4-f0b67bdd69e2

--- map.h
+++ map.h
@@ -132,13 +132,14 @@ extern "C" {
 #define MS_SGN(a)	(((a)<0) ? -1 : 1)
 #define MS_NINT(x)      ((x) >= 0.0 ? ((long) ((x)+.5)) : ((long) ((x)-.5)))
 
-#define MS_PEN_UNSET	-4
+#define MS_PEN_TRANSPARENT -1
+#define MS_PEN_UNSET	   -4
 
 #define MS_VALID_EXTENT(minx, miny, maxx, maxy)  (((minx<maxx) && (miny<maxy))?MS_TRUE:MS_FALSE)
 
 #define MS_INIT_COLOR(color,r,g,b) { (color).red = r; (color).green = g; (color).blue = b; (color).pen = MS_PEN_UNSET; }
 #define MS_VALID_COLOR(color) (((color).red==-1 || (color).green==-1 || (color).blue==-1)?MS_FALSE:MS_TRUE)
-#define MS_TRANSPARENT_COLOR(color) (((color).red==-255 || (color).green==-255 || (color).blue==-255)?MS_FALSE:MS_TRUE)
+#define MS_TRANSPARENT_COLOR(color) (((color).red==-255 || (color).green==-255 || (color).blue==-255)?MS_TRUE:MS_FALSE)
 #define MS_COMPARE_COLORS(a,b) (((a).red!=(b).red || (a).green!=(b).green || (a).blue!=(b).blue)?MS_FALSE:MS_TRUE)
 
 #define MS_IMAGE_MIME_TYPE(format) (format->mimetype ? format->mimetype : "unknown")
