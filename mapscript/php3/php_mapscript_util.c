3ddd95943b6ee4fa3b57dcb9624be847bfe280cd
Daniel Morissette <dmorissette@mapgears.com>
2003-06-26 22:18:34 +0000

    Fixed bug 323: when setting class properties, class member variables on which the script had a reference needed to be destroyed before we set them. Taken out all PHP3 legacy stuff.
    git-svn-id: http://svn.osgeo.org/mapserver/trunk@2188 7532c77e-422f-0410-93f4-f0b67bdd69e2

--- mapscript/php3/php_mapscript_util.c
+++ mapscript/php3/php_mapscript_util.c
@@ -8,7 +8,7 @@
  * Author:   Daniel Morissette, morissette@dmsolutions.ca
  *
  **********************************************************************
- * Copyright (c) 2000-2002, DM Solutions Group
+ * Copyright (c) 2000-2003, DM Solutions Group
  *
  * Permission is hereby granted, free of charge, to any person obtaining a
  * copy of this software and associated documentation files (the "Software"),
@@ -30,6 +30,11 @@
  **********************************************************************
  *
  * $Log$
+ * Revision 1.17  2003/06/26 22:18:34  dan
+ * Fixed bug 323: when setting class properties, class member variables on
+ * which the script had a reference needed to be destroyed before we set them.
+ * Taken out all PHP3 legacy stuff.
+ *
  * Revision 1.16  2003/04/23 18:38:13  dan
  * Do not reset errorObj in _phpms_report_mapserver_error()
  *
@@ -120,7 +125,6 @@ void _phpms_report_mapserver_error(int php_err_type)
 /**********************************************************************
  *                     _phpms_fetch_handle2()
  **********************************************************************/ 
-#ifdef PHP4
 void *_phpms_fetch_handle2(pval *pObj, 
                            int handle_type1, int handle_type2,
                             HashTable *list TSRMLS_DC)
@@ -159,44 +163,6 @@ void *_phpms_fetch_handle2(pval *pObj,
      */
     return retVal;    
 }
-#else
-
-void *_phpms_fetch_handle2(pval *pObj, 
-                           int handle_type1, int handle_type2, 
-                           HashTable *list)
-{
-    pval *phandle;
-    void *retVal = NULL;
-
-    if (pObj->type != IS_OBJECT)
-    {
-        php3_error(E_ERROR, "Object expected as argument.");
-        retVal = NULL;
-    }
-    else if (_php3_hash_find(pObj->value.ht, "_handle_", sizeof("_handle_"), 
-                             (void **)&phandle) == FAILURE)
-    {
-        php3_error(E_ERROR, 
-                   "Unable to find _handle_ property");
-        retVal = NULL;
-    }
-    else
-    {
-        int type;
-        retVal = (void *)php3_list_find(phandle->value.lval, &type);
-        if (retVal == NULL || (type != handle_type1 && type != handle_type2))
-        {
-            php3_error(E_ERROR, "Object has an invalid _handle_ property");
-            retVal = NULL;
-        }
-    }
-
-    /* Note: because of the php3_error() calls above, this function
-     *       will probably never return a NULL value.
-     */
-    return retVal;    
-}
-#endif
 
 /**********************************************************************
  *                     _phpms_fetch_handle()
@@ -211,7 +177,6 @@ void *_phpms_fetch_handle(pval *pObj, int handle_type,
 /**********************************************************************
  *                     _phpms_fetch_property_handle2()
  **********************************************************************/
-#ifdef PHP4
 char *_phpms_fetch_property_handle2(pval *pObj, char *property_name, 
                                     int handle_type1, int handle_type2,
                                     HashTable *list TSRMLS_DC, int err_type)
@@ -246,42 +211,6 @@ char *_phpms_fetch_property_handle2(pval *pObj, char *property_name,
 
     return retVal;
 }
-#else
-char *_phpms_fetch_property_handle2(pval *pObj, char *property_name,
-                                    int handle_type1, int handle_type2,
-                                    HashTable *list, int err_type)
-{
-    pval *phandle;
-    void *retVal = NULL;
-    int type;
-
-    if (pObj->type != IS_OBJECT)
-    {
-        php3_error(err_type, "Object expected as argument.");
-        return NULL;
-    }
-    else if (_php3_hash_find(pObj->value.ht, property_name, 
-                             strlen(property_name)+1, 
-                             (void **)&phandle) == FAILURE)
-    {
-        if (err_type != 0)
-            php3_error(err_type, "Unable to find %s property", property_name);
-        return NULL;
-    }
-
-    if (phandle->type != IS_LONG ||
-        (retVal = (void *)php3_list_find(phandle->value.lval, &type)) ==NULL ||
-        (type != handle_type1 && type != handle_type2))
-    {
-        if (err_type != 0)
-            php3_error(err_type, "Object has an invalid '%s' property", 
-                       property_name);
-        retVal = NULL;
-    }
-
-    return retVal;
-}
-#endif
 
 /**********************************************************************
  *                     _phpms_fetch_property_handle()
@@ -298,7 +227,6 @@ char *_phpms_fetch_property_handle(pval *pObj, char *property_name,
 /**********************************************************************
  *                     _phpms_fetch_property_string()
  **********************************************************************/
-#ifdef PHP4
 char *_phpms_fetch_property_string(pval *pObj, char *property_name, 
                                    int err_type)
 {
@@ -321,30 +249,6 @@ char *_phpms_fetch_property_string(pval *pObj, char *property_name,
     convert_to_string((*phandle));
     return (*phandle)->value.str.val;
 }
-#else
-char *_phpms_fetch_property_string(pval *pObj, char *property_name, 
-                                   int err_type)
-{
-    pval *phandle;
-
-    if (pObj->type != IS_OBJECT)
-    {
-        php3_error(err_type, "Object expected as argument.");
-        return "";
-    }
-    else if (_php3_hash_find(pObj->value.ht, property_name, 
-                             strlen(property_name)+1, 
-                             (void **)&phandle) == FAILURE)
-    {
-        if (err_type != 0)
-            php3_error(err_type, "Unable to find %s property", property_name);
-        return "";
-    }
-
-    convert_to_string(phandle);
-    return phandle->value.str.val;
-}
-#endif
 
 /**********************************************************************
  *                     _phpms_fetch_property_long()
@@ -352,33 +256,22 @@ char *_phpms_fetch_property_string(pval *pObj, char *property_name,
 long _phpms_fetch_property_long(pval *pObj, char *property_name, 
                                 int err_type)
 {
-#ifdef PHP4    
     pval **phandle;
-#else
-    pval *phandle;
-#endif
 
     if (pObj->type != IS_OBJECT)
     {
         php3_error(err_type, "Object expected as argument.");
         return 0;
     }
-#ifdef PHP4
     else if (zend_hash_find(pObj->value.obj.properties, property_name, 
                             strlen(property_name)+1, 
                             (void **)&phandle) == FAILURE)
-#else
-    else if (_php3_hash_find(pObj->value.ht, property_name, 
-                             strlen(property_name)+1, 
-                             (void **)&phandle) == FAILURE)
-#endif
     {
         if (err_type != 0)
             php3_error(err_type, "Unable to find %s property", property_name);
         return 0;
     }
 
-#ifdef PHP4
     if ((*phandle)->type == IS_RESOURCE)
     {
         php3_error(err_type, 
@@ -388,10 +281,6 @@ long _phpms_fetch_property_long(pval *pObj, char *property_name,
     }
     convert_to_long(*phandle);
     return (*phandle)->value.lval;
-#else
-    convert_to_long(phandle);
-    return phandle->value.lval;
-#endif
 }
 
 /**********************************************************************
@@ -400,39 +289,24 @@ long _phpms_fetch_property_long(pval *pObj, char *property_name,
 double _phpms_fetch_property_double(pval *pObj, char *property_name,
                                     int err_type)
 {
-#ifdef PHP4    
     pval **phandle;
-#else
-    pval *phandle;
-#endif
 
     if (pObj->type != IS_OBJECT)
     {
         php3_error(err_type, "Object expected as argument.");
         return 0.0;
     }
-#ifdef PHP4
     else if (zend_hash_find(pObj->value.obj.properties, property_name, 
                             strlen(property_name)+1, 
                             (void **)&phandle) == FAILURE)
-#else
-    else if (_php3_hash_find(pObj->value.ht, property_name, 
-                             strlen(property_name)+1, 
-                             (void **)&phandle) == FAILURE)
-#endif
    {
         if (err_type != 0)
             php3_error(err_type, "Unable to find %s property", property_name);
         return 0.0;
     }
 
-#ifdef PHP4
     convert_to_double(*phandle);
     return (*phandle)->value.dval;
-#else
-    convert_to_double(phandle);
-    return phandle->value.dval;
-#endif
 }
 
 /**********************************************************************
@@ -441,33 +315,22 @@ double _phpms_fetch_property_double(pval *pObj, char *property_name,
 long _phpms_fetch_property_resource(pval *pObj, char *property_name, 
                                     int err_type)
 {
-#ifdef PHP4    
     pval **phandle;
-#else
-    pval *phandle;
-#endif
 
     if (pObj->type != IS_OBJECT)
     {
         php3_error(err_type, "Object expected as argument.");
         return 0;
     }
-#ifdef PHP4
     else if (zend_hash_find(pObj->value.obj.properties, property_name, 
                             strlen(property_name)+1, 
                             (void **)&phandle) == FAILURE)
-#else
-    else if (_php3_hash_find(pObj->value.ht, property_name, 
-                             strlen(property_name)+1, 
-                             (void **)&phandle) == FAILURE)
-#endif
     {
         if (err_type != 0)
             php3_error(err_type, "Unable to find %s property", property_name);
         return 0;
     }
 
-#ifdef PHP4
     if ((*phandle)->type != IS_RESOURCE)
     {
         if (err_type != 0)
@@ -477,10 +340,6 @@ long _phpms_fetch_property_resource(pval *pObj, char *property_name,
         return 0;
     }
     return (*phandle)->value.lval;
-#else
-    convert_to_long(phandle);
-    return phandle->value.lval;
-#endif
 }
 
 
@@ -492,45 +351,26 @@ long _phpms_fetch_property_resource(pval *pObj, char *property_name,
 int _phpms_set_property_string(pval *pObj, char *property_name, 
                                char *szNewValue, int err_type)
 {
-#ifdef PHP4    
     pval **phandle;
-#else
-    pval *phandle;
-#endif
 
     if (pObj->type != IS_OBJECT)
     {
         php3_error(err_type, "Object expected as argument.");
         return -1;
     }
-#ifdef PHP4
     else if (zend_hash_find(pObj->value.obj.properties, property_name, 
                             strlen(property_name)+1, 
                             (void **)&phandle) == FAILURE)
-#else
-    else if (_php3_hash_find(pObj->value.ht, property_name, 
-                             strlen(property_name)+1, 
-                             (void **)&phandle) == FAILURE)
-#endif
     {
         if (err_type != 0)
             php3_error(err_type, "Unable to find %s property", property_name);
         return -1;
     }
 
-#ifdef PHP4
-    convert_to_string(*phandle);
-    if ((*phandle)->value.str.val)
-        efree((*phandle)->value.str.val);
-    (*phandle)->value.str.val = estrdup(szNewValue);
-    (*phandle)->value.str.len = strlen(szNewValue);
-#else
-    convert_to_string(phandle);
-    if (phandle->value.str.val)
-        efree(phandle->value.str.val);
-    phandle->value.str.val = estrdup(szNewValue);
-    phandle->value.str.len = strlen(szNewValue);
-#endif
+    SEPARATE_ZVAL(phandle);
+    zval_dtor(*phandle);
+    ZVAL_STRING(*phandle, szNewValue, 1);
+
     return 0;
 }
 
@@ -540,40 +380,25 @@ int _phpms_set_property_string(pval *pObj, char *property_name,
 int _phpms_set_property_long(pval *pObj, char *property_name, 
                              long lNewValue, int err_type)
 {
-#ifdef PHP4    
     pval **phandle;
-#else
-    pval *phandle;
-#endif
-
 
     if (pObj->type != IS_OBJECT)
     {
         php3_error(err_type, "Object expected as argument.");
         return -1;
     }
-#ifdef PHP4
     else if (zend_hash_find(pObj->value.obj.properties, property_name, 
                             strlen(property_name)+1, 
                             (void **)&phandle) == FAILURE)
-#else
-    else if (_php3_hash_find(pObj->value.ht, property_name, 
-                             strlen(property_name)+1, 
-                             (void **)&phandle) == FAILURE)
-#endif
     {
         if (err_type != 0)
             php3_error(err_type, "Unable to find %s property", property_name);
         return -1;
     }
 
-#ifdef PHP4
-    convert_to_long(*phandle);
-    (*phandle)->value.lval = lNewValue;
-#else
-    convert_to_long(phandle);
-    phandle->value.lval = lNewValue;
-#endif
+    SEPARATE_ZVAL(phandle);
+    zval_dtor(*phandle);
+    ZVAL_LONG(*phandle, lNewValue);
 
     return 0;
 }
@@ -584,39 +409,25 @@ int _phpms_set_property_long(pval *pObj, char *property_name,
 int _phpms_set_property_double(pval *pObj, char *property_name, 
                                double dNewValue, int err_type)
 {
-#ifdef PHP4    
     pval **phandle;
-#else
-    pval *phandle;
-#endif
 
     if (pObj->type != IS_OBJECT)
     {
         php3_error(err_type, "Object expected as argument.");
         return -1;
     }
-#ifdef PHP4
     else if (zend_hash_find(pObj->value.obj.properties, property_name, 
                             strlen(property_name)+1, 
                             (void **)&phandle) == FAILURE)
-#else
-    else if (_php3_hash_find(pObj->value.ht, property_name, 
-                             strlen(property_name)+1, 
-                             (void **)&phandle) == FAILURE)
-#endif
     {
         if (err_type != 0)
             php3_error(err_type, "Unable to find %s property", property_name);
         return -1;
     }
 
-#ifdef PHP4
-     convert_to_double(*phandle);
-     (*phandle)->value.dval = dNewValue;
-#else
-    convert_to_double(phandle);
-    phandle->value.dval = dNewValue;
-#endif
+    SEPARATE_ZVAL(phandle);
+    zval_dtor(*phandle);
+    ZVAL_DOUBLE(*phandle, dNewValue);
 
     return 0;
 }
@@ -624,7 +435,6 @@ int _phpms_set_property_double(pval *pObj, char *property_name,
 /**********************************************************************
  *                     _phpms_add_property_object()
  **********************************************************************/
-#ifdef PHP4
 int _phpms_add_property_object(pval *pObj,      
                                char *property_name, pval *pObjToAdd,
                                int err_type)
@@ -638,40 +448,6 @@ int _phpms_add_property_object(pval *pObj,
 
     return 0;
 }
-#else
-int _phpms_add_property_object(pval *pObj,      
-                               char *property_name, pval *pObjToAdd,
-                               int err_type)
-{
-    pval *phandle;
-
-    /* This is kind of a hack...
-     * We will add a 'long' property, and then we'll replace its contents 
-     * with the object that was passed.
-     */
-
-    if (pObj->type != IS_OBJECT || 
-        (pObjToAdd->type != IS_OBJECT && pObjToAdd->type != IS_ARRAY))
-    {
-        php3_error(err_type, "Object or array expected as argument.");
-        return -1;
-    }
-    else if (add_property_long(pObj, property_name, 0) == FAILURE ||
-             _php3_hash_find(pObj->value.ht, property_name, 
-                             strlen(property_name)+1, 
-                             (void **)&phandle) == FAILURE)
-    {
-        if (err_type != 0)
-            php3_error(err_type, "Unable to add %s property", property_name);
-        return -1;
-    }
-
-    phandle->type = pObjToAdd->type;
-    phandle->value.ht = pObjToAdd->value.ht;
-
-    return 0;
-}
-#endif
 
 
 /**********************************************************************
@@ -690,24 +466,11 @@ int _phpms_object_init(pval *return_value, int  handle_id,
   void ***tsrm_ls = NULL;
 #endif
 
-#ifdef PHP4
     zend_class_entry *new_class_entry_ptr;
     new_class_entry_ptr = (zend_class_entry *)zend_class_entry_ptr;
 
     object_init_ex(return_value, new_class_entry_ptr);
     add_property_resource(return_value, "_handle_", handle_id);
-#else
-    object_init(return_value);
-    add_property_long(return_value, "_handle_", handle_id);
-
-    while(class_functions && class_functions->fname != NULL)
-    {
-        add_method(return_value, 
-                   class_functions->fname, class_functions->handler );
-
-        class_functions++;
-    }
-#endif
 
     return 0;
 }
@@ -735,7 +498,6 @@ int _php_extract_associative_array(HashTable *php, char **array)
 /*      Note : code extacted from sablot.c (functions related to        */
 /*      xslt)                                                           */
 /* -------------------------------------------------------------------- */
-#ifdef PHP4
     zval **value;
     char *string_key = NULL;
     ulong num_key;
@@ -758,11 +520,6 @@ int _php_extract_associative_array(HashTable *php, char **array)
     }
     array[i++] = NULL;
 
-#endif
     return 1;
 }
-
-
-
-
 
