da2d4428c44714a12ddf8bd16d2b9cd6d1802b85
Daniel Morissette <dmorissette@mapgears.com>
2004-02-04 16:57:43 +0000

    Replace tabs in source with spaces (using 4 chars tabs)
    git-svn-id: http://svn.osgeo.org/mapserver/trunk@2709 7532c77e-422f-0410-93f4-f0b67bdd69e2

--- tile4ms.c
+++ tile4ms.c
@@ -39,11 +39,11 @@
 int process_shapefiles(char *metaFileNameP, char *tileFileNameP, 
                        int tile_path_only) 
 {
-SHPHandle	hSHP, tileSHP;
-rectObj 	extentRect;
+SHPHandle   hSHP, tileSHP;
+rectObj     extentRect;
 lineObj         line;
-shapeObj	shapeRect;
-DBFHandle	tileDBF;
+shapeObj    shapeRect;
+DBFHandle   tileDBF;
 DBFHandle       shpDBF;
 
 typedef struct DBFFieldDef_struct {
@@ -60,15 +60,15 @@ int             decimals;
 int             fieldCnt;
 int             i;
 
-FILE		*metaFP = NULL;
-char		*p;
-char		tileshapeName[256];
-char		tiledbfName[256];
-char		shapeFileName[256];
-int		entityNum;
+FILE        *metaFP = NULL;
+char        *p;
+char        tileshapeName[256];
+char        tiledbfName[256];
+char        shapeFileName[256];
+int     entityNum;
 
-int		tilesFound = 0;
-int		tilesProcessed = 0;
+int     tilesFound = 0;
+int     tilesProcessed = 0;
 
   msInitShape(&shapeRect);
   line.point = (pointObj *)malloc(sizeof(pointObj)*5);
@@ -77,155 +77,155 @@ int		tilesProcessed = 0;
   // open metafile
   // -------------
   if (NULL==(metaFP=fopen(metaFileNameP, "r"))) {
-	printf( "Unable to open:%s\n", metaFileNameP);
-	return(1);
-	}
+    printf( "Unable to open:%s\n", metaFileNameP);
+    return(1);
+    }
 
 
   // create new tileindex shapefiles and create a header
   // --------------------------------------------------
   sprintf(tileshapeName, "%s.shp", tileFileNameP);
   if(NULL==(tileSHP=msSHPCreate(tileFileNameP, SHP_POLYGON))) {
-	fclose(metaFP);
-	printf("Unable to create %s.shp (.shx)\n", tileshapeName);
-	return(1);
-	}
+    fclose(metaFP);
+    printf("Unable to create %s.shp (.shx)\n", tileshapeName);
+    return(1);
+    }
 
   
   // create new tileindex dbf-file
   // -----------------------------
   sprintf(tiledbfName, "%s.dbf", tileFileNameP);
   if (NULL==(tileDBF=msDBFCreate(tiledbfName))) {
-	fclose(metaFP);
-	msSHPClose(tileSHP);
-	printf("DBFCreate(%s) failed.\n", tiledbfName);
-	return(1);
-	}
+    fclose(metaFP);
+    msSHPClose(tileSHP);
+    printf("DBFCreate(%s) failed.\n", tiledbfName);
+    return(1);
+    }
 
-   if(msDBFAddField(tileDBF, "LOCATION", FTString, 255, 0 )== -1 ) {
-	fclose(metaFP);
-	msSHPClose(tileSHP);
-	msDBFClose(tileDBF);
-	printf("DBFAddField(fieldname='LOCATION') failed.\n");
-	return(1);
-	}
+  if(msDBFAddField(tileDBF, "LOCATION", FTString, 255, 0 )== -1 ) {
+    fclose(metaFP);
+    msSHPClose(tileSHP);
+    msDBFClose(tileDBF);
+    printf("DBFAddField(fieldname='LOCATION') failed.\n");
+    return(1);
+    }
 
 
 
   // loop through files listed in metafile
   // =====================================
   while (fgets(shapeFileName, 255, metaFP)) {
-	
+    
 
-	if ((p=strchr(shapeFileName, '\n')) != NULL) *p='\0';
+    if ((p=strchr(shapeFileName, '\n')) != NULL) *p='\0';
 
-	if (!strlen(shapeFileName))
-		break;
+    if (!strlen(shapeFileName))
+        break;
 
-	tilesFound++;
+    tilesFound++;
 
-	// read the DBFFields for this shapefile
-	// and save them if the first, otherwise compare them
-	
-	shpDBF = msDBFOpen(shapeFileName, "rb");
+    // read the DBFFields for this shapefile
+    // and save them if the first, otherwise compare them
+    
+    shpDBF = msDBFOpen(shapeFileName, "rb");
 
-	if( shpDBF == NULL ) {
-		printf( "Aborted. Unable to open DBF:%s\n", shapeFileName);
-		break;
-		}
+    if( shpDBF == NULL ) {
+        printf( "Aborted. Unable to open DBF:%s\n", shapeFileName);
+        break;
+        }
 
-	if( theFields == NULL ) {
-		fieldCnt = msDBFGetFieldCount(shpDBF);
-		theFields = (DBFFieldDef *) calloc(fieldCnt, sizeof(DBFFieldDef));
-		for (i=0; i<fieldCnt; i++) {
-			theFields[i].type = msDBFGetFieldInfo(shpDBF, i, 
-					theFields[i].name, 
-					&theFields[i].width, &theFields[i].decimals);
-			if( theFields[i].type == FTInvalid ) {
-				printf( "Aborted. DBF Invalid field (%d of %d) file:%s\n", 
-						i, fieldCnt, shapeFileName);
-				break;
-			}
-		}
-	} else {
-		fieldCnt = msDBFGetFieldCount(shpDBF);
-		for (i=0; i<fieldCnt; i++) {
-			if( theFields[i].type != msDBFGetFieldInfo(shpDBF, i, fldname, &width, &decimals) ||
-			    strcmp(theFields[i].name, fldname) ||
-			    theFields[i].width != width || theFields[i].decimals != decimals ) {
-				printf( "Aborted. DBF fields do not match for file:%s\n", shapeFileName);
-				break;
-			}
-		}
-	}
-	msDBFClose(shpDBF);
+    if( theFields == NULL ) {
+        fieldCnt = msDBFGetFieldCount(shpDBF);
+        theFields = (DBFFieldDef *) calloc(fieldCnt, sizeof(DBFFieldDef));
+        for (i=0; i<fieldCnt; i++) {
+            theFields[i].type = msDBFGetFieldInfo(shpDBF, i, 
+                    theFields[i].name, 
+                    &theFields[i].width, &theFields[i].decimals);
+            if( theFields[i].type == FTInvalid ) {
+                printf( "Aborted. DBF Invalid field (%d of %d) file:%s\n", 
+                        i, fieldCnt, shapeFileName);
+                break;
+            }
+        }
+    } else {
+        fieldCnt = msDBFGetFieldCount(shpDBF);
+        for (i=0; i<fieldCnt; i++) {
+            if( theFields[i].type != msDBFGetFieldInfo(shpDBF, i, fldname, &width, &decimals) ||
+                strcmp(theFields[i].name, fldname) ||
+                theFields[i].width != width || theFields[i].decimals != decimals ) {
+                printf( "Aborted. DBF fields do not match for file:%s\n", shapeFileName);
+                break;
+            }
+        }
+    }
+    msDBFClose(shpDBF);
 
         // Get rid of .shp extension if it was included.
-	if (strlen(shapeFileName) > 4 && 
+    if (strlen(shapeFileName) > 4 && 
             (p=shapeFileName+strlen(shapeFileName)-4) &&
             strcasecmp(p, ".shp") == 0)
           *p = '\0';
 
-	if (!strlen(shapeFileName))
-		break;
+    if (!strlen(shapeFileName))
+        break;
 
-	// read extent from shapefile
-	// --------------------------
-	hSHP = msSHPOpen(shapeFileName, "rb");
+    // read extent from shapefile
+    // --------------------------
+    hSHP = msSHPOpen(shapeFileName, "rb");
 
-	if( hSHP == NULL )  {
-		printf( "Aborted. Unable to open SHP:%s\n", shapeFileName);
-		break;
-		}
-
-        msSHPReadBounds(hSHP, -1, &extentRect);
-	// SHPGetInfo(hSHP, &nEntities, &nShapeType, adfBndsMin, adfBndsMax);
-
-	//printf("File:  %s Bounds 0/1: (%15.10lg,%15.10lg)\n\t(%15.10lg,%15.10lg)\n", shapeFileName, adfBndsMin[0], adfBndsMin[1], adfBndsMax[0], adfBndsMax[1] );
-
-	msSHPClose(hSHP);
-
-
-	// create rectangle describing current shapefile extent
-	// ----------------------------------------------------
-
-	line.point[0].x = line.point[4].x = extentRect.minx; // bottom left
-	line.point[0].y = line.point[4].y  = extentRect.miny;
-	line.point[1].x = extentRect.minx; // top left
-	line.point[1].y = extentRect.maxy;
-	line.point[2].x = extentRect.maxx; // top left
-	line.point[2].y = extentRect.maxy;
-	line.point[3].x = extentRect.maxx; // bottom right
-	line.point[3].y = extentRect.miny;
-
-
-	// create and add shape object.  Returns link to entry in DBF file
-	// ---------------------------------------------------------------
-
-        msAddLine(&shapeRect, &line);
-        entityNum = msSHPWriteShape( tileSHP, &shapeRect );
-	
-        msFreeShape(&shapeRect);
-
-	// store filepath of current shapefile as attribute of rectangle
-	// -------------------------------------------------------------
-
-        // Strip off filename if requested
-        if (tile_path_only)
-        {
-            char *pszTmp;
-            if ((pszTmp = strrchr(shapeFileName, '/')) != NULL ||
-                (pszTmp = strrchr(shapeFileName, '\\')) != NULL )
-            {
-                *(pszTmp+1) = '\0';  // Keep the trailing '/' only.
-            }
+    if( hSHP == NULL )  {
+        printf( "Aborted. Unable to open SHP:%s\n", shapeFileName);
+        break;
         }
 
-	msDBFWriteStringAttribute(tileDBF, entityNum, 0, shapeFileName);
+    msSHPReadBounds(hSHP, -1, &extentRect);
+    // SHPGetInfo(hSHP, &nEntities, &nShapeType, adfBndsMin, adfBndsMax);
 
-	tilesProcessed++;
+    //printf("File:  %s Bounds 0/1: (%15.10lg,%15.10lg)\n\t(%15.10lg,%15.10lg)\n", shapeFileName, adfBndsMin[0], adfBndsMin[1], adfBndsMax[0], adfBndsMax[1] );
 
-	}
+    msSHPClose(hSHP);
+
+
+    // create rectangle describing current shapefile extent
+    // ----------------------------------------------------
+
+    line.point[0].x = line.point[4].x = extentRect.minx; // bottom left
+    line.point[0].y = line.point[4].y  = extentRect.miny;
+    line.point[1].x = extentRect.minx; // top left
+    line.point[1].y = extentRect.maxy;
+    line.point[2].x = extentRect.maxx; // top left
+    line.point[2].y = extentRect.maxy;
+    line.point[3].x = extentRect.maxx; // bottom right
+    line.point[3].y = extentRect.miny;
+
+
+    // create and add shape object.  Returns link to entry in DBF file
+    // ---------------------------------------------------------------
+
+    msAddLine(&shapeRect, &line);
+    entityNum = msSHPWriteShape( tileSHP, &shapeRect );
+    
+    msFreeShape(&shapeRect);
+
+    // store filepath of current shapefile as attribute of rectangle
+    // -------------------------------------------------------------
+
+    // Strip off filename if requested
+    if (tile_path_only)
+    {
+        char *pszTmp;
+        if ((pszTmp = strrchr(shapeFileName, '/')) != NULL ||
+            (pszTmp = strrchr(shapeFileName, '\\')) != NULL )
+        {
+            *(pszTmp+1) = '\0';  // Keep the trailing '/' only.
+        }
+    }
+
+    msDBFWriteStringAttribute(tileDBF, entityNum, 0, shapeFileName);
+
+    tilesProcessed++;
+
+  }
 
   msSHPClose(tileSHP);
   msDBFClose(tileDBF);
@@ -249,7 +249,7 @@ void print_usage_and_exit() {
     printf("<meta-file>\tINPUT  file containing list of shapefile names\n\t\t(complete paths 255 chars max, no extension)\n");
     printf("<tile-file>\tOUTPUT shape file of extent rectangles and names\n\t\tof tiles in <tile-file>.dbf\n");
     printf("-tile-path-only\tOptional flag.  If specified then only the path to the \n\t\tshape files will be stored in the LOCATION field\n\t\tinstead of storing the full filename.\n\n");
-	exit(1);
+    exit(1);
 }
 
 
