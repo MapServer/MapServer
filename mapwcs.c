9f557dfa4538b08eb56850b8ef28d98fc50aaefd
Steve Lime <sdlime@gmail.com>
2004-02-26 03:28:15 +0000

    More skeletal WCS work.
    git-svn-id: http://svn.osgeo.org/mapserver/trunk@2799 7532c77e-422f-0410-93f4-f0b67bdd69e2

--- mapwcs.c
+++ mapwcs.c
@@ -6,33 +6,46 @@ typedef struct {
 } rangeObj; 
 
 typedef struct {
-  char *version;				// 1.0.0 for now
-  char *request;				// GetCapabilities|DescribeCoverage|GetCoverage
-  char *service;				// MUST be WCS
-  char *section;				// of capabilities document: Service|Capability|ContentMetadata
-  char **coverages;				// NULL terminated list of coverages (in the case of a GetCoverage there will only be 1)
-  char *crs;					// request coordinate reference system
-  char *response_crs;			// response coordinate reference system
-  double bbox[6];				// subset bounding box (3D)
+  char *version;		// 1.0.0 for now
+  char *request;		// GetCapabilities|DescribeCoverage|GetCoverage
+  char *service;		// MUST be WCS
+  char *section;		// of capabilities document: Service|Capability|ContentMetadata
+  char **coverages;		// NULL terminated list of coverages (in the case of a GetCoverage there will only be 1)
+  char *crs;			// request coordinate reference system
+  char *response_crs;		// response coordinate reference system
+  double bbox[6];		// subset bounding box (3D), although we'll only use 2D at best
   char **times;
   long width, height, depth;	// image dimensions
-  double resx, resy, resz;		// resolution
+  double resx, resy, resz;      // resolution
   char *format;
-  char *exceptions;				// exception MIME type, (default application=vnd.ogc.se_xml?)
+  char *exceptions;		// exception MIME type, (default application=vnd.ogc.se_xml?)
 } wcsParamsObj;
 
 static int msWCSException(mapObj *map, const char *version) 
 {
+  printf("Content-type: text/xml%c%c",10,10);
+
+  printf("<ServiceExceptionReport\n");
+  printf("xmlns=\"http://www.opengis.net/ogc\" ");
+  printf("xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" ");
+  printf("xsi:schemaLocation=\"http://www.opengis.net/ogc http://ogc.dmsolutions.ca/wfs/1.0/OGC-exception.xsd\">\n");
+  printf("  <ServiceException>\n");
+  msWriteErrorXML(stdout);
+  printf("  </ServiceException>\n");
+  printf("</ServiceExceptionReport>\n");
+
   return MS_FAILURE;
 }
 
 static wcsParamsObj *msWCSCreateParams()
 {
   wcsParamsObj *params;
+
+  fprintf(stderr, "in msWCSCreateParams()\n");
   
   params = (wcsParamsObj *) calloc(1, sizeof(wcsParamsObj));
-  if(params) { // initialize a few things
-   
+  if(params) { // initialize a few things to default values
+    params->version = strdup("1.0.0");
   }
   
   return params;
@@ -40,17 +53,19 @@ static wcsParamsObj *msWCSCreateParams()
 
 static void msWCSFreeParams(wcsParamsObj *params)
 {
-    if(params) {
-	  // TODO
-      if(params->version) free(params->version);
-	  if(params->request) free(params->request);
-	  if(params->service) free(params->service);
-      if(params->section) free(params->section);
-	  if(params->crs) free(params->crs);
-      if(params->response_crs) free(params->crs);      
-      if(params->format) free(params->format);
-	  if(params->exceptions) free(params->exceptions);
-	}
+  fprintf(stderr, "in msWCSFreeParams()\n");
+
+  if(params) {
+    // TODO
+    if(params->version) free(params->version);
+    if(params->request) free(params->request);
+    if(params->service) free(params->service);
+    if(params->section) free(params->section);
+    if(params->crs) free(params->crs);
+    if(params->response_crs) free(params->crs);      
+    if(params->format) free(params->format);
+    if(params->exceptions) free(params->exceptions);
+  }
 }
 
 static void msWCSParseRequest(cgiRequestObj *request, wcsParamsObj *params)
@@ -59,36 +74,75 @@ static void msWCSParseRequest(cgiRequestObj *request, wcsParamsObj *params)
 
   if(!request || !params) // nothing to do
 	return;
-	
+
+  fprintf(stderr, "in msWCSParseRequest()\n");	
+
   if(request->NumParams > 0) {
     for(i=0; i<request->NumParams; i++) {
        if(strcasecmp(request->ParamNames[i], "VERSION") == 0)
-	     params->version = strdup(request->ParamValues[i]);
+	 params->version = strdup(request->ParamValues[i]);
        else if(strcasecmp(request->ParamNames[i], "REQUEST") == 0)
-		 params->request = strdup(request->ParamValues[i]);
+	 params->request = strdup(request->ParamValues[i]);
        else if(strcasecmp(request->ParamNames[i], "SERVICE") == 0)
-		 params->service = strdup(request->ParamValues[i]);
+	 params->service = strdup(request->ParamValues[i]);
        // and so on...
     }
   }
+
+  fprintf(stderr, "version=%s\n", params->version);
 
   // we are not dealing with an XML encoded request at this point
   return;
 }
 
-static int msWCSGetCapabilties(mapObj *map, wcsParamsObj *params)
+static int msWCSGetCapabilities_Service(mapObj *map, wcsParamsObj *params)
 {
-  return MS_FALSE;
+  printf("Service section goes here...\n");
+  return MS_SUCCESS;
+}
+
+static int msWCSGetCapabilities_Capability(mapObj *map, wcsParamsObj *params)
+{
+  printf("Capability section(s) go here...\n");
+  return MS_SUCCESS;
+}
+
+static int msWCSGetCapabilities_ContentMetadata(mapObj *map, wcsParamsObj *params)
+{
+  printf("ContentMetadata section(s) go here...\n");
+  return MS_SUCCESS;
+}
+
+static int msWCSGetCapabilities(mapObj *map, wcsParamsObj *params)
+{
+  printf("Content-type: text/xml%c%c",10,10);
+
+  // print common capability elements
+  printf("capabilties start here...\n");
+
+  // print the various capability sections
+  if(!params->service || strcasecmp(params->service, "Service"))
+    msWCSGetCapabilities_Service(map, params);
+
+  if(!params->service || strcasecmp(params->service, "Capability"))
+    msWCSGetCapabilities_Capability(map, params);
+
+  if(!params->service || strcasecmp(params->service, "ContentMetadata"))
+    msWCSGetCapabilities_ContentMetadata(map, params);
+
+  printf("capabilties end here...\n");
+
+  return MS_SUCCESS;
 }
 
 static int msWCSDescribeCoverage(mapObj *map, wcsParamsObj *params)
 {
-  return MS_FALSE;
+  return MS_SUCCESS;
 }
 
 static int msWCSGetCoverage(mapObj *map, wcsParamsObj *params)
 {
-  return MS_FALSE;
+  return MS_SUCCESS;
 }
 
 // Entry point for WCS requests
@@ -97,21 +151,34 @@ int msWCSDispatch(mapObj *map, cgiRequestObj *request)
 #ifdef USE_WCS_SVR
   wcsParamsObj *params;
   
+  fprintf(stderr, "in mwWCSDispatch()\n");
+
   // populate the service parameters
   params = msWCSCreateParams();
   msWCSParseRequest(request, params);
 
   // If SERVICE is specified then it MUST be "WCS"
-  if(params->ervice != NULL && strcasecmp(params->service, "WFS") != 0)
+  if(params->service && strcasecmp(params->service, "WCS") != 0)
     return MS_DONE;
 
-  // If SERVICE, VERSION and REQUEST not included then noat a WCS request
-  if(!params->service && !params->version && !params->request)
-	return MS_DONE;
+  // If SERVICE and REQUEST not included then not a WCS request
+  if(!params->service && !params->request)
+    return MS_DONE;
 
-  return(MS_DONE); // no WCS support committed yet
+  /*
+  ** Start dispatching requests
+  */
+  if(strcasecmp(params->request, "GetCapabilities") == 0)    
+    return msWCSGetCapabilities(map, params); // TODO, how do we free params
+
+  /*
+  ** Clean-up
+  */
+  msWCSFreeParams(params);
+
+  return MS_SUCCESS;
 #else
   msSetError(MS_WCSERR, "WCS server support is not available.", "msWCSDispatch()");
-  return(MS_FAILURE);
+  return MS_FAILURE;
 #endif
 }
