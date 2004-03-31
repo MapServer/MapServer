38a39ae79ba6787c9457583f721c2afad73f908c
Frank Warmerdam <warmerdam@pobox.com>
2004-03-31 14:44:48 +0000

    Added my standard headers so I can see the log messages.
    git-svn-id: http://svn.osgeo.org/mapserver/trunk@2932 7532c77e-422f-0410-93f4-f0b67bdd69e2

--- mapraster.c
+++ mapraster.c
@@ -1,4 +1,38 @@
-/* $Id$ */
+/******************************************************************************
+ * $Id$
+ *
+ * Project:  MapServer
+ * Purpose:  Code for drawing GDAL raster layers.  Called from 
+ *           msDrawRasterLayerLow() in mapraster.c.  
+ * Author:   Steve Lime 
+ *           Frank Warmerdam, warmerdam@pobox.com
+ ******************************************************************************
+ * Copyright (c) 1996-2001 Regents of the University of Minnesota.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ ******************************************************************************
+ *
+ * $Log$
+ * Revision 1.111  2004/03/31 14:44:48  frank
+ * Added my standard headers so I can see the log messages.
+ *
+ */
 
 #include <assert.h>
 #include "map.h"
