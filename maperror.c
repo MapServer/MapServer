f5b9a1a460957a491c6281be8629dd57379e9a51
Daniel Morissette <dmorissette@mapgears.com>
2004-05-08 14:48:29 +0000

    Fixed another write past end of buffer in msWriteErrorImage() (bug 644)
    git-svn-id: http://svn.osgeo.org/mapserver/trunk@3085 7532c77e-422f-0410-93f4-f0b67bdd69e2

--- maperror.c
+++ maperror.c
@@ -373,9 +373,7 @@ void msWriteErrorImage(mapObj *map, char *filename, int blank) {
     } else {
       nLines = 1;
       papszLines = (char **)malloc(nLines*sizeof(char *));
-      papszLines[0] = (char *)malloc((strlen(errormsg)+1)*sizeof(char));
-      papszLines[0] = strcpy(papszLines[0], errormsg);
-      papszLines[0][strlen(papszLines[0])+1]='\0';
+      papszLines[0] = strdup(errormsg);
     }   
     for (i=0; i<nLines; i++) {
       nYPos = (nSpaceBewteenLines) * ((i*2) +1); 
