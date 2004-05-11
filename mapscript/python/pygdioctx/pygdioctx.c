b340afca3a7a1d5dfb1e51be1d908fb4c063f75e
Sean Gillies <sean.gillies@gmail.com>
2004-05-11 15:36:57 +0000

    Finished bug 658, added prototypes for msLoadImageGDCtx and msLoadImageGDStream to map.h, and made note in HISTORY.TXT
    git-svn-id: http://svn.osgeo.org/mapserver/branches/branch-4-2@3103 7532c77e-422f-0410-93f4-f0b67bdd69e2

--- mapscript/python/pygdioctx/pygdioctx.c
+++ mapscript/python/pygdioctx/pygdioctx.c
@@ -139,12 +139,11 @@ imageObj *createImageObjFromPyFile(int width, int height,
                                    PyObject *file, const char *driver)
 {
     imageObj *image=NULL;
-    outputFormatObj *format;
     struct PyFileIfaceObj_gdIOCtx *pctx;
     
     if (file) {
         pctx = alloc_PyFileIfaceObj_IOCtx(file);
-        image = (imageObj *) msImageLoadGDCtx(pctx, driver);
+        image = (imageObj *) msImageLoadGDCtx((gdIOCtx *) pctx, driver);
         free_PyFileIfaceObj_IOCtx(pctx);
         return image;
     }
