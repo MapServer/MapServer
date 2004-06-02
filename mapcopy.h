d9bbbe8a869702268ce287f8c52a754230175af8
Sean Gillies <sean.gillies@gmail.com>
2004-06-02 12:59:58 +0000

    Update to map copying macros
    git-svn-id: http://svn.osgeo.org/mapserver/branches/branch-4-2@3219 7532c77e-422f-0410-93f4-f0b67bdd69e2

--- mapcopy.h
+++ mapcopy.h
@@ -14,42 +14,42 @@
 /* Following works for GCC and MSVC.  That's ~98% of our users, if Tyler
  * Mitchell's survey is correct
  */
-#define MS_COPYSTELEM(name) (dst)->/**/name = (src)->/**/name
+#define MS_COPYSTELEM(_name) (dst)->/**/_name = (src)->/**/_name
 
 #define MS_MACROBEGIN              do {
 #define MS_MACROEND                } while (0) 
 
-#define MS_COPYRECT(dst, src)       \
-    MS_MACROBEGIN                   \
-        (dst)->minx = (src)->minx;  \
-        (dst)->miny = (src)->miny;  \
-        (dst)->maxx = (src)->maxx;  \
-        (dst)->maxy = (src)->maxy;  \
+#define MS_COPYRECT(_dst, _src)       \
+    MS_MACROBEGIN                     \
+        (_dst)->minx = (_src)->minx;  \
+        (_dst)->miny = (_src)->miny;  \
+        (_dst)->maxx = (_src)->maxx;  \
+        (_dst)->maxy = (_src)->maxy;  \
     MS_MACROEND
 
-#define MS_COPYPOINT(dst, src)      \
-    MS_MACROBEGIN                   \
-        (dst)->x = (src)->x;        \
-        (dst)->y = (src)->y;        \
-        (dst)->m = (src)->m;        \
+#define MS_COPYPOINT(_dst, _src)      \
+    MS_MACROBEGIN                     \
+        (_dst)->x = (_src)->x;        \
+        (_dst)->y = (_src)->y;        \
+        (_dst)->m = (_src)->m;        \
     MS_MACROEND
 
-#define MS_COPYCOLOR(dst, src)      \
-    MS_MACROBEGIN                   \
-        (dst)->pen   = (src)->pen;  \
-        (dst)->red   = (src)->red;  \
-        (dst)->green = (src)->green;\
-        (dst)->blue  = (src)->blue; \
+#define MS_COPYCOLOR(_dst, _src)      \
+    MS_MACROBEGIN                     \
+        (_dst)->pen   = (_src)->pen;  \
+        (_dst)->red   = (_src)->red;  \
+        (_dst)->green = (_src)->green;\
+        (_dst)->blue  = (_src)->blue; \
     MS_MACROEND
 
-#define MS_COPYSTRING(dst, src)     \
-    MS_MACROBEGIN                   \
-        if ((dst) != NULL)          \
-            msFree((dst));          \
-        if (src)                    \
-            (dst) = strdup((src));  \
-        else                        \
-            (dst) = NULL;           \
+#define MS_COPYSTRING(_dst, _src)     \
+    MS_MACROBEGIN                     \
+        if ((_dst) != NULL)           \
+            msFree((_dst));           \
+        if ((_src))                   \
+            (_dst) = strdup((_src));  \
+        else                          \
+            (_dst) = NULL;            \
     MS_MACROEND
 
 
