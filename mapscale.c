9bdcbae9e1bab45e84df3840bf1704ce439c65a4
Steve Lime <sdlime@gmail.com>
2004-06-18 06:08:29 +0000

    Fixed bug 720. Memory leak with embedded scalebars. The layer->numclasses for the dynamic layer was not set to 1 so the class holding the scalebar marker was never freed.
    git-svn-id: http://svn.osgeo.org/mapserver/trunk@3258 7532c77e-422f-0410-93f4-f0b67bdd69e2

--- mapscale.c
+++ mapscale.c
@@ -322,8 +322,10 @@ int msEmbedScalebar(mapObj *map, gdImagePtr img)
     map->layers[l].name = strdup("__embed__scalebar");
     map->layers[l].type = MS_LAYER_ANNOTATION;
 
-    if(initClass(&(map->layers[l].class[0])) == -1) return(-1);    
-      //Update the layer order list with the layer's index.
+    if(initClass(&(map->layers[l].class[0])) == -1) return(-1);
+    map->layers[l].numclasses = 1; // so we make sure to free it
+    
+    // update the layer order list with the layer's index.
     map->layerorder[l] = l;
   }
 
