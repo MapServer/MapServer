2ce7cfcfe9d8b5caf012cd35b1f310fd9c4dd4fe
Frank Warmerdam <warmerdam@pobox.com>
2004-10-28 02:23:35 +0000

    added standard header
    git-svn-id: http://svn.osgeo.org/mapserver/trunk@3902 7532c77e-422f-0410-93f4-f0b67bdd69e2

--- mapgeos.cpp
+++ mapgeos.cpp
@@ -1,3 +1,37 @@
+/******************************************************************************
+ *
+ * Project:  MapServer
+ * Purpose:  MapServer-GEOS integration.
+ * Author:   Steve Lime and the MapServer team.
+ *
+ ******************************************************************************
+ * Copyright (c) 1996-2004 Regents of the University of Minnesota.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in 
+ * all copies of this Software or works derived from this Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ ******************************************************************************
+ *
+ * $Log$
+ * Revision 1.2  2004/10/28 02:23:35  frank
+ * added standard header
+ *
+ */
+
 #ifdef USE_GEOS
 
 #include <string>
@@ -6,6 +40,8 @@
 
 #include "geos.h"
 #include "map.h"
+
+MS_CVSID("$Id$")
 
 using namespace geos;
 
