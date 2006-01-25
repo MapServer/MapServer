0847cfbfda11158a74435952c51b8039c7b419b6
Steve Lime <sdlime@gmail.com>
2006-01-25 22:55:17 +0000

    This commit was manufactured by cvs2svn to create tag 'rel-4-8-0-rc3'.
    git-svn-id: http://svn.osgeo.org/mapserver/tags/rel-4-8-0-rc3@5135 7532c77e-422f-0410-93f4-f0b67bdd69e2

--- mapsde.c
+++ mapsde.c
@@ -27,6 +27,13 @@
  ******************************************************************************
  *
  * $Log$
+ * Revision 1.101.2.1  2006/01/10 19:29:39  hobu
+ * fix the memory allocation bug described in bug 1606
+ *
+ * Revision 1.101  2006/01/10 00:19:49  hobu
+ * Fix up allocation of the ROW_ID columns and how the functions that
+ * call it were using it. (bug 1605)
+ *
  * Revision 1.100  2005/12/15 15:36:59  frank
  * avoid comment-in-comment warnings
  *
@@ -257,12 +264,14 @@ char *msSDELayerGetRowIDColumn(layerObj *layer)
 {
 #ifdef USE_SDE
   long status, column_type; 
-  char* row_id_column;
+  char* column_name;
   SE_REGINFO registration;
 
   msSDELayerInfo *sde=NULL;
   sde = layer->layerinfo;
-  row_id_column = (char*) malloc(SE_MAX_COLUMN_LEN);
+
+
+  column_name = (char*) malloc(SE_MAX_COLUMN_LEN);
   if(!sde) {
     msSetError( MS_SDEERR, 
                 "SDE layer has not been opened.", 
@@ -286,7 +295,7 @@ char *msSDELayerGetRowIDColumn(layerObj *layer)
     }
     
     status = SE_registration_get_info ( sde->connection, 
-                                        strdup(sde->table), 
+                                        sde->table, 
                                         registration);
     if(status != SE_SUCCESS) {
       sde_error(status, 
@@ -296,8 +305,9 @@ char *msSDELayerGetRowIDColumn(layerObj *layer)
     }
     
     status= SE_reginfo_get_rowid_column ( registration, 
-                                          row_id_column, 
+                                          column_name, 
                                           &column_type);
+    SE_reginfo_free(registration);
     if(status != SE_SUCCESS) {
       sde_error(status, 
                 "msSDELayerGetRowIDColumn()", 
@@ -310,15 +320,15 @@ char *msSDELayerGetRowIDColumn(layerObj *layer)
         "returning %s.\n", 
         MS_SDE_ROW_ID_COLUMN);
       }
-      SE_reginfo_free(registration);
       return (MS_SDE_ROW_ID_COLUMN);
     }
     
-    SE_reginfo_free(registration);
-    if (row_id_column){
-      return (strdup(row_id_column)); 
+    if (column_name){
+
+      return (column_name); 
     }
     else {
+      free(column_name);
       return(strdup(MS_SDE_ROW_ID_COLUMN));
     }
 }
@@ -483,13 +493,16 @@ long msSDEGetLayerInfo(layerObj *layer,
 /*     Copies a SDE shape into a MapServer shapeObj                     */
 /* -------------------------------------------------------------------- */
 static int sdeShapeCopy(SE_SHAPE inshp, shapeObj *outshp) {
-	
-  long numparts, numsubparts, numpoints;
-  long *subparts=NULL;
+
   SE_POINT *points=NULL;
   SE_ENVELOPE envelope;
   long type, status;
-
+  long *part_offsets = NULL;
+  long *subpart_offsets = NULL;
+  long num_parts = -1;
+  long num_subparts = -1;
+  long num_points = -1;
+  
   lineObj line={0,NULL};
 
   int i,j,k;
@@ -528,20 +541,16 @@ static int sdeShapeCopy(SE_SHAPE inshp, shapeObj *outshp) {
     return(MS_FAILURE);
   }
 
-  SE_shape_get_num_points(inshp, 0, 0, &numpoints);
-  SE_shape_get_num_parts(inshp, &numparts, &numsubparts);
 
-  if(numsubparts > 0) {
-    subparts = (long *)malloc(numsubparts*sizeof(long));
-    if(!subparts) {
-      msSetError( MS_MEMERR, 
-                  "Unable to allocate parts array.", 
-                  "sdeCopyShape()");
-      return(MS_FAILURE);
-    }
-  }
+  SE_shape_get_num_parts (inshp, &num_parts, &num_subparts);
+  SE_shape_get_num_points (inshp, 0, 0, &num_points); 
+	 
+  part_offsets = (long *) malloc( (num_parts + 1) * sizeof(long));
+  subpart_offsets = (long *) malloc( (num_subparts + 1)	* sizeof(long));
+  part_offsets[num_parts] = num_subparts;
+  subpart_offsets[num_subparts]	= num_points;
 
-  points = (SE_POINT *)malloc(numpoints*sizeof(SE_POINT));
+  points = (SE_POINT *)malloc(num_points*sizeof(SE_POINT));
   if(!points) {
     msSetError( MS_MEMERR, 
                 "Unable to allocate points array.", 
@@ -551,8 +560,8 @@ static int sdeShapeCopy(SE_SHAPE inshp, shapeObj *outshp) {
 
   status = SE_shape_get_all_points( inshp, 
                                     SE_DEFAULT_ROTATION, 
-                                    NULL, 
-                                    subparts, 
+                                    part_offsets, 
+                                    subpart_offsets, 
                                     points, 
                                     NULL, 
                                     NULL);
@@ -562,12 +571,12 @@ static int sdeShapeCopy(SE_SHAPE inshp, shapeObj *outshp) {
   }
 
   k = 0; /* overall point counter */
-  for(i=0; i<numsubparts; i++) {
+  for(i=0; i<num_subparts; i++) {
     
-    if( i == numsubparts-1)
-      line.numpoints = numpoints - subparts[i];
+    if( i == num_subparts-1)
+      line.numpoints = num_points - subpart_offsets[i];
     else
-      line.numpoints = subparts[i+1] - subparts[i];
+      line.numpoints = subpart_offsets[i+1] - subpart_offsets[i];
 
     line.point = (pointObj *)malloc(sizeof(pointObj)*line.numpoints);
     if(!line.point) {
@@ -587,7 +596,8 @@ static int sdeShapeCopy(SE_SHAPE inshp, shapeObj *outshp) {
     free(line.point);
   }
 
-  free(subparts);
+  free(part_offsets);
+  free(subpart_offsets);
   free(points);
 
   /* finally copy the bounding box for the entire shape */
@@ -801,6 +811,7 @@ int msSDELayerOpen(layerObj *layer) {
 #ifdef USE_SDE
   long status=-1;
   char **params=NULL;
+  char **data_params=NULL;
   int numparams=0;
   SE_ERROR error;
   SE_STATEINFO state;
@@ -823,14 +834,13 @@ int msSDELayerOpen(layerObj *layer) {
   }
  
   sde->state_id = SE_BASE_STATE_ID;
+  
   /* initialize the table and spatial column names */
-  sde->table = sde->column = NULL;
-  /* initialize the row_id column */
-  sde->row_id_column = (char*) malloc(SE_MAX_COLUMN_LEN);
+  sde->table = NULL;
+  sde->column = NULL;
 
   /* request a connection and stream from the pool */
   poolinfo = (msSDEConnPoolInfo *)msConnPoolRequest( layer ); 
-
   
   /* If we weren't returned a connection and stream, initialize new ones */
   if (!poolinfo) {
@@ -899,12 +909,12 @@ int msSDELayerOpen(layerObj *layer) {
     msConnPoolRegister(layer, poolinfo, msSDECloseConnection);
     msFreeCharArray(params, numparams); /* done with parameter list */
   }
-  
+
   /* Split the DATA member into its parameters using the comma */
   /* Periods (.) are used to denote table names and schemas in SDE,  */
   /* as are underscores (_). */
-  params = split(layer->data, ',', &numparams);
-  if(!params) {
+  data_params = split(layer->data, ',', &numparams);
+  if(!data_params) {
     msSetError(MS_MEMERR, 
         "Error spliting SDE layer information.", "msSDELayerOpen()");
     return(MS_FAILURE);
@@ -916,29 +926,31 @@ int msSDELayerOpen(layerObj *layer) {
     return(MS_FAILURE);
   }
 
-  sde->table = params[0]; 
-  sde->column = params[1];
+  sde->table = strdup(data_params[0]); 
+  sde->column = strdup(data_params[1]);
+
 
   if (numparams < 3){ 
     /* User didn't specify a version, we won't use one */
     if (layer->debug) {
       msDebug("msSDELayerOpen(): Layer %s did not have a " 
               "specified version.\n", layer->name);
-    }
+    } 
     sde->state_id = SE_DEFAULT_STATE_ID;
   } 
   else {
     if (layer->debug) {
       msDebug("msSDELayerOpen(): Layer %s specified version %s.\n", 
               layer->name, 
-              params[2]);
+              data_params[2]);
     }
     status = SE_versioninfo_create (&(version));
     if(status != SE_SUCCESS) {
       sde_error(status, "msSDELayerOpen()", "SE_versioninfo_create()");
       return(MS_FAILURE);
     }
-    status = SE_version_get_info(poolinfo->connection, params[2], version);
+    status = SE_version_get_info(poolinfo->connection, data_params[2], version);
+    
     if(status != SE_SUCCESS) {
        
       if (status == SE_INVALID_RELEASE) {
@@ -951,7 +963,8 @@ int msSDELayerOpen(layerObj *layer) {
         sde_error(status, "msSDELayerOpen()", "SE_version_get_info()");
         return(MS_FAILURE);
       }
-    } 
+    }
+  
   }
 
   /* Get the STATEID from the given version and set the stream to  */
@@ -981,7 +994,8 @@ int msSDELayerOpen(layerObj *layer) {
       return(MS_FAILURE);
     }
     SE_stateinfo_free (state); 
-    
+
+        msFreeCharArray(data_params, numparams);  
   } /* if (!(sde->state_id == SE_DEFAULT_STATE_ID)) */
   
   
@@ -994,8 +1008,8 @@ int msSDELayerOpen(layerObj *layer) {
 
   status = msSDEGetLayerInfo( layer,
                               poolinfo->connection,
-                              params[0],
-                              params[1],
+                              sde->table,
+                              sde->column,
                               layer->connection,
                               sde->layerinfo);
 
@@ -1214,7 +1228,7 @@ int msSDELayerWhichShapes(layerObj *layer, rectObj rect) {
   if(!(layer->filter.string))
     /* set to empty string */
     status = SE_queryinfo_set_where_clause (query_info, 
-                                            (const CHAR * ) strdup(""));
+                                            (const CHAR * ) "");
   else
     /* set to the layer's filter.string */
     status = SE_queryinfo_set_where_clause (query_info, 
@@ -1377,10 +1391,12 @@ int msSDELayerGetItems(layerObj *layer) {
                 "msSDELayerGetItems()");
     return(MS_FAILURE);
   }
-
-  sde->row_id_column = (char*) malloc(SE_MAX_COLUMN_LEN);
-  sde->row_id_column = msSDELayerGetRowIDColumn(layer);
   
+  if (!sde->row_id_column) {
+    sde->row_id_column = (char*) malloc(SE_MAX_COLUMN_LEN);
+  }
+  sde->row_id_column = msSDELayerGetRowIDColumn(layer);
+
   status = SE_table_describe(sde->connection, sde->table, &n, &itemdefs);
   if(status != SE_SUCCESS) {
     sde_error(status, "msSDELayerGetItems()", "SE_table_describe()");
@@ -1388,8 +1404,6 @@ int msSDELayerGetItems(layerObj *layer) {
   }
 
   layer->numitems = n+1;
-
-  layer->items = (char **)malloc(layer->numitems*sizeof(char *));
   if(!layer->items) {
     msSetError( MS_MEMERR, 
                 "Error allocating layer items array.", 
@@ -1564,10 +1578,9 @@ int msSDELayerInitItemInfo(layerObj *layer)
   msSDELayerInfo *sde=NULL;
 
   sde = layer->layerinfo;
-
-  sde->row_id_column = (char*) malloc(SE_MAX_COLUMN_LEN);
   sde->row_id_column = msSDELayerGetRowIDColumn(layer);
 
+  
   if(!sde) {
     msSetError( MS_SDEERR, 
                 "SDE layer has not been opened.", 
@@ -2143,17 +2156,19 @@ msSDELayerCreateItems(layerObj *layer,
 {
 #ifdef USE_SDE
     /* should be more than enough space, 
-     * SDE always needs a couple of additional items 
+     * SDE always needs a couple of additional items  
      */
+
     layer->items = (char **)calloc(nt+2, sizeof(char *)); 
     if( ! layer->items) {
         msSetError(MS_MEMERR, NULL, "msSDELayerCreateItems()");
         return(MS_FAILURE);
-    }
+      }
     layer->items[0] = msSDELayerGetRowIDColumn(layer); /* row id */
     layer->items[1] = msSDELayerGetSpatialColumn(layer);
     layer->numitems = 2;
     return MS_SUCCESS;
+
 #else
   msSetError( MS_MISCERR, 
               "SDE support is not available.", 
