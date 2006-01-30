07e337d8a1747e4ca6ef3bb41b42986c32b17437
Daniel Morissette <dmorissette@mapgears.com>
2006-01-30 15:06:57 +0000

    Treat classindex as an int instead of a char in resultCacheMemberObj to prevent problems with more than 128 classes (bug 1633)
    git-svn-id: http://svn.osgeo.org/mapserver/trunk@5137 7532c77e-422f-0410-93f4-f0b67bdd69e2

--- map.h
+++ map.h
@@ -28,6 +28,10 @@
  ******************************************************************************
  *
  * $Log$
+ * Revision 1.441  2006/01/30 15:06:57  dan
+ * Treat classindex as an int instead of a char in resultCacheMemberObj to
+ * prevent problems with more than 128 classes (bug 1633)
+ *
  * Revision 1.440  2006/01/20 06:06:53  sdlime
  * Added contains operator to mapgeos.cpp and the Swig-based MapScript interface.
  *
@@ -911,7 +915,7 @@ typedef struct {
 typedef struct {
   long shapeindex;
   int tileindex;
-  char classindex;
+  int classindex;
 } resultCacheMemberObj;
 #ifdef SWIG
 %mutable;
