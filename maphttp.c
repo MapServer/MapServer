96a7739097fd0c13058cd5be3ce75679f81b035b
Assefa Yewondwossen <assefa@dmsolutions.ca>
2007-06-15 12:08:49 +0000

    Chnage copyright to fir MIT license
    git-svn-id: http://svn.osgeo.org/mapserver/trunk@6213 7532c77e-422f-0410-93f4-f0b67bdd69e2

--- maphttp.c
+++ maphttp.c
@@ -15,8 +15,8 @@
  * and/or sell copies of the Software, and to permit persons to whom the
  * Software is furnished to do so, subject to the following conditions:
  * 
- * The above copyright notice and this permission notice shall be included
- * in all copies or substantial portions of the Software.
+ * The above copyright notice and this permission notice shall be included in 
+ * all copies of this Software or works derived from this Software.
  * 
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
