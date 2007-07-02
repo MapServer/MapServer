bf5def9f782fafeaf80889b1d83f3666a63fab28
Steve Lime <sdlime@gmail.com>
2007-07-02 14:46:52 +0000

    Added line to copy autofollow property in msCopyLabel in maplabel.c. (bug 1993).
    git-svn-id: http://svn.osgeo.org/mapserver/branches/branch-4-10@6255 7532c77e-422f-0410-93f4-f0b67bdd69e2

--- mapcopy.c
+++ mapcopy.c
@@ -321,6 +321,7 @@ int msCopyLabel(labelObj *dst, labelObj *src)
     MS_COPYSTELEM(offsety);
     MS_COPYSTELEM(angle);
     MS_COPYSTELEM(autoangle);
+    MS_COPYSTELEM(autofollow);
     MS_COPYSTELEM(buffer);
     MS_COPYSTELEM(antialias);
     MS_COPYSTELEM(wrap);
