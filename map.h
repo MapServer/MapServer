MapServer/MapServer

commit b98d906f268d1994268fccb32db090b60e528008

RFC 31 initial submission. (bug 2143)
git-svn-id: http://svn.osgeo.org/mapserver/trunk@6361 7532c77e-422f-0410-93f4-f0b67bdd69e2

map.h
@@ -1269,7 +1269,6 @@ struct layerVTable {
 };
 #endif /*SWIG*/
 
-
 /* Function prototypes, wrapable */
 MS_DLL_EXPORT int msSaveImage(mapObj *map, imageObj *img, char *filename);
 MS_DLL_EXPORT void msFreeImage(imageObj *img);
@@ -1369,7 +1368,16 @@ MS_DLL_EXPORT int msGetSymbolIndex(symbolSetObj *set, char *name, int try_addima
 MS_DLL_EXPORT mapObj  *msLoadMap(char *filename, char *new_mappath);
 MS_DLL_EXPORT int msSaveMap(mapObj *map, char *filename);
 MS_DLL_EXPORT void msFreeCharArray(char **array, int num_items);
-MS_DLL_EXPORT int msLoadMapParameter(mapObj *map, char *name, char *value);
+MS_DLL_EXPORT int msUpdateScalebarFromString(scalebarObj *scalebar, char *string, int url_string);
+MS_DLL_EXPORT int msUpdateQueryMapFromString(queryMapObj *querymap, char *string, int url_string);
+MS_DLL_EXPORT int msUpdateLabelFromString(labelObj *label, char *string);
+MS_DLL_EXPORT int msUpdateReferenceMapFromString(referenceMapObj *ref, char *string, int url_string);
+MS_DLL_EXPORT int msUpdateLegendFromString(legendObj *legend, char *string, int url_string);
+MS_DLL_EXPORT int msUpdateWebFromString(webObj *web, char *string, int url_string);
+MS_DLL_EXPORT int msUpdateStyleFromString(styleObj *style, char *string, int url_string);
+MS_DLL_EXPORT int msUpdateClassFromString(classObj *class, char *string, int url_string);
+MS_DLL_EXPORT int msUpdateLayerFromString(layerObj *layer, char *string, int url_string);
+MS_DLL_EXPORT int msUpdateMapFromURL(mapObj *map, char *variable, char *string);
 MS_DLL_EXPORT int msEvalRegex(char *e, char *s);
 MS_DLL_EXPORT void msFree(void *p);
 MS_DLL_EXPORT char **msTokenizeMap(char *filename, int *numtokens);

mapfile.h
@@ -1,7 +1,7 @@
 #ifndef MAPFILE_H
 #define MAPFILE_H
 
-enum MS_LEXER_STATES {MS_TOKENIZE_DEFAULT=0, MS_TOKENIZE_FILE, MS_TOKENIZE_STRING, MS_TOKENIZE_EXPRESSION, MS_TOKENIZE_NAME, MS_TOKENIZE_VALUE};
+enum MS_LEXER_STATES {MS_TOKENIZE_DEFAULT=0, MS_TOKENIZE_FILE, MS_TOKENIZE_STRING, MS_TOKENIZE_EXPRESSION, MS_TOKENIZE_URL_VARIABLE, MS_TOKENIZE_URL_STRING};
 
 /*
 ** Keyword definitions for the mapfiles and symbolfiles (used by lexer)

mapserv.c
@@ -205,8 +205,8 @@ mapObj *loadMap(void)
   /* check for any %variable% substitutions here, also do any map_ changes, we do this here so WMS/WFS  */
   /* services can take advantage of these "vendor specific" extensions */
   for(i=0;i<msObj->request->NumParams;i++) {
-    if(strncasecmp(msObj->request->ParamNames[i],"map_",4) == 0) { /* check to see if there are any additions to the mapfile */
-      if(msLoadMapParameter(map, msObj->request->ParamNames[i], msObj->request->ParamValues[i]) == -1) writeError();
+    if(strncasecmp(msObj->request->ParamNames[i],"map_",4) == 0 || strncasecmp(msObj->request->ParamNames[i],"map.",4) == 0) { /* check to see if there are any additions to the mapfile */
+      if(msUpdateMapFromURL(map, msObj->request->ParamNames[i], msObj->request->ParamValues[i]) != MS_SUCCESS) writeError();
       continue;
     }
 

commit a057c31cfb520d9393ffb254c11fe5eb82e281c0

Added MS_TOKENIZE_VALUE, MS_TOKENIZE_NAME definitions and fixed 2 other build issues introduced by r6361 (tichek #2143)
git-svn-id: http://svn.osgeo.org/mapserver/trunk@6364 7532c77e-422f-0410-93f4-f0b67bdd69e2

map.h
@@ -1375,7 +1375,7 @@ MS_DLL_EXPORT int msUpdateReferenceMapFromString(referenceMapObj *ref, char *str
 MS_DLL_EXPORT int msUpdateLegendFromString(legendObj *legend, char *string, int url_string);
 MS_DLL_EXPORT int msUpdateWebFromString(webObj *web, char *string, int url_string);
 MS_DLL_EXPORT int msUpdateStyleFromString(styleObj *style, char *string, int url_string);
-MS_DLL_EXPORT int msUpdateClassFromString(classObj *class, char *string, int url_string);
+MS_DLL_EXPORT int msUpdateClassFromString(classObj *_class, char *string, int url_string);
 MS_DLL_EXPORT int msUpdateLayerFromString(layerObj *layer, char *string, int url_string);
 MS_DLL_EXPORT int msUpdateMapFromURL(mapObj *map, char *variable, char *string);
 MS_DLL_EXPORT int msEvalRegex(char *e, char *s);

mapfile.h
@@ -1,7 +1,7 @@
 #ifndef MAPFILE_H
 #define MAPFILE_H
 
-enum MS_LEXER_STATES {MS_TOKENIZE_DEFAULT=0, MS_TOKENIZE_FILE, MS_TOKENIZE_STRING, MS_TOKENIZE_EXPRESSION, MS_TOKENIZE_URL_VARIABLE, MS_TOKENIZE_URL_STRING};
+enum MS_LEXER_STATES {MS_TOKENIZE_DEFAULT=0, MS_TOKENIZE_FILE, MS_TOKENIZE_STRING, MS_TOKENIZE_EXPRESSION, MS_TOKENIZE_URL_VARIABLE, MS_TOKENIZE_URL_STRING, MS_TOKENIZE_VALUE, MS_TOKENIZE_NAME};
 
 /*
 ** Keyword definitions for the mapfiles and symbolfiles (used by lexer)

mapserv.c
@@ -206,7 +206,9 @@ mapObj *loadMap(void)
   /* services can take advantage of these "vendor specific" extensions */
   for(i=0;i<msObj->request->NumParams;i++) {
     if(strncasecmp(msObj->request->ParamNames[i],"map_",4) == 0 || strncasecmp(msObj->request->ParamNames[i],"map.",4) == 0) { /* check to see if there are any additions to the mapfile */
+/* TODO: missing definition of msUpdateMapFromURL() in r6361
       if(msUpdateMapFromURL(map, msObj->request->ParamNames[i], msObj->request->ParamValues[i]) != MS_SUCCESS) writeError();
+*/
       continue;
     }
 