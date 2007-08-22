6f198e409edfcda031acc5318f740045f710f7ce
Tamas Szekeres <szekerest@gmail.com>
2007-08-22 11:04:33 +0000

    Added the example for writing onto a FileStream
    git-svn-id: http://svn.osgeo.org/mapserver/trunk@6670 7532c77e-422f-0410-93f4-f0b67bdd69e2

--- mapscript/csharp/examples/getbytes.cs
+++ mapscript/csharp/examples/getbytes.cs
@@ -28,15 +28,25 @@ class GetBytes
         try 
         {
 			mapObj map = new mapObj(args[0]);
-			Console.WriteLine ("Drawing map: '" + map.name + "' using imageObj.getBytes");
 
 			using(imageObj image = map.draw())
 			{
+				// solution 1
+				Console.WriteLine ("Drawing map: '" + map.name + "' using imageObj.getBytes");
+				
 				byte[] img = image.getBytes();
 				using (MemoryStream ms = new MemoryStream(img))
 				{
 					Image mapimage = Image.FromStream(ms);
 					mapimage.Save(args[1]);
+				}
+				
+				// solution 2
+				Console.WriteLine ("Drawing map: '" + map.name + "' using imageObj.write");
+
+				using (FileStream fs = File.Open("_" + args[1], FileMode.OpenOrCreate, FileAccess.ReadWrite))
+				{
+					image.write(fs);
 				}
 			}
         } 
