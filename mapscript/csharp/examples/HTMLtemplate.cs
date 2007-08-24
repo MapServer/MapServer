e15a5516bec4200db75331e06886cee7a0a65a31
Tamas Szekeres <szekerest@gmail.com>
2007-08-24 18:23:56 +0000

    Added example for processLegendTemplate
    git-svn-id: http://svn.osgeo.org/mapserver/trunk@6699 7532c77e-422f-0410-93f4-f0b67bdd69e2

---
+++ mapscript/csharp/examples/HTMLtemplate.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Drawing;
+using System.IO;
+using OSGeo.MapServer;
+	
+/**
+ * <p>Title: Mapscript HTMLTemplate example.</p>
+ * <p>Description: A C# based mapscript example to show the usage of HTML templates.</p>
+ * @author Tamas Szekeres	szekerest@gmail.com
+ * @version 1.0  
+*/
+
+/// <summary>
+/// A C# based mapscript example to show the usage of HTML templates.
+/// </summary>
+class HTMLTemplate
+{
+    public static void usage() 
+    { 
+	    Console.WriteLine("usage: HTMLTemplate {mapfile} {templatefile} {outdir}");
+	    System.Environment.Exit(-1);
+    }
+    		  
+    public static void Main(string[] args)
+    {
+        if (args.Length < 3) usage();
+        
+        mapObj map = new mapObj(args[0]);
+		map.legend.template = args[1];
+		map.web.imagepath = args[2];
+		map.web.imageurl = "";
+		string str = null;
+		string[] names = null, values = null;
+		names = new string[] {"map"};
+		values = new string[] { args[0] };
+		str = map.processLegendTemplate(names, values, names.Length);
+		Console.Write(str);
+    }
+}
+
