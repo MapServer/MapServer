5b0ac204cd1c794e85f4a1f6ad43c121a3b9eb7e
Paul Ramsey <pramsey@cleverelephant.ca>
2008-05-07 04:36:01 +0000

    Remove strcasestr in deference to MSVC
    git-svn-id: http://svn.osgeo.org/mapserver/branches/branch-5-0@7568 7532c77e-422f-0410-93f4-f0b67bdd69e2

--- mapshape.c
+++ mapshape.c
@@ -1460,7 +1460,8 @@ int msSHPWhichShapes(shapefileObj *shpfile, rectObj rect, int debug)
 
     /* deal with case where sourcename is of the form 'file.shp' */
     sourcename = strdup(shpfile->source);
-    s = strcasestr(sourcename, ".shp");
+    /* TODO: need to handle this case insensitively */
+    s = strstr(sourcename, ".shp");
     if( s ) *s = '\0';
 
     if((filename = (char *)malloc(strlen(sourcename)+strlen(MS_INDEX_EXTENSION)+1)) == NULL) {
