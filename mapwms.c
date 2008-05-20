be06474686ea3aa41d45d87fd5f06235fad2ea39
Assefa Yewondwossen <assefa@dmsolutions.ca>
2008-05-20 19:21:07 +0000

    wms setup with layers set to default might lead to memeory corruption #2622
    git-svn-id: http://svn.osgeo.org/mapserver/trunk@7603 7532c77e-422f-0410-93f4-f0b67bdd69e2

--- mapwms.c
+++ mapwms.c
@@ -448,8 +448,11 @@ int msWMSLoadGetMapParams(mapObj *map, int nVersion,
                   (map->name && strcasecmp(map->name, layers[k]) == 0) ||
                   (GET_LAYER(map, j)->group && strcasecmp(GET_LAYER(map, j)->group, layers[k]) == 0))
               {
-                  GET_LAYER(map, j)->status = MS_ON;
-                  map->layerorder[nLayerOrder++] = j;
+                  if (GET_LAYER(map, j)->status != MS_DEFAULT)
+                  {
+                      map->layerorder[nLayerOrder++] = j;
+                      GET_LAYER(map, j)->status = MS_ON;
+                  }
                   validlayers++;
                   layerfound = 1;
               }
