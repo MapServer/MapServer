5e2213aec06e10320cb00ef351917a56155b517f
Steve Lime <sdlime@gmail.com>
2008-07-10 02:22:49 +0000

    Copying release...
    git-svn-id: http://svn.osgeo.org/mapserver/tags/rel-5-2-0-rc1@7808 7532c77e-422f-0410-93f4-f0b67bdd69e2

---
+++ mapparser.h
@@ -0,0 +1,96 @@
+/* A Bison parser, made by GNU Bison 1.875.  */
+
+/* Skeleton parser for Yacc-like parsing with Bison,
+   Copyright (C) 1984, 1989, 1990, 2000, 2001, 2002 Free Software Foundation, Inc.
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2, or (at your option)
+   any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 59 Temple Place - Suite 330,
+   Boston, MA 02111-1307, USA.  */
+
+/* As a special exception, when this file is copied by Bison into a
+   Bison output file, you may use that output file without restriction.
+   This special exception was added by the Free Software Foundation
+   in version 1.24 of Bison.  */
+
+/* Tokens.  */
+#ifndef YYTOKENTYPE
+# define YYTOKENTYPE
+   /* Put the tokens into the symbol table, so that GDB and other debuggers
+      know about them.  */
+   enum yytokentype {
+     NUMBER = 258,
+     STRING = 259,
+     ISTRING = 260,
+     TIME = 261,
+     REGEX = 262,
+     IREGEX = 263,
+     OR = 264,
+     AND = 265,
+     NOT = 266,
+     IEQ = 267,
+     IN = 268,
+     GE = 269,
+     LE = 270,
+     GT = 271,
+     LT = 272,
+     NE = 273,
+     EQ = 274,
+     RE = 275,
+     LENGTH = 276,
+     NEG = 277
+   };
+#endif
+#define NUMBER 258
+#define STRING 259
+#define ISTRING 260
+#define TIME 261
+#define REGEX 262
+#define IREGEX 263
+#define OR 264
+#define AND 265
+#define NOT 266
+#define IEQ 267
+#define IN 268
+#define GE 269
+#define LE 270
+#define GT 271
+#define LT 272
+#define NE 273
+#define EQ 274
+#define RE 275
+#define LENGTH 276
+#define NEG 277
+
+
+
+
+#if ! defined (YYSTYPE) && ! defined (YYSTYPE_IS_DECLARED)
+#line 24 "mapparser.y"
+typedef union YYSTYPE {
+  double dblval;
+  int intval;  
+  char *strval;  
+  struct tm tmval;
+} YYSTYPE;
+/* Line 1249 of yacc.c.  */
+#line 87 "mapparser.h"
+# define yystype YYSTYPE /* obsolescent; will be withdrawn */
+# define YYSTYPE_IS_DECLARED 1
+# define YYSTYPE_IS_TRIVIAL 1
+#endif
+
+extern YYSTYPE msyylval;
+
+
+
