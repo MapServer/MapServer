98199767f8a20cc3ef426208202b6216d57f9f2e
Tom Kralidis <tomkralidis@hotmail.com>
2008-07-17 21:14:10 +0000

    updated version to 5.3-dev
    git-svn-id: http://svn.osgeo.org/mapserver/trunk@7825 7532c77e-422f-0410-93f4-f0b67bdd69e2

--- mapserver.h
+++ mapserver.h
@@ -119,7 +119,7 @@ extern "C" {
 
 /* General defines, wrapable */
 
-#define MS_VERSION "5.2.0"
+#define MS_VERSION "5.3-dev"
 
 #define MS_TRUE 1 /* logical control variables */
 #define MS_FALSE 0
