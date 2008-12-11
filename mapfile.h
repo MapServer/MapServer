e920382478d3a2e2214b19343bd5a97f67cb4f0f
Steve Lime <sdlime@gmail.com>
2008-12-11 03:47:35 +0000

    Added VALIDATION keyword (RFC 44).
    git-svn-id: http://svn.osgeo.org/mapserver/trunk@8236 7532c77e-422f-0410-93f4-f0b67bdd69e2

--- mapfile.h
+++ mapfile.h
@@ -270,12 +270,14 @@ enum MS_TOKEN_SOURCES {MS_FILE_TOKENS=0, MS_STRING_TOKENS, MS_URL_TOKENS};
 #define MAXGEOWIDTH 1204
 #define MINGEOWIDTH 1205
 
-/*rfc40 label wrapping*/
+/* rfc40 label wrapping */
 #define MAXLENGTH 1210
 #define MINLENGTH 1211
 
+/* rfc44 URL configuration support */
+#define VALIDATION 1212
 
-/*rfc48 geometry transforms*/
+/* rfc48 geometry transforms */
 #define GEOMTRANSFORM 1220
 
 #endif /* MAPFILE_H */
