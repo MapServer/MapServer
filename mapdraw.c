660fea171dc3172d39581939c1652eba544d7fa4
Umberto Nicoletti <umberto.nicoletti@gmail.com>
2009-02-12 07:48:42 +0000

    #2803: mapserver will crash when rendering a query map in HILITE mode and there are is no STYLE defined
    git-svn-id: http://svn.osgeo.org/mapserver/trunk@8533 7532c77e-422f-0410-93f4-f0b67bdd69e2

--- mapdraw.c
+++ mapdraw.c
@@ -1089,6 +1089,10 @@ int msDrawQueryLayer(mapObj *map, layerObj *layer, imageObj *image)
 
     for(i=0; i<layer->numclasses; i++) {
       if(layer->type == MS_LAYER_POLYGON) { /* alter BOTTOM style since that's almost always the fill */
+	if (layer->class[i]->styles == NULL) {
+           msSetError(MS_MISCERR, "Don't know how to draw class %s of layer %s without a style definition.", "msDrawQueryLayer()", layer->class[i]->name, layer->name);
+           return(MS_FAILURE);
+	}
         if(MS_VALID_COLOR(layer->class[i]->styles[0]->color)) {
           colorbuffer[i] = layer->class[i]->styles[0]->color; /* save the color from the BOTTOM style */
           layer->class[i]->styles[0]->color = map->querymap.color;
