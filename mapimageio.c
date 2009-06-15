9c0923a92aacf57b4fd03583039bb2999c20a7be
Frank Warmerdam <warmerdam@pobox.com>
2009-06-15 19:09:44 +0000

    add standard header, set keywords properties
    git-svn-id: http://svn.osgeo.org/mapserver/trunk@9105 7532c77e-422f-0410-93f4-f0b67bdd69e2

--- mapimageio.c
+++ mapimageio.c
@@ -1,8 +1,39 @@
+/******************************************************************************
+ * $Id$
+ *
+ * Project:  MapServer
+ * Purpose:  Low level PNG/JPEG image io functions independent of GD.
+ * Author:   Thomas Bonfort (tbonfort)
+ *
+ ******************************************************************************
+ * Copyright (c) 2009 Thomas Bonfort
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in 
+ * all copies of this Software or works derived from this Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ ****************************************************************************/
+
 #include "png.h"
 #include "setjmp.h"
 #include "mapserver.h"
 
 #include "jpeglib.h"
+
+MS_CVSID("$Id$")
 
 typedef struct _streamInfo {
     FILE *fp;
