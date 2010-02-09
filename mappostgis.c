54ea16d9216e5b3398d9533f8b2eb688bd654892
Paul Ramsey <pramsey@cleverelephant.ca>
2010-02-09 03:25:47 +0000

    Determine PgSQL version in a more backwards compatible way (#3291)
    git-svn-id: http://svn.osgeo.org/mapserver/trunk@9785 7532c77e-422f-0410-93f4-f0b67bdd69e2

--- mappostgis.c
+++ mappostgis.c
@@ -394,6 +394,63 @@ static void find_bounds(shapeObj *shape)
 /* TODO Review and clean above! */
 
 
+static int msPostGISRetrievePgVersion(PGconn *pgconn) { 
+#ifndef POSTGIS_HAS_SERVER_VERSION 
+    int pgVersion = 0; 
+    char *strVersion = NULL; 
+    char *strParts[3] = { NULL, NULL, NULL }; 
+    int i = 0, j = 0, len = 0; 
+    int factor = 10000; 
+ 
+    if (pgconn == NULL) { 
+        msSetError(MS_QUERYERR, "Layer does not have a postgis connection.", "msPostGISRetrievePgVersion()"); 
+        return(MS_FAILURE); 
+    } 
+
+    if (! PQparameterStatus(pgconn, "server_version") )
+        return(MS_FAILURE); 
+ 
+    strVersion = strdup(PQparameterStatus(pgconn, "server_version"));
+    if( ! strVersion ) 
+        return MS_FAILURE; 
+
+    strParts[j] = strVersion; 
+    j++; 
+    len = strlen(strVersion); 
+    for( i = 0; i < len; i++ ) { 
+        if( strVersion[i] == '.' ) { 
+            strVersion[i] = '\0'; 
+ 
+            if( j < 3 ) { 
+                strParts[j] = strVersion + i + 1; 
+                j++; 
+            } 
+            else { 
+                free(strVersion); 
+                msSetError(MS_QUERYERR, "Too many parts in version string.", "msPostGISRetrievePgVersion()"); 
+                return MS_FAILURE; 
+            } 
+        } 
+    } 
+     
+    for( j = 0; j < 3 && strParts[j]; j++ ) { 
+        if( atoi(strParts[j]) ) { 
+            pgVersion += factor * atoi(strParts[j]); 
+        } 
+        else { 
+            free(strVersion); 
+            msSetError(MS_QUERYERR, "Unable to parse version string.", "msPostGISRetrievePgVersion()"); 
+            return MS_FAILURE; 
+        } 
+        factor = factor / 100; 
+    } 
+    free(strVersion); 
+    return pgVersion; 
+#else 
+    return PQserverVersion(pgconn); 
+#endif 
+} 
+
 /*
 ** msPostGISRetrievePK()
 **
@@ -439,7 +496,7 @@ int msPostGISRetrievePK(layerObj *layer) {
         msSetError(MS_QUERYERR, "Layer does not have a postgis connection.", "msPostGISRetrievePK()");
         return(MS_FAILURE);
     }
-    pgVersion = PQserverVersion(layerinfo->pgconn);
+    pgVersion = msPostGISRetrievePgVersion(layerinfo->pgconn);
 
     if (pgVersion < 70000) {
         if (layer->debug) {
