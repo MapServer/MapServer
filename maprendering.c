5b1e0f3ea3ca4a254b3670a1782a04de091557cb
Thomas Bonfort <thomas.bonfort@gmail.com>
2011-03-31 09:10:37 +0000

    remove duplicate line
    git-svn-id: http://svn.osgeo.org/mapserver/trunk@11416 7532c77e-422f-0410-93f4-f0b67bdd69e2

--- maprendering.c
+++ maprendering.c
@@ -543,7 +543,6 @@ int msDrawShadeSymbol(symbolSetObj *symbolset, imageObj *image, shapeObj *p, sty
             width = MS_MIN(width, style->maxwidth);
             width = MS_MAX(width, style->minwidth);
             spacing = (style->size <= 0)?scalefactor:style->size*scalefactor;
-            spacing = (style->size <= 0)?scalefactor:style->size*scalefactor;
             spacing = MS_MIN(spacing, style->size);
             spacing = MS_MAX(spacing, style->size);
             ret = msHatchPolygon(image,offsetPolygon,spacing,width,style->angle, &style->color);
