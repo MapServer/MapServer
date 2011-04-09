6747a1545d3e82cd77fbf35464a27d9600f8b7dc
Toby Rahilly <toby.rahilly@gmail.com>
2011-04-09 08:46:51 +0000

    Changing tabs to spaces and adding including standard header for OpenGl files. (#3819)
    git-svn-id: http://svn.osgeo.org/mapserver/trunk@11512 7532c77e-422f-0410-93f4-f0b67bdd69e2

--- mapoglcontext.h
+++ mapoglcontext.h
@@ -1,9 +1,31 @@
-/*
- * mapoglcontext.h
+/******************************************************************************
+ * $id: mapoglcontext.h 7725 2011-04-09 15:56:58Z toby $
  *
- *  Created on: 11/01/2009
- *      Author: toby
- */
+ * Project:  MapServer
+ * Purpose:  Various template processing functions.
+ * Author:   Steve Lime and the MapServer team.
+ *
+ ******************************************************************************
+ * Copyright (c) 1996-2008 Regents of the University of Minnesota.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in 
+ * all copies of this Software or works derived from this Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ ****************************************************************************/
 
 #ifndef MAPOGLCONTEXT_H_
 #define MAPOGLCONTEXT_H_
@@ -44,50 +66,50 @@ class OglRenderer;
 
 class OglContext {
 private:
-	static OglContext* current;
-	static bool initWindow();
-	static bool initSharingContext();
-	static ms_uint32 getTextureSize(GLuint dimension, ms_uint32 value);
-	static GLuint NextPowerOf2(GLuint in);
+    static OglContext* current;
+    static bool initWindow();
+    static bool initSharingContext();
+    static ms_uint32 getTextureSize(GLuint dimension, ms_uint32 value);
+    static GLuint NextPowerOf2(GLuint in);
 
 #if defined(_WIN32) && !defined(__CYGWIN__)
-	static HDC window;
-	static HGLRC sharingContext;
-	HDC hPBufferDC;
-	HGLRC hPBufferRC;
+    static HDC window;
+    static HGLRC sharingContext;
+    HDC hPBufferDC;
+    HGLRC hPBufferRC;
 #else
     static Display* window;
     static GLXContext sharingContext;
     static GLXFBConfig* configs;
-	GLXPbuffer pbuffer;
+    GLXPbuffer pbuffer;
 #endif
 
 public:
-	static ms_uint32 MAX_MULTISAMPLE_SAMPLES;
-	static ms_uint32 MAX_ANISOTROPY;
-	static ms_uint32 MAX_TEXTURE_SIZE;
-	static ms_uint32 MIN_TEXTURE_SIZE;
+    static ms_uint32 MAX_MULTISAMPLE_SAMPLES;
+    static ms_uint32 MAX_ANISOTROPY;
+    static ms_uint32 MAX_TEXTURE_SIZE;
+    static ms_uint32 MIN_TEXTURE_SIZE;
 
-	OglContext(ms_uint32 width, ms_uint32 height);
-	~OglContext();
-	int getHeight() const { return height; }
-	int getWidth() const { return width; }
-	bool isValid() const { return valid; }
-	bool makeCurrent();
-	void bindPBufferToTexture();
+    OglContext(ms_uint32 width, ms_uint32 height);
+    ~OglContext();
+    int getHeight() const { return height; }
+    int getWidth() const { return width; }
+    bool isValid() const { return valid; }
+    bool makeCurrent();
+    void bindPBufferToTexture();
 private:
-	bool createPBuffer(ms_uint32 width, ms_uint32 height);
-	ms_uint32 width;
-	ms_uint32 height;
-	bool valid;
-	int windowPixelFormat;	
+    bool createPBuffer(ms_uint32 width, ms_uint32 height);
+    ms_uint32 width;
+    ms_uint32 height;
+    bool valid;
+    int windowPixelFormat;    
 
 #if defined(_WIN32) && !defined(__CYGWIN__)
-	static PFNWGLCHOOSEPIXELFORMATARBPROC wglChoosePixelFormatARB;
-	static PFNWGLCREATEPBUFFERARBPROC wglCreatePbufferARB;
-	static PFNWGLGETPBUFFERDCARBPROC wglGetPbufferDCARB;
-	static PFNWGLRELEASEPBUFFERDCARBPROC wglReleasePbufferDCARB;
-	static PFNWGLMAKECONTEXTCURRENTARBPROC wglMakeContextCurrentARB;
+    static PFNWGLCHOOSEPIXELFORMATARBPROC wglChoosePixelFormatARB;
+    static PFNWGLCREATEPBUFFERARBPROC wglCreatePbufferARB;
+    static PFNWGLGETPBUFFERDCARBPROC wglGetPbufferDCARB;
+    static PFNWGLRELEASEPBUFFERDCARBPROC wglReleasePbufferDCARB;
+    static PFNWGLMAKECONTEXTCURRENTARBPROC wglMakeContextCurrentARB;
 #endif
 };
 
