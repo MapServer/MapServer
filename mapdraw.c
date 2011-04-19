cb823624919a074da0e083a7b4ef3bcfc24831a8
Alan Boudreault <aboudreault@mapgears.com>
2011-04-19 14:58:02 +0000

    Fixed segmentation fault on invalid symbol (#3849)
    git-svn-id: http://svn.osgeo.org/mapserver/branches/branch-5-6@11603 7532c77e-422f-0410-93f4-f0b67bdd69e2

--- mapdraw.c
+++ mapdraw.c
@@ -1492,6 +1492,11 @@ int msDrawShape(mapObj *map, layerObj *layer, shapeObj *shape, imageObj *image, 
    * do not enter the image */
   if(layer->class[c]->numstyles > 0 && layer->class[c]->styles[0] != NULL) {
       double maxsize,maxunscaledsize;
+      styleObj *style = layer->class[c]->styles[0];
+      if (!MS_IS_VALID_ARRAY_INDEX(style->symbol, map->symbolset.numsymbols)) {
+          msSetError(MS_SYMERR, "Invalid symbol index: %d", "msDrawShape()", style->symbol);
+          return MS_FAILURE;
+      }
       maxsize = MS_MAX(
           msSymbolGetDefaultSize(map->symbolset.symbol[layer->class[c]->styles[0]->symbol]),
           MS_MAX(layer->class[c]->styles[0]->size,layer->class[c]->styles[0]->width)
