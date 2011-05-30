12b0d367a969cff8f6dc5f694a0386c1feb7118a
Daniel Morissette <dmorissette@mapgears.com>
2011-05-30 13:13:23 +0000

    Fix order of metadata lookup for WMS GML GetFeatureInfo. 'ows' should come last, not first.
    git-svn-id: http://svn.osgeo.org/mapserver/trunk@11767 7532c77e-422f-0410-93f4-f0b67bdd69e2

--- mapwms.c
+++ mapwms.c
@@ -3329,7 +3329,7 @@ int msWMSFeatureInfo(mapObj *map, int nVersion, char **names, char **values, int
         else
             msIO_printf("Content-type: application/vnd.ogc.gml%c%c",10,10);
 
-    msGMLWriteQuery(map, NULL, "OMG"); /* default is stdout */
+    msGMLWriteQuery(map, NULL, "MGO"); /* default is stdout */
 
   } 
   else
