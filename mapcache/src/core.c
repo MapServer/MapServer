01b37e3a85a4b454ee7f60869ee6ffba8dc93d08
Thomas Bonfort <thomas.bonfort@gmail.com>
2011-08-26 11:16:14 +0000

    add connection timeout on http requests
    closes issue 55.
    thomas.bonfort | 2011-02-28 16:23:08 +0100 (Mon, 28 Feb 2011)
    
    git-svn-id: http://svn.osgeo.org/mapserver/trunk@12314 7532c77e-422f-0410-93f4-f0b67bdd69e2

--- mapcache/src/core.c
+++ mapcache/src/core.c
@@ -37,32 +37,48 @@ geocache_tile *geocache_core_get_tile(geocache_context *ctx, geocache_request_ge
    return rettile;
 }
 
+geocache_image* _core_get_single_map(geocache_context *ctx, geocache_map *map) {
+
+   geocache_tile **maptiles;
+   int i,nmaptiles;
+   geocache_tileset_get_map_tiles(ctx,map->tileset,map->grid_link,
+         map->extent, map->width, map->height,
+         &nmaptiles, &maptiles);
+   for(i=0;i<nmaptiles;i++) {
+      geocache_tile *tile = maptiles[i];
+      geocache_tileset_tile_get(ctx, tile);
+      if(GC_HAS_ERROR(ctx))
+         return NULL;
+   }
+   geocache_image *getmapim = geocache_tileset_assemble_map_tiles(ctx,map->tileset,map->grid_link,
+         map->extent, map->width, map->height,
+         nmaptiles, maptiles);
+   return getmapim;
+}
+
 geocache_map *geocache_core_get_map(geocache_context *ctx, geocache_request_get_map *req_map) {
 #ifdef USE_CAIRO
-   if(req_map->nmaps==1) {
-      geocache_map *map = req_map->maps[0];
-      geocache_tile **maptiles;
-      int i,nmaptiles;
-      geocache_tileset_get_map_tiles(ctx,map->tileset,map->grid_link,
-            map->extent, map->width, map->height,
-            &nmaptiles, &maptiles);
-      for(i=0;i<nmaptiles;i++) {
-         geocache_tile *tile = maptiles[i];
-         geocache_tileset_tile_get(ctx, tile);
-         if(GC_HAS_ERROR(ctx))
-            return NULL;
-      }
-      geocache_image *getmapim = geocache_tileset_assemble_map_tiles(ctx,map->tileset,map->grid_link,
-            map->extent, map->width, map->height,
-            nmaptiles, maptiles);
-
-      geocache_map *getmap = geocache_tileset_map_create(ctx->pool,map->tileset, map->grid_link);
-      getmap->data = map->tileset->format->write(ctx,getmapim,map->tileset->format);
-      return getmap;
-   } else{
-      ctx->set_error(ctx,501,"get map not implemented for merged layers");
-      return NULL;
+#ifdef DEBUG
+   if(req_maps->nmaps ==0) {
+      ctx->set_error(ctx,500,"BUG: get_map called with 0 maps");
+      return;
    }
+#endif
+   int i;
+   geocache_map *basemap = req_map->maps[0];
+   geocache_image *baseim = _core_get_single_map(ctx,basemap);
+   if(GC_HAS_ERROR(ctx)) return NULL;
+   for(i=1;i<req_map->nmaps;i++) {
+      geocache_map *overlaymap = req_map->maps[i];
+      geocache_image *overlayim = _core_get_single_map(ctx,overlaymap); 
+      if(GC_HAS_ERROR(ctx)) return NULL;
+      geocache_image_merge(ctx,baseim,overlayim);
+      if(GC_HAS_ERROR(ctx)) return NULL;
+   }
+
+   geocache_map *getmap = geocache_tileset_map_create(ctx->pool,basemap->tileset, basemap->grid_link);
+   getmap->data = basemap->tileset->format->write(ctx,baseim,basemap->tileset->format);
+   return getmap;
 #else
    ctx->set_error(ctx,501,"wms getmap handling not configured in this build");
    return NULL;
