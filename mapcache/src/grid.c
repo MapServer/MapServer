eb54bbcd4060e2645d84d62bfae59ac9b3831bd2
Thomas Bonfort <thomas.bonfort@gmail.com>
2011-08-26 11:06:03 +0000

    ddetect trailing chars on service prefix thomas.bonfort | 2011-01-04 13:57:41 +0100 (Tue, 04 Jan 2011)
    git-svn-id: http://svn.osgeo.org/mapserver/trunk@12195 7532c77e-422f-0410-93f4-f0b67bdd69e2

---
+++ mapcache/src/grid.c
@@ -0,0 +1,25 @@
+/*
+ *  Copyright 2010 Thomas Bonfort
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+#include "geocache.h"
+
+/*
+ * allocate and initialize a new tileset
+ */
+geocache_grid* geocache_grid_create(apr_pool_t *pool) {
+   geocache_grid* grid = (geocache_grid*)apr_pcalloc(pool, sizeof(geocache_grid));
+   return grid;
+}
