98a09983366497ca1c2932b43ed6ba5b9f3aa84d
Thomas Bonfort <thomas.bonfort@gmail.com>
2011-08-26 11:12:32 +0000

    use only one semaphore per metatile instead of one semaphore per tile. thomas.bonfort | 2011-01-18 15:49:41 +0100 (Tue, 18 Jan 2011)
    git-svn-id: http://svn.osgeo.org/mapserver/trunk@12269 7532c77e-422f-0410-93f4-f0b67bdd69e2

--- mapcache/src/service_wmts.c
+++ mapcache/src/service_wmts.c
@@ -195,7 +195,7 @@ void _create_capabilities_wmts(geocache_context *ctx, geocache_request_get_capab
                "      <ows:CRS>%s</ows:CRS>\n"
                "      <ows:LowerCorner>%f %f</ows:Lowercorner>\n"
                "      <ows:Uppercorner>%f %f</ows:UpperCorner>\n"
-               "    <ows:BoundingBox>\n",
+               "    </ows:BoundingBox>\n",
                bboxes,
                geocache_grid_get_crs(ctx,grid_link->grid),
                bbox[0],bbox[1],
