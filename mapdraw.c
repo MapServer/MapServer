9ea84f335d4a414bfcd7f41002a6ee3123a08b83
Thomas Bonfort <thomas.bonfort@gmail.com>
2012-04-06 17:39:37 +0200

    fix order of auto labels around points
    fixes #4249 , applies to #4242

--- mapdraw.c
+++ mapdraw.c
@@ -2941,7 +2941,7 @@ int msDrawLabelCache(imageObj *image, mapObj *map)
                   positions[0]=MS_UC; positions[1]=MS_LC; positions[2]=MS_CC;
                   npositions = 3;
                 } else {
-                  positions[0]=MS_UL; positions[1]=MS_LR; positions[2]=MS_UR; positions[3]=MS_LL; positions[4]=MS_CR; positions[5]=MS_CL; positions[6]=MS_UC; positions[7]=MS_LC;
+                  positions[0]=MS_UC; positions[1]=MS_LC; positions[2]=MS_CR; positions[3]=MS_CL; positions[4]=MS_UR; positions[5]=MS_UL; positions[6]=MS_LR; positions[7]=MS_LL;
                   npositions = 8;
                 }
    
