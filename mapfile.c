MapServer/MapServer

commit dee0e49394bb9606e4fd89b8b589b226390edb25

use thread-free version of proj if availabel (#4041)
proj 4.8 supports proj contexts which prevent from placing a mutex
around calls to pj_transform. @warmerdam suggested we should create
a context fro each thread, which is correct. In practice we do not
support multiple threads accessing a mapfile concurrently, so this
should not be an issue here. (mapObjs obtained through msCopyMap()
will have a different proj context assigned).

mapfile.c
@@ -1038,7 +1038,12 @@ void msFreeProjection(projectionObj *p) {
 #ifdef USE_PROJ
   if(p->proj)
   {
+#if PJ_VERSION < 480
       pj_free(p->proj);
+#else
+      pj_ctx_free(p->proj_ctx);
+      p->proj_ctx = NULL;
+#endif
       p->proj = NULL;
   }
 
@@ -1165,8 +1170,8 @@ static int _msProcessAutoProjection(projectionObj *p)
     if( !(p->proj = pj_init(numargs, args)) ) {
         int *pj_errno_ref = pj_get_errno_ref();
         msReleaseLock( TLOCK_PROJ );
-        msSetError(MS_PROJERR, pj_strerrno(*pj_errno_ref), 
-                   "msProcessProjection()");	  
+        msSetError(MS_PROJERR, "proj error \"%s\" for \"%s\"",
+                   "msProcessProjection()", pj_strerrno(*pj_errno_ref), szProjBuf) ;	  
         return(-1);
     }
     
@@ -1194,7 +1199,7 @@ int msProcessProjection(projectionObj *p)
     }
 
     if (strcasecmp(p->args[0], "AUTO") == 0) {
-	p->proj = NULL;
+        p->proj = NULL;
         return 0;
     }
 
@@ -1206,11 +1211,17 @@ int msProcessProjection(projectionObj *p)
         return _msProcessAutoProjection(p);
     }
     msAcquireLock( TLOCK_PROJ );
+#if PJ_VERSION < 480
     if( !(p->proj = pj_init(p->numargs, p->args)) ) {
+#else
+    p->proj_ctx = pj_ctx_alloc();
+    if( !(p->proj=pj_init_ctx(p->proj_ctx, p->numargs, p->args)) ) {
+#endif
+
         int *pj_errno_ref = pj_get_errno_ref();
         msReleaseLock( TLOCK_PROJ );
-        msSetError(MS_PROJERR, pj_strerrno(*pj_errno_ref), 
-                   "msProcessProjection()");	  
+        msSetError(MS_PROJERR, "proj error \"%s\" for \"%s:%s\"",
+                   "msProcessProjection()", pj_strerrno(*pj_errno_ref), p->args[0],p->args[1]) ;	  
         return(-1);
     }
     

mapproject.c
@@ -90,10 +90,14 @@ int msProjectPoint(projectionObj *in, projectionObj *out, pointObj *point)
           point->y *= DEG_TO_RAD;
       }
 
+#if PJ_VERSION < 480
       msAcquireLock( TLOCK_PROJ );
+#endif
       error = pj_transform( in->proj, out->proj, 1, 0, 
                             &(point->x), &(point->y), &z );
+#if PJ_VERSION < 480
       msReleaseLock( TLOCK_PROJ );
+#endif
 
       if( error || point->x == HUGE_VAL || point->y == HUGE_VAL )
           return MS_FAILURE;

mapproject.h
@@ -55,6 +55,9 @@ typedef struct {
   char **args; /* variable number of projection args */
 #ifdef USE_PROJ
   projPJ proj; /* a projection structure for the PROJ package */
+#if PJ_VERSION >= 480
+  projCtx proj_ctx;
+#endif
 #else
   void *proj;
 #endif

commit d082a3360f5feaf12d55c96da63ed3acee404347

fix segfault in proj error message reporting
fix a stupid segfault caused by some more verbose
error reporting introduced in #4041

mapfile.c
@@ -1202,8 +1202,13 @@ int msProcessProjection(projectionObj *p)
 
         int *pj_errno_ref = pj_get_errno_ref();
         msReleaseLock( TLOCK_PROJ );
-        msSetError(MS_PROJERR, "proj error \"%s\" for \"%s:%s\"",
+        if(p->numargs>1) {
+          msSetError(MS_PROJERR, "proj error \"%s\" for \"%s:%s\"",
                    "msProcessProjection()", pj_strerrno(*pj_errno_ref), p->args[0],p->args[1]) ;	  
+        } else {
+          msSetError(MS_PROJERR, "proj error \"%s\" for \"%s\"",
+                   "msProcessProjection()", pj_strerrno(*pj_errno_ref), p->args[0]) ;
+        }
         return(-1);
     }
     