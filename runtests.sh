7ac4452d97b7519974d304c74f9652f86bcb188a
Thomas Bonfort <thomas.bonfort@gmail.com>
2012-09-12 19:32:56 +0200

    test successfult run

--- runtests.sh
+++ runtests.sh
@@ -2,13 +2,14 @@
 
 ret=0
 
-for testcase in misc gdal renderers wxs; do
+#for testcase in misc gdal renderers wxs; do
+for testcase in  gdal ; do
    logfile="/tmp/logfile-$testcase.log"
    exec > >(tee $logfile)
    exec 2>&1
 
    cd msautotest/$testcase
-   ./run_test.py
+   ./run_test.py class16_range.map
 
    grep -q "^0 tests failed" $logfile
    RETVAL=$?
