a66ccb4506c702dcb6b685574c0e9c6055dc0c92
Thomas Bonfort <thomas.bonfort@gmail.com>
2012-09-14 16:15:00 +0200

    test colored ouput, prominent failed testcases

--- runtests.sh
+++ runtests.sh
@@ -3,6 +3,14 @@
 ret=0
 
 tests=( query )
+command_exists () {
+   type "$1" &> /dev/null ;
+}
+
+DIFF=diff
+if command_exists colordiff ; then
+   DIFF="colordiff"
+fi
 
 for testcase in  "${tests[@]}" ; do
    cd msautotest/$testcase
@@ -37,10 +45,13 @@ for testcase in "${tests[@]}"; do
    echo "!!!!! $testcase: FAIL !!!!!"
    echo "failing tests:"
    for failedtest in $failedtests; do
-      echo $failedtest
+      echo ""
+      echo "######################################"
+      echo "# $failedtest"
+      echo "######################################"
       #for txt, gml and xml files, print a diff
       if echo "$failedtest" | egrep -q "(txt|xml|gml)$"; then
-        diff -u "../expected/$failedtest" "$failedtest"
+        $DIFF -u "../expected/$failedtest" "$failedtest"
      fi
    done
    cd ../../..
