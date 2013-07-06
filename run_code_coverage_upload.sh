c347dc825aea14f76ec940e7570132c14f24a4b8
Even Rouault <even.rouault@mines-paris.org>
2013-07-06 22:14:56 +0200

    script cleanup

--- run_code_coverage_upload.sh
+++ run_code_coverage_upload.sh
@@ -1,19 +1,21 @@
 #!/bin/sh
 lcov --directory . --capture --output-file mapserver.info 
-#gcc filter_info.c -o filter_info
-#./filter_info < gdal.info > gdal_filtered.info 
-#genhtml -o ./coverage_html --num-spaces 2 gdal_filtered.info
+
 cp /usr/bin/genhtml .
 # to remove date, so that only real differences show up
 patch -p0 < genhtml.patch
+
 ./genhtml -o ./mapserver_coverage_html --num-spaces 2 mapserver.info  
+
+# download last updated filelist
 lftp -c "set ftp:list-options -a; open $MY_FTP_REPO; get mapserver_coverage_html.txt"
+
 find mapserver_coverage_html -type f -exec md5sum {} \; > local_mapserver_coverage_html.txt
+# compute incremental update
 PUT_AND_RM=$(python find_updated_files.py)
-cp local_mapserver_coverage_html.txt mapserver_coverage_html.txt
 echo $PUT_AND_RM
-#lftp -c "set ftp:list-options -a; open $MY_FTP_REPO; lcd ./mapserver_coverage_html; cd ./mapserver_coverage_html; mirror --reverse --delete --use-cache --verbose --allow-chown --allow-suid --no-umask --parallel=2 --exclude-glob .svn"
+
+cp local_mapserver_coverage_html.txt mapserver_coverage_html.txt
+
+# upload files
 lftp -c "set ftp:list-options -a; open $MY_FTP_REPO; $PUT_AND_RM; put mapserver_coverage_html.txt;"
-#mkdir /tmp/ftpperso
-#curlftpfs $MY_FTP_REPO /tmp/ftpperso/
-#rsync -av mapserver_coverage_html /tmp/ftpperso
