00f90a723cf55871f4b78b1276a89beab24a60a6
Even Rouault <even.rouault@mines-paris.org>
2013-07-06 20:17:37 +0200

    Try using lftp

--- run_code_coverage_upload.sh
+++ run_code_coverage_upload.sh
@@ -4,6 +4,7 @@ lcov --directory . --capture --output-file mapserver.info
 #./filter_info < gdal.info > gdal_filtered.info 
 #genhtml -o ./coverage_html --num-spaces 2 gdal_filtered.info
 genhtml -o ./mapserver_coverage_html --num-spaces 2 mapserver.info  
-mkdir /tmp/ftpperso
-curlftpfs $MY_FTP_REPO /tmp/ftpperso/
-rsync -av mapserver_coverage_html /tmp/ftpperso
+lftp -c "set ftp:list-options -a; open $MY_FTP_REPO; lcd ./mapserver_coverage_html; cd ./mapserver_coverage_html; mirror --reverse --delete --use-cache --verbose --allow-chown --allow-suid --no-umask --parallel=2 --exclude-glob .svn"
+#mkdir /tmp/ftpperso
+#curlftpfs $MY_FTP_REPO /tmp/ftpperso/
+#rsync -av mapserver_coverage_html /tmp/ftpperso
