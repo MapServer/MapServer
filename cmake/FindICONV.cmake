e6e2715613707ea2aaf0b6e1582e5e85db23de7a
Thomas Bonfort <thomas.bonfort@gmail.com>
2013-07-26 09:54:11 +0200

    Add support for coveralls code-coverage reporting in travis builds

--- cmake/FindICONV.cmake
+++ cmake/FindICONV.cmake
@@ -1,5 +1,5 @@
 #
-#  Copyright (c) 2006, Peter Kümmel, <syntheticpp@gmx.net>
+#  Copyright (c) 2006, Peter KÃ¼mmel, <syntheticpp@gmx.net>
 #
 #  Redistribution and use in source and binary forms, with or without
 #  modification, are permitted provided that the following conditions
