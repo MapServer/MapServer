6f0606bd3411de6cfaf176c595279f6249992d7b
Thomas Bonfort <thomas.bonfort@gmail.com>
2013-09-29 11:32:15 +0200

    skip legend rendering for empty class name/title

--- maplegend.c
+++ maplegend.c
@@ -599,7 +599,7 @@ imageObj *msDrawLegend(mapObj *map, int scale_independent, map_hittest *hittest)
 
     for(j=lp->numclasses-1; j>=0; j--) {
       text = lp->class[j]->title?lp->class[j]->title:lp->class[j]->name; /* point to the right legend text, title takes precedence */
-      if(!text) continue; /* skip it */
+      if(!text || !*text) continue; /* skip it */
 
       /* skip the class if the classgroup is defined */
       if(lp->classgroup && (lp->class[j]->group == NULL || strcasecmp(lp->class[j]->group, lp->classgroup) != 0))
