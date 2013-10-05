a7ac6f750368e6cf8e14c92f1ebe94fbfadb2fd3
Even Rouault <even.rouault@mines-paris.org>
2013-10-05 12:00:10 +0200

    Fix ISO C90'ism that causes MSVC compilation error

--- mapresample.c
+++ mapresample.c
@@ -1254,12 +1254,13 @@ int msResampleGDALToMap( mapObj *map, layerObj *layer, imageObj *image,
   
   if(layer->mask) {
     int ret, maskLayerIdx;
+    layerObj *maskLayer;
     maskLayerIdx = msGetLayerIndex(map,layer->mask);
     if(maskLayerIdx == -1) {
       msSetError(MS_MISCERR, "Invalid mask layer specified", "msResampleGDALToMap()");
       return -1;
     }
-    layerObj *maskLayer = GET_LAYER(map, maskLayerIdx);
+    maskLayer = GET_LAYER(map, maskLayerIdx);
     mask_rb = msSmallCalloc(1,sizeof(rasterBufferObj)); 
     ret = MS_IMAGE_RENDERER(maskLayer->maskimage)->getRasterBufferHandle(maskLayer->maskimage,mask_rb);
     if(ret != MS_SUCCESS) {
