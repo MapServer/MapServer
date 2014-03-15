9a1c5eb918cdebfcd062574625fba2eba1d199bd
Luigi Pirelli <luipir@gmail.com>
2014-03-15 15:23:16 +0100

    added managing of SVG format in applytSLD - added thanks to Regione Toscana - SITA
    Added support to svg managing creation of SYMBOL for each svg symbol and setting MS_SYMBOL_SVG.
    Skipping remote symbols in case not set ALLOW_REMOTE_ASSETS config option.

--- mapogcsld.c
+++ mapogcsld.c
@@ -2073,9 +2073,6 @@ int msSLDParsePointSymbolizer(CPLXMLNode *psRoot, layerObj *psLayer,
 int msSLDParseExternalGraphic(CPLXMLNode *psExternalGraphic,
                               styleObj *psStyle,  mapObj *map)
 {
-  /* needed for libcurl function msHTTPGetFile in maphttp.c */
-#if defined(USE_CURL)
-
   char *pszFormat = NULL;
   CPLXMLNode *psURL=NULL, *psFormat=NULL, *psTmp=NULL;
   char *pszURL=NULL;
@@ -2087,12 +2084,13 @@ int msSLDParseExternalGraphic(CPLXMLNode *psExternalGraphic,
   if (psFormat && psFormat->psChild && psFormat->psChild->pszValue)
     pszFormat = psFormat->psChild->pszValue;
 
-  /* supports GIF and PNG */
+  /* supports GIF and PNG and SVG */
   if (pszFormat &&
       (strcasecmp(pszFormat, "GIF") == 0 ||
        strcasecmp(pszFormat, "image/gif") == 0 ||
        strcasecmp(pszFormat, "PNG") == 0 ||
-       strcasecmp(pszFormat, "image/png") == 0)) {
+       strcasecmp(pszFormat, "image/png") == 0 ||
+       strcasecmp(pszFormat, "image/svg+xml") == 0)) {
 
     /* <OnlineResource xmlns:xlink="http://www.w3.org/1999/xlink" xlink:type="simple" xlink:href="http://www.vendor.com/geosym/2267.svg"/> */
     psURL = CPLGetXMLNode(psExternalGraphic, "OnlineResource");
@@ -2105,6 +2103,15 @@ int msSLDParseExternalGraphic(CPLXMLNode *psExternalGraphic,
       }
       if (psTmp && psTmp->psChild) {
         pszURL = (char*)psTmp->psChild->pszValue;
+
+        /* manage ALLOW_REMOTE_ASSETS option to allow loading remote symbols */
+        if ( !msTestConfigOption( map, "ALLOW_REMOTE_ASSETS", MS_FALSE ) )
+        {
+          /* avoid using remote symbol */
+          if (strncasecmp(pszURL, "http", 4) == 0) {
+            return MS_SUCCESS;
+          }
+        }
 
         /*external symbols using http will be automaticallly downloaded. The file should be
           saved in a temporary directory (msAddImageSymbol) #2305*/
@@ -2127,9 +2134,6 @@ int msSLDParseExternalGraphic(CPLXMLNode *psExternalGraphic,
   }
 
   return MS_SUCCESS;
-#else
-  return MS_FAILURE;
-#endif
 }
 
 
