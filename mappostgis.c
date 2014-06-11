MapServer/MapServer

commit 62d3e6146bc1b446965dbbaf36afb6377cd0d1cf

PostGIS input: add 2.5D support when USE_POINT_Z_M is defined
Consequently the PROCESSING option FORCE2D is assumed to be NO by
default when USE_POINT_Z_M is defined (related to #4803)

mappostgis.c
@@ -74,6 +74,12 @@
 #define SEGMENT_ANGLE 10.0
 #define SEGMENT_MINPOINTS 10
 
+#define WKBZOFFSET_NONISO 0x80000000
+#define WKBMOFFSET_NONISO 0x40000000
+
+#define HAS_Z   0x1
+#define HAS_M   0x2
+
 #ifdef USE_POSTGIS
 
 
@@ -106,7 +112,11 @@ msPostGISLayerInfo *msPostGISCreateLayerInfo(void)
   layerinfo->rownum = 0;
   layerinfo->version = 0;
   layerinfo->paging = MS_TRUE;
+#ifdef USE_POINT_Z_M
+  layerinfo->force2d = MS_FALSE;
+#else
   layerinfo->force2d = MS_TRUE;
+#endif
   return layerinfo;
 }
 
@@ -195,10 +205,45 @@ pointArrayAddPoint(pointArrayObj *d, const pointObj *p)
 ** Pass an input type number through the PostGIS version
 ** type map array to handle the pre-2.0 incorrect WKB types
 */
+
 static int
-wkbTypeMap(wkbObj *w, int type)
+wkbTypeMap(wkbObj *w, int type, int* pnZMFlag)
 {
-  if ( type < WKB_TYPE_COUNT )
+  *pnZMFlag = 0;
+  /* PostGIS >= 2 : ISO SQL/MM style Z types ? */
+  if( type >= 1000 && type < 2000 )
+  {
+    type -= 1000;
+    *pnZMFlag = HAS_Z;
+  }
+  /* PostGIS >= 2 : ISO SQL/MM style M types ? */
+  else if( type >= 2000 && type < 3000 )
+  {
+    type -= 2000;
+    *pnZMFlag = HAS_M;
+  }
+  /* PostGIS >= 2 : ISO SQL/MM style ZM types ? */
+  else if( type >= 3000 && type < 4000 )
+  {
+    type -= 3000;
+    *pnZMFlag = HAS_Z | HAS_M;
+  }
+  /* PostGIS 1.X EWKB : Extended WKB Z or ZM ? */
+  else if( (type & WKBZOFFSET_NONISO) != 0 )
+  {
+    if( (type & WKBMOFFSET_NONISO) != 0 )
+        *pnZMFlag = HAS_Z | HAS_M;
+    else
+        *pnZMFlag = HAS_Z;
+    type &= 0x00FFFFFF;
+  }
+  /* PostGIS 1.X EWKB: Extended WKB M ? */
+  else if( (type & WKBMOFFSET_NONISO) != 0 )
+  {
+    *pnZMFlag = HAS_M;
+    type &= 0x00FFFFFF;
+  }
+  if ( type >= 0 && type < WKB_TYPE_COUNT )
     return w->typemap[type];
   else
     return 0;
@@ -209,11 +254,11 @@ wkbTypeMap(wkbObj *w, int type)
 ** advancing the read pointer.
 */
 static int
-wkbType(wkbObj *w)
+wkbType(wkbObj *w, int* pnZMFlag)
 {
   int t;
   memcpy(&t, (w->ptr + 1), sizeof(int));
-  return wkbTypeMap(w,t);
+  return wkbTypeMap(w,t, pnZMFlag);
 }
 
 /*
@@ -221,11 +266,11 @@ wkbType(wkbObj *w)
 ** collection without advancing the read pointer.
 */
 static int
-wkbCollectionSubType(wkbObj *w)
+wkbCollectionSubType(wkbObj *w, int* pnZMFlag)
 {
   int t;
   memcpy(&t, (w->ptr + 1 + 4 + 4 + 1), sizeof(int));
-  return wkbTypeMap(w,t);
+  return wkbTypeMap(w,t, pnZMFlag);
 }
 
 /*
@@ -271,12 +316,31 @@ wkbReadDouble(wkbObj *w)
 ** We assume the endianess of the WKB is the same as this machine.
 */
 static inline void
-wkbReadPointP(wkbObj *w, pointObj *p)
+wkbReadPointP(wkbObj *w, pointObj *p, int nZMFlag)
 {
   memcpy(&(p->x), w->ptr, sizeof(double));
   w->ptr += sizeof(double);
   memcpy(&(p->y), w->ptr, sizeof(double));
   w->ptr += sizeof(double);
+  if( nZMFlag & HAS_Z )
+  {
+#ifdef USE_POINT_Z_M
+      memcpy(&(p->z), w->ptr, sizeof(double));
+      if( !(nZMFlag & HAS_M) )
+          p->m = 0.0;
+#endif
+      w->ptr += sizeof(double);
+  }
+  if( nZMFlag & HAS_M )
+  {
+#ifdef USE_POINT_Z_M
+      if( !(nZMFlag & HAS_Z) )
+          p->z = 0.0;
+      memcpy(&(p->m), w->ptr, sizeof(double));
+#endif
+      w->ptr += sizeof(double);
+  }
+
 }
 
 /*
@@ -284,10 +348,10 @@ wkbReadPointP(wkbObj *w, pointObj *p)
 ** We assume the endianess of the WKB is the same as this machine.
 */
 static inline pointObj
-wkbReadPoint(wkbObj *w)
+wkbReadPoint(wkbObj *w, int nZMFlag)
 {
   pointObj p;
-  wkbReadPointP(w, &p);
+  wkbReadPointP(w, &p, nZMFlag);
   return p;
 }
 
@@ -299,7 +363,7 @@ wkbReadPoint(wkbObj *w)
 ** form.
 */
 static void
-wkbReadLine(wkbObj *w, lineObj *line)
+wkbReadLine(wkbObj *w, lineObj *line, int nZMFlag)
 {
   int i;
   pointObj p;
@@ -308,7 +372,7 @@ wkbReadLine(wkbObj *w, lineObj *line)
   line->numpoints = npoints;
   line->point = msSmallMalloc(npoints * sizeof(pointObj));
   for ( i = 0; i < npoints; i++ ) {
-    wkbReadPointP(w, &p);
+    wkbReadPointP(w, &p, nZMFlag);
     line->point[i] = p;
   }
 }
@@ -321,22 +385,25 @@ static void
 wkbSkipGeometry(wkbObj *w)
 {
   int type, npoints, nrings, ngeoms, i;
+  int nZMFlag;
+  int nCoordDim;
   /*endian = */wkbReadChar(w);
-  type = wkbTypeMap(w,wkbReadInt(w));
+  type = wkbTypeMap(w,wkbReadInt(w), &nZMFlag);
+  nCoordDim = 2 + (((nZMFlag & HAS_Z) != 0) ? 1 : 0) + (((nZMFlag & HAS_M) != 0) ? 1 : 0);
   switch(type) {
     case WKB_POINT:
-      w->ptr += 2 * sizeof(double);
+      w->ptr += nCoordDim * sizeof(double);
       break;
     case WKB_CIRCULARSTRING:
     case WKB_LINESTRING:
       npoints = wkbReadInt(w);
-      w->ptr += npoints * 2 * sizeof(double);
+      w->ptr += npoints * nCoordDim * sizeof(double);
       break;
     case WKB_POLYGON:
       nrings = wkbReadInt(w);
       for ( i = 0; i < nrings; i++ ) {
         npoints = wkbReadInt(w);
-        w->ptr += npoints * 2 * sizeof(double);
+        w->ptr += npoints * nCoordDim * sizeof(double);
       }
       break;
     case WKB_MULTIPOINT:
@@ -362,16 +429,17 @@ wkbConvPointToShape(wkbObj *w, shapeObj *shape)
 {
   int type;
   lineObj line;
+  int nZMFlag;
 
   /*endian = */wkbReadChar(w);
-  type = wkbTypeMap(w,wkbReadInt(w));
+  type = wkbTypeMap(w,wkbReadInt(w),&nZMFlag);
 
   if( type != WKB_POINT ) return MS_FAILURE;
 
   if( ! (shape->type == MS_SHAPE_POINT) ) return MS_FAILURE;
   line.numpoints = 1;
   line.point = msSmallMalloc(sizeof(pointObj));
-  line.point[0] = wkbReadPoint(w);
+  line.point[0] = wkbReadPoint(w, nZMFlag);
   msAddLineDirectly(shape, &line);
   return MS_SUCCESS;
 }
@@ -384,13 +452,14 @@ wkbConvLineStringToShape(wkbObj *w, shapeObj *shape)
 {
   int type;
   lineObj line;
+  int nZMFlag;
 
   /*endian = */wkbReadChar(w);
-  type = wkbTypeMap(w,wkbReadInt(w));
+  type = wkbTypeMap(w,wkbReadInt(w), &nZMFlag);
 
   if( type != WKB_LINESTRING ) return MS_FAILURE;
 
-  wkbReadLine(w,&line);
+  wkbReadLine(w,&line, nZMFlag);
   msAddLineDirectly(shape, &line);
 
   return MS_SUCCESS;
@@ -405,9 +474,10 @@ wkbConvPolygonToShape(wkbObj *w, shapeObj *shape)
   int type;
   int i, nrings;
   lineObj line;
+  int nZMFlag;
 
   /*endian = */wkbReadChar(w);
-  type = wkbTypeMap(w,wkbReadInt(w));
+  type = wkbTypeMap(w,wkbReadInt(w), &nZMFlag);
 
   if( type != WKB_POLYGON ) return MS_FAILURE;
 
@@ -416,7 +486,7 @@ wkbConvPolygonToShape(wkbObj *w, shapeObj *shape)
 
   /* Add each ring to the shape */
   for( i = 0; i < nrings; i++ ) {
-    wkbReadLine(w,&line);
+    wkbReadLine(w,&line, nZMFlag);
     msAddLineDirectly(shape, &line);
   }
 
@@ -434,9 +504,10 @@ wkbConvCurvePolygonToShape(wkbObj *w, shapeObj *shape)
   int type, i, ncomponents;
   int failures = 0;
   int was_poly = ( shape->type == MS_SHAPE_POLYGON );
+  int nZMFlag;
 
   /*endian = */wkbReadChar(w);
-  type = wkbTypeMap(w,wkbReadInt(w));
+  type = wkbTypeMap(w,wkbReadInt(w), &nZMFlag);
   ncomponents = wkbReadInt(w);
 
   if( type != WKB_CURVEPOLYGON ) return MS_FAILURE;
@@ -469,15 +540,16 @@ static int
 wkbConvCircularStringToShape(wkbObj *w, shapeObj *shape)
 {
   int type;
+  int nZMFlag;
   lineObj line = {0, NULL};
 
   /*endian = */wkbReadChar(w);
-  type = wkbTypeMap(w,wkbReadInt(w));
+  type = wkbTypeMap(w,wkbReadInt(w), &nZMFlag);
 
   if( type != WKB_CIRCULARSTRING ) return MS_FAILURE;
 
   /* Stroke the string into a point array */
-  if ( arcStrokeCircularString(w, SEGMENT_ANGLE, &line) == MS_FAILURE ) {
+  if ( arcStrokeCircularString(w, SEGMENT_ANGLE, &line, nZMFlag) == MS_FAILURE ) {
     if(line.point) free(line.point);
     return MS_FAILURE;
   }
@@ -505,9 +577,10 @@ wkbConvCompoundCurveToShape(wkbObj *w, shapeObj *shape)
   int type, ncomponents, i, j;
   lineObj line;
   shapeObj shapebuf;
+  int nZMFlag;
 
   /*endian = */wkbReadChar(w);
-  type = wkbTypeMap(w,wkbReadInt(w));
+  type = wkbTypeMap(w,wkbReadInt(w), &nZMFlag);
 
   /* Init our shape buffer */
   msInitShape(&shapebuf);
@@ -571,9 +644,10 @@ wkbConvCollectionToShape(wkbObj *w, shapeObj *shape)
 {
   int i, ncomponents;
   int failures = 0;
+  int nZMFlag;
 
   /*endian = */wkbReadChar(w);
-  /*type = */wkbTypeMap(w,wkbReadInt(w));
+  /*type = */wkbTypeMap(w,wkbReadInt(w), &nZMFlag);
   ncomponents = wkbReadInt(w);
 
   /*
@@ -602,7 +676,8 @@ wkbConvCollectionToShape(wkbObj *w, shapeObj *shape)
 int
 wkbConvGeometryToShape(wkbObj *w, shapeObj *shape)
 {
-  int wkbtype = wkbType(w); /* Peak at the type number */
+  int nZMFlag;
+  int wkbtype = wkbType(w, &nZMFlag); /* Peak at the type number */
 
   switch(wkbtype) {
       /* Recurse into anonymous collections */
@@ -884,7 +959,7 @@ arcStrokeCircle(const pointObj *p1, const pointObj *p2, const pointObj *p3,
 ** argument.
 */
 int
-arcStrokeCircularString(wkbObj *w, double segment_angle, lineObj *line)
+arcStrokeCircularString(wkbObj *w, double segment_angle, lineObj *line, int nZMFlag)
 {
   pointObj p1, p2, p3;
   int npoints, nedges;
@@ -903,13 +978,13 @@ arcStrokeCircularString(wkbObj *w, double segment_angle, lineObj *line)
   /* Make a large guess at how much space we'll need */
   pa = pointArrayNew(nedges * 180 / segment_angle);
 
-  wkbReadPointP(w,&p3);
+  wkbReadPointP(w,&p3,nZMFlag);
 
   /* Fill out the point array with stroked arcs */
   while( edge < nedges ) {
     p1 = p3;
-    wkbReadPointP(w,&p2);
-    wkbReadPointP(w,&p3);
+    wkbReadPointP(w,&p2,nZMFlag);
+    wkbReadPointP(w,&p3,nZMFlag);
     if ( arcStrokeCircle(&p1, &p2, &p3, segment_angle, edge ? 0 : 1, pa) == MS_FAILURE ) {
       pointArrayFree(pa);
       return MS_FAILURE;
@@ -940,13 +1015,14 @@ static int
 msPostGISFindBestType(wkbObj *w, shapeObj *shape)
 {
   int wkbtype;
+  int nZMFlag;
 
   /* What kind of geometry is this? */
-  wkbtype = wkbType(w);
+  wkbtype = wkbType(w, &nZMFlag);
 
   /* Generic collection, we need to look a little deeper. */
   if ( wkbtype == WKB_GEOMETRYCOLLECTION )
-    wkbtype = wkbCollectionSubType(w);
+    wkbtype = wkbCollectionSubType(w, &nZMFlag);
 
   switch ( wkbtype ) {
     case WKB_POLYGON:
@@ -1652,15 +1728,24 @@ char *msPostGISBuildSQLItems(layerObj *layer)
     */
     char *force2d = "";
 #if TRANSFER_ENCODING == 64
-    static char *strGeomTemplate = "encode(ST_AsBinary(%s(\"%s\"),'%s'),'base64') as geom,\"%s\"";
+    const char *strGeomTemplate = "encode(ST_AsBinary(%s(\"%s\"),'%s'),'base64') as geom,\"%s\"";
 #else
-    static char *strGeomTemplate = "encode(ST_AsBinary(%s(\"%s\"),'%s'),'hex') as geom,\"%s\"";
+    const char *strGeomTemplate = "encode(ST_AsBinary(%s(\"%s\"),'%s'),'hex') as geom,\"%s\"";
 #endif
     if( layerinfo->force2d ) {
       if( layerinfo->version >= 20100 )
         force2d = "ST_Force2D";
       else
         force2d = "ST_Force_2D";
+    }
+    else if( layerinfo->version < 20000 )
+    {
+        /* Use AsEWKB() to get 3D */
+#if TRANSFER_ENCODING == 64
+        strGeomTemplate = "encode(AsEWKB(%s(\"%s\"),'%s'),'base64') as geom,\"%s\"";
+#else
+        strGeomTemplate = "encode(AsEWKB(%s(\"%s\"),'%s'),'hex') as geom,\"%s\"";
+#endif
     }
     strGeom = (char*)msSmallMalloc(strlen(strGeomTemplate) + strlen(force2d) + strlen(strEndian) + strlen(layerinfo->geomcolumn) + strlen(layerinfo->uid) + 1);
     sprintf(strGeom, strGeomTemplate, force2d, layerinfo->geomcolumn, strEndian, layerinfo->uid);
@@ -2146,7 +2231,23 @@ int msPostGISReadShape(layerObj *layer, shapeObj *shape)
   if( layerinfo->version >= 20000 ) /* PostGIS 2.0+ */
     w.typemap = wkb_postgis20;
   else
+  {
     w.typemap = wkb_postgis15;
+    if( layerinfo->force2d == MS_FALSE )
+    {
+        /* Is there SRID ? Skip it */
+        if( w.size >= 9 && (w.ptr[4] & 0x20) != 0 )
+        {
+            w.ptr[5] = w.ptr[1];
+            w.ptr[6] = w.ptr[2];
+            w.ptr[7] = w.ptr[3];
+            w.ptr[8] = w.ptr[4];
+            w.ptr[4] = 1;
+            w.ptr += 4;
+            w.size -= 4;
+        }
+    }
+  }
 
   switch (layer->type) {
 
@@ -2383,6 +2484,9 @@ int msPostGISLayerOpen(layerObj *layer)
   force2d_processing = msLayerGetProcessingKey( layer, "FORCE2D" );
   if(force2d_processing && !strcasecmp(force2d_processing,"no")) {
     layerinfo->force2d = MS_FALSE;
+  }
+  else if(force2d_processing && !strcasecmp(force2d_processing,"yes")) {
+    layerinfo->force2d = MS_TRUE;
   }
   if (layer->debug)
     msDebug("msPostGISLayerOpen: Forcing 2D geometries: %s.\n", (layerinfo->force2d)?"yes":"no");

mappostgis.h
@@ -163,7 +163,7 @@ void msPostGISFreeLayerInfo(layerObj *layer);
 msPostGISLayerInfo *msPostGISCreateLayerInfo(void);
 char *msPostGISBuildSQL(layerObj *layer, rectObj *rect, long *uid);
 int msPostGISParseData(layerObj *layer);
-int arcStrokeCircularString(wkbObj *w, double segment_angle, lineObj *line);
+int arcStrokeCircularString(wkbObj *w, double segment_angle, lineObj *line, int pnZMFlag);
 int wkbConvGeometryToShape(wkbObj *w, shapeObj *shape);
 pointArrayObj* pointArrayNew(int maxpoints);
 void pointArrayFree(pointArrayObj *d);

commit f2acfb46525a7dcb8d8a53744f29071d026e0e4d

PostGIS input: fix regression introduced by 62d3e61 when dealing with 2D-only PostGIS 1.X geometries (#4941)

mappostgis.c
@@ -2241,7 +2241,7 @@ int msPostGISReadShape(layerObj *layer, shapeObj *shape)
             w.ptr[5] = w.ptr[1];
             w.ptr[6] = w.ptr[2];
             w.ptr[7] = w.ptr[3];
-            w.ptr[8] = w.ptr[4];
+            w.ptr[8] = w.ptr[4] & ~(0x20);
             w.ptr[4] = 1;
             w.ptr += 4;
             w.size -= 4;