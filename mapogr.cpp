aae74ff2abed3f2f8fe1f76c9b4bbccf15023dc1
Even Rouault <even.rouault@spatialys.com>
2017-06-19 12:27:11 +0200

    [OGR] Correctly extract BBOX from a Intersects() filter. Complementary fix to f53fd782

--- mapogr.cpp
+++ mapogr.cpp
@@ -3268,6 +3268,7 @@ static msExprNode* BuildExprTree(tokenListNodeObjPtr node,
         {
             if( node->next && node->next->token == '(' )
             {
+                int node_token = node->token;
                 msExprNode* subExpr = BuildExprTree(node->next->next, &node,
                                                     nParenthesisLevel + 1);
                 if( subExpr == NULL )
@@ -3275,7 +3276,7 @@ static msExprNode* BuildExprTree(tokenListNodeObjPtr node,
                     goto fail;
                 }
                 msExprNode* newNode = new msExprNode;
-                newNode->m_nToken = node->token;
+                newNode->m_nToken = node_token;
                 if( subExpr->m_nToken == 0 )
                 {
                     newNode->m_aoChildren = subExpr->m_aoChildren;
@@ -3393,6 +3394,8 @@ fail:
 
 /**********************************************************************
  *                 msOGRExtractTopSpatialFilter()
+ * 
+ * Recognize expressions like "Intersects([shape], wkt) == TRUE [AND ....]"
  **********************************************************************/
 static int  msOGRExtractTopSpatialFilter( msOGRFileInfo *info,
                                           const msExprNode* expr,
@@ -3400,6 +3403,15 @@ static int  msOGRExtractTopSpatialFilter( msOGRFileInfo *info,
 {
   if( expr == NULL )
       return MS_FALSE;
+
+  if( expr->m_nToken == MS_TOKEN_COMPARISON_EQ &&
+      expr->m_aoChildren.size() == 2 &&
+      expr->m_aoChildren[1]->m_nToken == MS_TOKEN_LITERAL_BOOLEAN &&
+      expr->m_aoChildren[1]->m_dfVal == 1.0 )
+  {
+      return msOGRExtractTopSpatialFilter(info, expr->m_aoChildren[0],
+                                          pSpatialFilterNode);
+  }
 
   if( expr->m_nToken == MS_TOKEN_COMPARISON_INTERSECTS &&
       expr->m_aoChildren.size() == 2 &&
