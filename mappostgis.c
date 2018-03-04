796e5f18e402b357b3f2c6a78828734abd3846fb
tlaczy <36709693+tlaczy@users.noreply.github.com>
2018-03-04 03:54:54 +0100

    Update mappostgis.c

--- mappostgis.c
+++ mappostgis.c
@@ -1380,18 +1380,18 @@ int msPostGISParseData(layerObj *layer)
  ***/
       for ( pos_uid = tmp + 7; *pos_uid == ' '; pos_uid++ );
     };
-/***
     if ( strncmp ( tmp, "srid=", 5 ) == 0 ) pos_srid = tmp + 5;
     {
+/***
       if ( pos_srid )
       {
         free ( data );
         msSetError(MS_QUERYERR, "Error parsing PostGIS DATA variable. Too many 'USING SRID' found! %s", "msPostGISParseData()", layer->data);
         return MS_FAILURE;
       };
+ ***/
       pos_srid = tmp + 5;
     };
- ***/
   };
 
   /*
