f6f4bf915bc4627ec76e111f55a50b94bd643523
Even Rouault <even.rouault@spatialys.com>
2018-06-18 15:45:06 +0200

    .travis.yml: update to Trusty

--- scripts/vagrant/packages.sh
+++ scripts/vagrant/packages.sh
@@ -13,8 +13,8 @@ apt-get -y upgrade
 # install packages we need
 apt-get install -q -y git build-essential pkg-config cmake libgeos-dev rake \
     libpq-dev python-all-dev libproj-dev libxml2-dev postgis php5-dev \
-    postgresql-server-dev-9.1 postgresql-9.1-postgis-2.1 vim bison flex swig \
+    postgresql-server-dev-9.3 postgresql-9.3-postgis-2.2 vim bison flex swig \
     librsvg2-dev libpng12-dev libjpeg-dev libgif-dev \
     libfreetype6-dev libfcgi-dev libcurl4-gnutls-dev libcairo2-dev \
     libgdal1-dev libfribidi-dev libexempi-dev \
-    libprotobuf-dev libprotobuf-c0-dev protobuf-c-compiler
+    libprotobuf-dev libprotobuf-c0-dev protobuf-c-compiler libharfbuzz-dev gdal-bin
