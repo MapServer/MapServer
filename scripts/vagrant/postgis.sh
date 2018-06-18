33a5a1209162b8d3e9addb975293a764703faeac
Even Rouault <even.rouault@spatialys.com>
2018-06-18 15:45:06 +0200

    .travis.yml: update to Trusty

--- scripts/vagrant/postgis.sh
+++ scripts/vagrant/postgis.sh
@@ -2,8 +2,8 @@
 
 cd /vagrant/msautotest
 
-sed -i  's/md5/trust/' /etc/postgresql/9.1/main/pg_hba.conf
-sed -i  's/peer/trust/' /etc/postgresql/9.1/main/pg_hba.conf
+sed -i  's/md5/trust/' /etc/postgresql/9.3/main/pg_hba.conf
+sed -i  's/peer/trust/' /etc/postgresql/9.3/main/pg_hba.conf
 
 service postgresql restart
 
