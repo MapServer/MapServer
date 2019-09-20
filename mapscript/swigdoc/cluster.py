0da1ec0e141ce9f80f0051cf399cb9fa0b75f36b
sethg <sethg@geographika.co.uk>
2019-09-21 00:48:06 +0200

    Add clusterObj docs

---
+++ mapscript/swigdoc/cluster.py
@@ -0,0 +1,8 @@
+#: `double`. See :mapfile:`BUFFER <cluster.html#index-4>`
+buffer = None
+
+#: `double`. See :mapfile:`MAXDISTANCE <cluster.html#index-2>`
+maxdistance = None
+
+#: `string`. See :mapfile:`REGION <cluster.html#index-3>`
+region = None
