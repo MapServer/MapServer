ead62a6efff6f53474b1a007483696ac212db3ba
Even Rouault <even.rouault@spatialys.com>
2020-01-11 12:11:02 +0100

    GetFeatureInfo: fix for layers using a lon_wrap PROJECTION and requested in the wrapped part of the raster

--- maprasterquery.c
+++ maprasterquery.c
@@ -415,6 +415,9 @@ msRasterQueryByRectLow(mapObj *map, layerObj *layer, GDALDatasetH hDS,
   CPLErr      eErr;
   rasterLayerInfo *rlinfo;
   rectObj     searchrect;
+#if PROJ_VERSION_MAJOR < 6
+  int         mayNeedLonWrapAdjustment = MS_FALSE;
+#endif
 
   rlinfo = (rasterLayerInfo *) layer->layerinfo;
 
@@ -551,6 +554,16 @@ msRasterQueryByRectLow(mapObj *map, layerObj *layer, GDALDatasetH hDS,
     + sqrt( rlinfo->range_dist );
   dfAdjustedRange = dfAdjustedRange * dfAdjustedRange;
 
+#if PROJ_VERSION_MAJOR < 6
+    if( layer->project &&
+        msProjIsGeographicCRS(&(layer->projection)) &&
+        msProjIsGeographicCRS(&(map->projection)) )
+    {
+        double dfLonWrap = 0;
+        mayNeedLonWrapAdjustment = msProjectHasLonWrap(&(layer->projection), &dfLonWrap);
+    }
+#endif
+
   /* -------------------------------------------------------------------- */
   /*      Loop over all pixels determining which are "in".                */
   /* -------------------------------------------------------------------- */
@@ -576,8 +589,23 @@ msRasterQueryByRectLow(mapObj *map, layerObj *layer, GDALDatasetH hDS,
       /* coordinates if we have a hit */
       sReprojectedPixelLocation = sPixelLocation;
       if( layer->project )
+      {
+#if PROJ_VERSION_MAJOR < 6
+        /* Works around a bug in PROJ < 6 when reprojecting from a lon_wrap */
+        /* geogCRS to a geogCRS, and the input abs(longitude) is > 180. Then */
+        /* lon_wrap was ignored and the output longitude remained as the source */
+        if( mayNeedLonWrapAdjustment )
+        {
+            if( rlinfo->target_point.x < sReprojectedPixelLocation.x - 180 )
+                sReprojectedPixelLocation.x -= 360;
+            else if( rlinfo->target_point.x > sReprojectedPixelLocation.x + 180 )
+                sReprojectedPixelLocation.x += 360;
+        }
+#endif
+
         msProjectPoint( &(layer->projection), &(map->projection),
                         &sReprojectedPixelLocation);
+      }
 
       /* If we are doing QueryByShape, check against the shape now */
       if( rlinfo->searchshape != NULL ) {
