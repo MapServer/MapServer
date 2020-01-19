cca274480bf55a0b7ab458077d21c544a9d09a0f
Even Rouault <even.rouault@spatialys.com>
2020-01-20 00:40:34 +0100

    vagrant: fixes to make it work

--- scripts/vagrant/packages.sh
+++ scripts/vagrant/packages.sh
@@ -8,11 +8,12 @@ apt-get update
 apt-get install -y python-software-properties
 add-apt-repository -y ppa:ubuntugis/ppa
 add-apt-repository -y ppa:ubuntugis/ubuntugis-testing
+add-apt-repository -y ppa:dluxen/cmake-backports
 apt-get update
 apt-get -y upgrade
 
 # install packages we need
-apt-get install -q -y git build-essential pkg-config cmake libgeos-dev rake \
+apt-get install -q -y git build-essential pkg-config cmake3 libgeos-dev rake \
     libpq-dev python-all-dev libproj-dev libxml2-dev postgis php5-dev \
     postgresql-server-dev-9.3 postgresql-9.3-postgis-2.2 vim bison flex swig \
     librsvg2-dev libpng12-dev libjpeg-dev libgif-dev \
--- scripts/vagrant/proj6.sh
+++ scripts/vagrant/proj6.sh
@@ -13,4 +13,4 @@ mv proj-6.1.1 vagrant-proj
 (cd vagrant-proj/data && curl -s http://download.osgeo.org/proj/proj-datumgrid-1.8.tar.gz > proj-datumgrid-1.8.tar.gz && tar xvzf proj-datumgrid-1.8.tar.gz)
 (cd vagrant-proj; CFLAGS='-DPROJ_RENAME_SYMBOLS -O2' CXXFLAGS='-DPROJ_RENAME_SYMBOLS -O2' ./configure --disable-static --prefix=/vagrant/install-vagrant-proj-6 && make -j $NUMTHREADS && make -j $NUMTHREADS install)
 cp -r /vagrant/install-vagrant-proj-6/include /vagrant/install-vagrant-proj-6/include_proj4_api_only
-rm /vagrant/install-vagrant-proj-6/include_proj4_api_only/proj.h
+rm -f /vagrant/install-vagrant-proj-6/include_proj4_api_only/proj.h
