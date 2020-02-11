91ef9a6e26a52d37fc2481514332ce0928db3d86
sethg <sethg@geographika.co.uk>
2020-02-11 12:39:58 +0100

    Clean up debug statements

--- msautotest/pymod/mstestlib.py
+++ msautotest/pymod/mstestlib.py
@@ -109,8 +109,6 @@ def has_requires( version_info, gdal_version, requires_list ):
             if compare_version(gdal_version, item[len('GDAL=='):]) != 0:
                 return 0
         elif version_info.find( item ) == -1:
-            print(version_info)
-            print(item)
             return 0
 
     return 1
