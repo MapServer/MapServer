375e943bb5ed3ef3c94883aace944dd03ff0559e
sethg <sethg@geographika.co.uk>
2020-05-17 00:03:57 +0200

    Remove submodule name from mapscript package

--- mapscript/python/mapscript/__init__.py
+++ mapscript/python/mapscript/__init__.py
@@ -1,1 +1,12 @@
+import inspect
 from .mapscript import *
+
+# change all the class module names from mapscript.mapscript to mapscript
+
+for key, value in globals().copy().items():
+    if inspect.isclass(value) and value.__module__.startswith('mapscript.'):
+        value.__module__= 'mapscript'
+
+# remove the submodule name
+
+del mapscript
