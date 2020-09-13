31aea4abfe1b7b501a6f2546f8b925eeadac2e04
sethg <sethg@geographika.co.uk>
2020-09-13 15:09:38 +0200

    Cleanup script

--- ci/travis/before_install.sh
+++ ci/travis/before_install.sh
@@ -8,33 +8,27 @@ sudo apt-get remove --purge postgresql* libpq-dev libpq5
 sudo add-apt-repository -y ppa:ubuntugis/ubuntugis-unstable
 sudo apt-get update
 sudo apt-get install --allow-unauthenticated protobuf-c-compiler libprotobuf-c0-dev bison flex libfribidi-dev cmake librsvg2-dev colordiff libpq-dev libpng-dev libjpeg-dev libgif-dev libgeos-dev libfreetype6-dev libfcgi-dev libcurl4-gnutls-dev libcairo2-dev libgdal-dev libproj-dev libxml2-dev libexempi-dev lcov lftp postgis libharfbuzz-dev gdal-bin ccache curl postgresql-server-dev-10 postgresql-10-postgis-3 postgresql-10-postgis-3-scripts swig
-#sudo apt-get install --allow-unauthenticated python3.7 python3-dev python3-pip python3-lxml python3-setuptools python3-pyflakes
+# following are already installed on Travis CI
+#sudo apt-get install --allow-unauthenticated php-dev python-dev python3-dev
 sudo apt-get install --allow-unauthenticated libmono-system-drawing4.0-cil mono-mcs
-#sudo apt-get install --allow-unauthenticated php-dev
 sudo apt-get install --allow-unauthenticated libperl-dev
 
-pyenv which pip
-pyenv which python
-
-echo $(pyenv root)
 # list installed and available Python versions
-pyenv versions
-# sudo ls $(pyenv root)/versions/
+# pyenv versions
+# echo $(pyenv root)
 
 # set the global Python version
 pyenv global $PYTHON_VERSION
 
+# check we are using the correct versions
 pyenv which pip
 pyenv which python
 
-pip install cpp-coveralls
-pip install pyflakes
+pip install cpp-coveralls pyflakes lxml
+pip install -r msautotest/requirements.txt
 
-pyenv which pyflakes
 export CC="ccache gcc"
 export CXX="ccache g++"
-
-pip install -r msautotest/requirements.txt
 
 sudo sed -i  's/md5/trust/' /etc/postgresql/10/main/pg_hba.conf
 sudo sed -i  's/peer/trust/' /etc/postgresql/10/main/pg_hba.conf
@@ -47,7 +41,8 @@ python -m pyflakes .
 if [ $PYTHON_VERSION = "2.7" ]; then
     python -m SimpleHTTPServer &> /dev/null &
 else
-    python3 -m http.server &> /dev/null &
+    # py3
+    python -m http.server &> /dev/null &
 fi
 
 cd ..
