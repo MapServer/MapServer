255f6dac8f95098763f852f4c1f31ea10a65b7cc
sethg <sethg@geographika.co.uk>
2021-03-07 01:00:24 +0100

    Switch back def order

--- maperror.h
+++ maperror.h
@@ -110,14 +110,14 @@ Instances of errorObj are created internally by MapServer as errors happen.
 Errors are managed as a chained list with the first item being the most recent error.
 */
   typedef struct errorObj {
-#ifndef SWIG
-      struct errorObj *next;
-#endif
     int code; ///< MapServer error code such as :data:`MS_IMGERR`
     char routine[ROUTINELENGTH]; ///< MapServer function in which the error was set
     char message[MESSAGELENGTH]; ///< Context-dependent error message
     int isreported; ///< :data:`MS_TRUE` or :data:`MS_FALSE` flag indicating if the error has been output
     int errorcount; ///< Number of subsequent errors
+#ifndef SWIG
+    struct errorObj *next;
+#endif
   } errorObj;
 
   /*
