b5d02c55f141e5617c57164589a9e79b675b980a
Seth G <sethg@geographika.co.uk>
2021-04-15 14:52:11 +0200

    Only select requested fields for all WFS query types (#6288)

--- maplayer.c
+++ maplayer.c
@@ -990,6 +990,15 @@ int msLayerWhichItems(layerObj *layer, int get_all, const char *metadata)
       get_all = MS_FALSE;
   }
 
+  if(metadata == NULL){
+      // check item set by mapwfs.cpp to restrict the number of columns selected
+      metadata = msOWSLookupMetadata(&(layer->metadata), "G", "select_items");
+      if (metadata) {
+          /* get only selected items */
+          get_all = MS_FALSE;
+      }
+  }
+
   /* always retrieve all items in some cases */
   if(layer->connectiontype == MS_INLINE ||
       (layer->map->outputformat && layer->map->outputformat->renderer == MS_RENDER_WITH_KML)) {
