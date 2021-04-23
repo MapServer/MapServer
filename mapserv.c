3c3c9b3934f42808c15957f9378dec904203228d
Seth G <sethg@geographika.co.uk>
2021-04-23 10:20:22 +0200

    Use CPLSetConfigOption/CPLGetConfigOption for some CGI/FastCGI-related env vars (#6305)
    Push a few high-value env vars into CPL config and then reference that instead of the env (mostly for IIS/FastCGI).

--- mapserv.c
+++ mapserv.c
@@ -36,6 +36,8 @@
 #include "mapio.h"
 #include "maptime.h"
 
+#include "cpl_conv.h"
+
 #ifndef WIN32
 #include <signal.h>
 #endif
@@ -155,6 +157,15 @@ int main(int argc, char *argv[])
 
   if(msGetGlobalDebugLevel() >= MS_DEBUGLEVEL_TUNING)
     msGettimeofday(&execstarttime, NULL);
+
+  /* push high-value ENV vars into the CPL global config - primarily for IIS/FastCGI */
+  const char* const apszEnvVars[] = { 
+    "CURL_CA_BUNDLE", "MS_MAPFILE", "MS_MAP_NO_PATH", "MS_MAP_PATTERN",
+     NULL /* guard */ };
+  for( int i = 0; apszEnvVars[i] != NULL; ++i ) {
+    const char* value = getenv(apszEnvVars[i]);
+    if(value) CPLSetConfigOption(apszEnvVars[i], value);
+  }
 
   /* -------------------------------------------------------------------- */
   /*      Process arguments.  In normal use as a cgi-bin there are no     */
