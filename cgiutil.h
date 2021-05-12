d45d7ce54d6e27c6c393d498d70f798fcae5fb42
Steve Lime <steve.lime@state.mn.us>
2021-05-12 07:28:05 -0500

    Hide api-related additions to cgiRequestObj from SWIG.

--- cgiutil.h
+++ cgiutil.h
@@ -71,10 +71,11 @@ typedef struct {
 
   char *httpcookiedata;
 
-  // todo, SWIG?
-  char *path_info;
+#ifndef SWIG
+  const char *path_info;
   int api_path_length;
   char **api_path;
+#endif
 
 } cgiRequestObj;
 
