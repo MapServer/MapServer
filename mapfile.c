9d086cacc2df243d6ea900f0d6785cd0417a34a5
Seth G <sethg@geographika.co.uk>
2021-05-14 21:01:38 +0200

    Removal of styling keywords from a Mapfile CLASS (#6317)
    * Ensure CLASS styling is in STYLE blocks
    
    * Remove styling from CLASS blocks
    
    * Remove unused variable
    
    * Update PHP test maps
    
    * Remove CLASS OVERLAY.. keywords from lexer and mapfile.h

--- mapfile.c
+++ mapfile.c
@@ -3179,7 +3179,6 @@ labelObj *msGrowClassLabels( classObj *class )
 
 int loadClass(classObj *class, layerObj *layer)
 {
-  int state;
   mapObj *map=NULL;
 
   class->layer = (layerObj *) layer;
@@ -3310,90 +3309,6 @@ int loadClass(classObj *class, layerObj *layer)
             return(-1);
           }
         }
-        break;
-
-        /*
-        ** for backwards compatability, these are shortcuts for style 0
-        */
-      case(BACKGROUNDCOLOR):
-        if (msMaybeAllocateClassStyle(class, 0)) return MS_FAILURE;
-        if(loadColor(&(class->styles[0]->backgroundcolor), NULL) != MS_SUCCESS) return(-1);
-        break;
-      case(COLOR):
-        if (msMaybeAllocateClassStyle(class, 0)) return MS_FAILURE;
-        if(loadColor(&(class->styles[0]->color), NULL) != MS_SUCCESS) return(-1);
-        class->numstyles = 1; /* must *always* set a color or outlinecolor */
-        break;
-      case(MAXSIZE):
-        if (msMaybeAllocateClassStyle(class, 0)) return MS_FAILURE;
-        if(getDouble(&(class->styles[0]->maxsize)) == -1) return(-1);
-        break;
-      case(MINSIZE):
-        if (msMaybeAllocateClassStyle(class, 0)) return MS_FAILURE;
-        if(getDouble(&(class->styles[0]->minsize)) == -1) return(-1);
-        break;
-      case(OUTLINECOLOR):
-        if (msMaybeAllocateClassStyle(class, 0)) return MS_FAILURE;
-        if(loadColor(&(class->styles[0]->outlinecolor), NULL) != MS_SUCCESS) return(-1);
-        class->numstyles = 1; /* must *always* set a color, symbol or outlinecolor */
-        break;
-      case(SIZE):
-        if (msMaybeAllocateClassStyle(class, 0)) return MS_FAILURE;
-        if(getDouble(&(class->styles[0]->size)) == -1) return(-1);
-        break;
-      case(SYMBOL):
-        if (msMaybeAllocateClassStyle(class, 0)) return MS_FAILURE;
-        if((state = getSymbol(2, MS_NUMBER,MS_STRING)) == -1) return(-1);
-        if(state == MS_NUMBER)
-          class->styles[0]->symbol = (int) msyynumber;
-        else {
-          if (class->styles[0]->symbolname != NULL)
-            msFree(class->styles[0]->symbolname);
-          class->styles[0]->symbolname = msStrdup(msyystring_buffer);
-          class->numstyles = 1;
-        }
-        break;
-
-        /*
-        ** for backwards compatability, these are shortcuts for style 1
-        */
-      case(OVERLAYBACKGROUNDCOLOR):
-        if (msMaybeAllocateClassStyle(class, 1)) return MS_FAILURE;
-        if(loadColor(&(class->styles[1]->backgroundcolor), NULL) != MS_SUCCESS) return(-1);
-        break;
-      case(OVERLAYCOLOR):
-        if (msMaybeAllocateClassStyle(class, 1)) return MS_FAILURE;
-        if(loadColor(&(class->styles[1]->color), NULL) != MS_SUCCESS) return(-1);
-        class->numstyles = 2; /* must *always* set a color, symbol or outlinecolor */
-        break;
-      case(OVERLAYMAXSIZE):
-        if (msMaybeAllocateClassStyle(class, 1)) return MS_FAILURE;
-        if(getDouble(&(class->styles[1]->maxsize)) == -1) return(-1);
-        break;
-      case(OVERLAYMINSIZE):
-        if (msMaybeAllocateClassStyle(class, 1)) return MS_FAILURE;
-        if(getDouble(&(class->styles[1]->minsize)) == -1) return(-1);
-        break;
-      case(OVERLAYOUTLINECOLOR):
-        if (msMaybeAllocateClassStyle(class, 1)) return MS_FAILURE;
-        if(loadColor(&(class->styles[1]->outlinecolor), NULL) != MS_SUCCESS) return(-1);
-        class->numstyles = 2; /* must *always* set a color, symbol or outlinecolor */
-        break;
-      case(OVERLAYSIZE):
-        if (msMaybeAllocateClassStyle(class, 1)) return MS_FAILURE;
-        if(getDouble(&(class->styles[1]->size)) == -1) return(-1);
-        break;
-      case(OVERLAYSYMBOL):
-        if (msMaybeAllocateClassStyle(class, 1)) return MS_FAILURE;
-        if((state = getSymbol(2, MS_NUMBER,MS_STRING)) == -1) return(-1);
-        if(state == MS_NUMBER)
-          class->styles[1]->symbol = (int) msyynumber;
-        else  {
-          if (class->styles[1]->symbolname != NULL)
-            msFree(class->styles[1]->symbolname);
-          class->styles[1]->symbolname = msStrdup(msyystring_buffer);
-        }
-        class->numstyles = 2;
         break;
 
       case(VALIDATION):
