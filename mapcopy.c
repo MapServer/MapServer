6fed096e7241be7eb406b910adddf00793a73166
sethg <sethg@geographika.co.uk>
2021-05-23 13:38:09 +0200

    Remove web.extent from copying

--- mapcopy.c
+++ mapcopy.c
@@ -443,8 +443,6 @@ int msCopyWeb(webObj *dst, const webObj *src, mapObj *map)
   MS_COPYSTRING(dst->empty, src->empty);
   MS_COPYSTRING(dst->error, src->error);
 
-  MS_COPYRECT(&(dst->extent), &(src->extent));
-
   MS_COPYSTELEM(minscaledenom);
   MS_COPYSTELEM(maxscaledenom);
   MS_COPYSTRING(dst->mintemplate, src->mintemplate);
