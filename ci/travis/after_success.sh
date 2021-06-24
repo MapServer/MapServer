5d5d996ecd287cf9f5f6d7464b0b51aff32c161e
jmckenna <jmckenna@gatewaygeomatics.com>
2021-06-24 13:54:10 -0300

    attempt to get Travis working

--- ci/travis/after_success.sh
+++ ci/travis/after_success.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 set -eu
 
-if [ "$BUILD_NAME" != "PHP_7.2_WITH_ASAN" ]; then
-    # Only run coverage when it is safe to do so (not on pull requests), and only on master branch
+if [ "$BUILD_NAME" != "PHP_7.3_WITH_ASAN" ]; then
+    # Only run coverage when it is safe to do so (not on pull requests), and only on main branch
     echo "$TRAVIS_SECURE_ENV_VARS"
     echo "$TRAVIS_BRANCH"
-    sh -c 'if test "$TRAVIS_SECURE_ENV_VARS" = "true" -a "$TRAVIS_BRANCH" = "master"; then echo "run coverage"; ./run_code_coverage_upload.sh; fi'
+    sh -c 'if test "$TRAVIS_SECURE_ENV_VARS" = "true" -a "$TRAVIS_BRANCH" = "main"; then echo "run coverage"; ./run_code_coverage_upload.sh; fi'
     ln -s ../../../mapparser.y build/CMakeFiles/mapserver.dir/
     ln -s ../../../maplexer.l build/CMakeFiles/mapserver.dir/
     coveralls --exclude renderers --exclude mapscript --exclude apache --exclude build/mapscript/mapscriptJAVA_wrap.c --exclude build/mapscript/mapscriptPYTHON_wrap.c --exclude shp2img.c --exclude legend.c --exclude scalebar.c --exclude msencrypt.c --exclude sortshp.c --exclude shptreevis.c --exclude shptree.c --exclude testexpr.c --exclude sym2img.c --exclude testcopy.c --exclude shptreetst.c --exclude tile4ms.c --exclude proj --exclude swig-3.0.12 --extension .c --extension .cpp
--- ci/travis/before_install.sh
+++ ci/travis/before_install.sh
@@ -1,31 +1,42 @@
 #!/bin/sh
 set -eu
 
-sudo mv /etc/apt/sources.list.d/pgdg* /tmp
-dpkg -l | grep postgresql
-dpkg -l | grep postgis
-sudo apt-get remove postgresql*
-sudo add-apt-repository -y ppa:ubuntugis/ppa
-sudo add-apt-repository -y ppa:ubuntugis/ubuntugis-testing
+# Remove pre-installed things in Travis image
+if ls /etc/apt/sources.list.d/pgdg* 2>/dev/null >/dev/null; then sudo mv /etc/apt/sources.list.d/pgdg* /tmp; fi
+dpkg -l | grep postgresql || /bin/true
+dpkg -l | grep postgis || /bin/true
+sudo apt-get remove --purge postgresql* libpq-dev libpq5 || /bin/true
+
+sudo add-apt-repository -y ppa:ubuntugis/ubuntugis-unstable
 sudo apt-get update
-sudo apt-get install --allow-unauthenticated protobuf-c-compiler libprotobuf-c0-dev bison flex python-lxml libfribidi-dev cmake librsvg2-dev colordiff libpq-dev libpng12-dev libjpeg-dev libgif-dev libgeos-dev libgd2-xpm-dev libfreetype6-dev libfcgi-dev libcurl4-gnutls-dev libcairo2-dev libgdal1-dev libproj-dev libxml2-dev python-dev libexempi-dev lcov lftp postgis libharfbuzz-dev gdal-bin ccache curl pyflakes
-sudo apt-get install --allow-unauthenticated libmono-system-drawing4.0-cil mono-mcs
-sudo apt-get install --allow-unauthenticated php5-dev || sudo apt-get install --allow-unauthenticated php7-dev
-sudo apt-get install --allow-unauthenticated libperl-dev
-sudo pip install cpp-coveralls
+sudo apt-get install -y --allow-unauthenticated protobuf-c-compiler libprotobuf-c0-dev bison flex libfribidi-dev cmake librsvg2-dev colordiff libpq-dev libpng-dev libjpeg-dev libgif-dev libgeos-dev libfreetype6-dev libfcgi-dev libcurl4-gnutls-dev libcairo2-dev libgdal-dev libproj-dev libxml2-dev libexempi-dev lcov lftp postgis libharfbuzz-dev gdal-bin ccache curl postgresql-server-dev-10 postgresql-10-postgis-3 postgresql-10-postgis-3-scripts swig g++
+# following are already installed on Travis CI
+#sudo apt-get install --allow-unauthenticated php-dev python-dev python3-dev
+sudo apt-get install -y --allow-unauthenticated libmono-system-drawing4.0-cil mono-mcs
+sudo apt-get install -y --allow-unauthenticated libperl-dev
+sudo apt-get install -y --allow-unauthenticated openjdk-8-jdk
+
+pip install cpp-coveralls pyflakes lxml
 sudo pip install -U -r msautotest/requirements.txt
-# install swig 3.0.12 (defaults to 2.0.11 on trusty)
-wget http://prdownloads.sourceforge.net/swig/swig-3.0.12.tar.gz
+
 export CC="ccache gcc"
 export CXX="ccache g++"
-tar xf swig-3.0.12.tar.gz
-cd swig-3.0.12 && ./configure --prefix=/usr && make -j2 && sudo make install
-swig -version
-cd ..
+
+sudo sed -i  's/md5/trust/' /etc/postgresql/10/main/pg_hba.conf
+sudo sed -i  's/peer/trust/' /etc/postgresql/10/main/pg_hba.conf
+sudo service postgresql restart 10
+
 cd msautotest
-pyflakes .
+python -m pyflakes .
 ./create_postgis_test_data.sh
-python -m SimpleHTTPServer &> /dev/null &
+
+if [ $PYTHON_VERSION = "2.7" ]; then
+    python -m SimpleHTTPServer &> /dev/null &
+else
+    # py3
+    python -m http.server &> /dev/null &
+fi
+
 cd ..
 touch maplexer.l
 touch mapparser.y
