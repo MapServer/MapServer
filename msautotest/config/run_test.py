b16ac2a46d6e7f3c808be98ce11109862bc1253b
Steve Lime <steve.lime@state.mn.us>
2022-02-15 19:06:52 -0600

    Added configuration file tests.

---
+++ msautotest/config/run_test.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+###############################################################################
+# $Id$
+#
+# Project:  MapServer
+# Purpose:  Test harnass script for MapServer autotest.
+# Author:   Frank Warmerdam, warmerdam@pobox.com
+#
+###############################################################################
+#  Copyright (c) 2002, Frank Warmerdam <warmerdam@pobox.com>
+# 
+#  Permission is hereby granted, free of charge, to any person obtaining a
+#  copy of this software and associated documentation files (the "Software"),
+#  to deal in the Software without restriction, including without limitation
+#  the rights to use, copy, modify, merge, publish, distribute, sublicense,
+#  and/or sell copies of the Software, and to permit persons to whom the
+#  Software is furnished to do so, subject to the following conditions:
+# 
+#  The above copyright notice and this permission notice shall be included
+#  in all copies or substantial portions of the Software.
+# 
+#  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+#  OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+#  THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+#  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+#  DEALINGS IN THE SOFTWARE.
+###############################################################################
+
+import os
+import pytest
+import sys
+
+sys.path.append( '../pymod' )
+
+import mstestlib
+
+@pytest.mark.parametrize('map,out_file,command', mstestlib.get_pytests(os.path.dirname(os.path.abspath(__file__))))
+def test(map, out_file, command, extra_args):
+    mstestlib.run_pytest(map, out_file, command, extra_args)
+
+
+###############################################################################
+# main()
+
+if __name__ == '__main__':
+    sys.exit(mstestlib.pytest_main())
