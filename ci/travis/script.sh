24eedb5adfaea152bba873f02d661c5969b4f355
jmckenna <jmckenna@gatewaygeomatics.com>
2022-03-13 11:44:50 -0300

    upgrade TravisCI tests for PHP8

--- ci/travis/script.sh
+++ ci/travis/script.sh
@@ -26,7 +26,7 @@ cd ../
 #check SWIG version
 swig -version
 
-if [ "$BUILD_NAME" = "PHP_7.2_WITH_ASAN" ]; then
+if [ "$BUILD_NAME" = "PHP_7.4_WITH_ASAN" ]; then
     # Force use of PROJ 4 API
     sudo rm /usr/include/proj.h
     # -DNDEBUG to avoid issues with cairo cleanup
@@ -34,7 +34,7 @@ if [ "$BUILD_NAME" = "PHP_7.2_WITH_ASAN" ]; then
     export AUTOTEST_OPTS="--strict --run_under_asan"
     # Only run tests that only involve mapserv/map2img binaries. mspython, etc would require LD_PREOLOAD'ing the asan shared object
     make -j4 asan_compatible_tests
-elif [ "$BUILD_NAME" = "PHP_7.3_WITH_PROJ7" ]; then
+elif [ "$BUILD_NAME" = "PHP_8.0_WITH_PROJ7" ]; then
     make cmakebuild MFLAGS="-j2" CMAKE_C_FLAGS="-O2" CMAKE_CXX_FLAGS="-O2" LIBMAPSERVER_EXTRA_FLAGS="-Wall -Werror -Wextra"
     make mspython-wheel
     make phpng-build
