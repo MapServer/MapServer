de3eb1a6b024e481c5e30facd315c4228e33c716
jmckenna <jmckenna@gatewaygeomatics.com>
2022-03-13 13:40:22 -0300

    upgrade TravisCI tests for PHP8

--- ci/travis/script.sh
+++ ci/travis/script.sh
@@ -27,22 +27,13 @@ cd ../
 swig -version
 
 if [ "$BUILD_NAME" = "PHP_7.4_WITH_ASAN" ]; then
-    # Force use of PROJ 4 API
-    sudo rm /usr/include/proj.h
     # -DNDEBUG to avoid issues with cairo cleanup
-    make cmakebuild MFLAGS="-j2" CMAKE_C_FLAGS="-g -fsanitize=address -DNDEBUG -DACCEPT_USE_OF_DEPRECATED_PROJ_API_H" CMAKE_CXX_FLAGS="-g -fsanitize=address -DNDEBUG -DACCEPT_USE_OF_DEPRECATED_PROJ_API_H" EXTRA_CMAKEFLAGS="-DCMAKE_BUILD_TYPE=None -DCMAKE_EXE_LINKER_FLAGS=-fsanitize=address"
+    make cmakebuild MFLAGS="-j2" CMAKE_C_FLAGS="-g -fsanitize=address -DNDEBUG" CMAKE_CXX_FLAGS="-g -fsanitize=address -DNDEBUG" EXTRA_CMAKEFLAGS="-DCMAKE_BUILD_TYPE=None -DCMAKE_EXE_LINKER_FLAGS=-fsanitize=address"
     export AUTOTEST_OPTS="--strict --run_under_asan"
     # Only run tests that only involve mapserv/map2img binaries. mspython, etc would require LD_PREOLOAD'ing the asan shared object
     make -j4 asan_compatible_tests
-elif [ "$BUILD_NAME" = "PHP_8.0_WITH_PROJ8" ]; then
-    make cmakebuild MFLAGS="-j2" CMAKE_C_FLAGS="-O2" CMAKE_CXX_FLAGS="-O2" LIBMAPSERVER_EXTRA_FLAGS="-Wall -Werror -Wextra"
-    make mspython-wheel
-    make phpng-build
-    make -j4 test
 else
-    # Force use of PROJ 4 API
-    sudo rm /usr/include/proj.h
-    make cmakebuild MFLAGS="-j2" CMAKE_C_FLAGS="-DACCEPT_USE_OF_DEPRECATED_PROJ_API_H" CMAKE_CXX_FLAGS="-DACCEPT_USE_OF_DEPRECATED_PROJ_API_H" LIBMAPSERVER_EXTRA_FLAGS="-Wall -Werror -Wextra"
+    make cmakebuild MFLAGS="-j2" CMAKE_C_FLAGS="-O2" CMAKE_CXX_FLAGS="-O2" LIBMAPSERVER_EXTRA_FLAGS="-Wall -Werror -Wextra"
     make mspython-wheel
     make phpng-build
     make -j4 test
