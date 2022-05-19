b1b572f8fe969ebb2124845cb6569a1dc90fc7c7
jmckenna <jmckenna@gatewaygeomatics.com>
2022-05-19 17:51:18 -0300

    install latest cmake

--- ci/travis/script.sh
+++ ci/travis/script.sh
@@ -30,6 +30,11 @@ swig -version
 export PATH=${TRAVIS_BUILD_DIR}/deps/cmake-install:${TRAVIS_BUILD_DIR}/deps/cmake-install/bin:$PATH
 cmake --version
 
+#check for python-config
+which python
+which python-config
+python-config --cflag
+
 if [ "$BUILD_NAME" = "PHP_7.4_WITH_ASAN" ]; then
     # -DNDEBUG to avoid issues with cairo cleanup
     make cmakebuild MFLAGS="-j2" CMAKE_C_FLAGS="-g -fsanitize=address -DNDEBUG" CMAKE_CXX_FLAGS="-g -fsanitize=address -DNDEBUG" EXTRA_CMAKEFLAGS="-DCMAKE_BUILD_TYPE=None -DCMAKE_EXE_LINKER_FLAGS=-fsanitize=address"
