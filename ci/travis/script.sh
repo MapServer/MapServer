024c584688e3d9f52f160019912fe0a15b098fda
jmckenna <jmckenna@gatewaygeomatics.com>
2022-05-23 10:55:26 -0300

    update GitHub build action

--- ci/travis/script.sh
+++ ci/travis/script.sh
@@ -36,7 +36,7 @@ if [ "$BUILD_NAME" = "PHP_7.4_WITH_ASAN" ]; then
     make -j4 asan_compatible_tests
 elif [ "$BUILD_NAME" = "PHP_7.4_WITH_PROJ8" ]; then
     #runs through GitHub action
-    make cmakebuild MFLAGS="-j2" CMAKE_C_FLAGS="-O2" CMAKE_CXX_FLAGS="-O2" LIBMAPSERVER_EXTRA_FLAGS="-Wall -Werror -Wextra -Wno-stringop-overflow"
+    make cmakebuild MFLAGS="-j2" CMAKE_C_FLAGS="-O2" CMAKE_CXX_FLAGS="-O2" LIBMAPSERVER_EXTRA_FLAGS="-Wall -Werror -Wextra -Wno-stringop-overflow -Wno-format-overflow"
     make mspython-wheel
     make phpng-build    
     make -j4 test
