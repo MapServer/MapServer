c9890c62f9108962e7698ccd7907ea44968e997b
jmckenna <jmckenna@gatewaygeomatics.com>
2022-05-23 09:26:07 -0300

    update GitHub build action

--- .github/workflows/start.sh
+++ .github/workflows/start.sh
@@ -34,7 +34,7 @@ pip install --upgrade pip
 
 # Install recent cmake
 wget -O - https://apt.kitware.com/keys/kitware-archive-latest.asc 2>/dev/null | gpg --dearmor - | sudo tee /etc/apt/trusted.gpg.d/kitware.gpg >/dev/null
-sudo apt-add-repository 'deb https://apt.kitware.com/ubuntu/ bionic main'
+sudo apt-add-repository 'deb https://apt.kitware.com/ubuntu/ focal main'
 
 cd "$WORK_DIR"
 
