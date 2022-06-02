512dbed35b95a7c2a197738b52fea7cf3f1c9cc2
Even Rouault <even.rouault@spatialys.com>
2022-06-02 22:58:25 +0200

    Vagrantfile: update to ubuntu 20.04

--- scripts/vagrant/postgis.sh
+++ scripts/vagrant/postgis.sh
@@ -2,9 +2,9 @@
 
 cd /vagrant/msautotest
 
-sed -i  's/md5/trust/' /etc/postgresql/10/main/pg_hba.conf
-sed -i  's/peer/trust/' /etc/postgresql/10/main/pg_hba.conf
+sed -i  's/md5/trust/' /etc/postgresql/12/main/pg_hba.conf
+sed -i  's/peer/trust/' /etc/postgresql/12/main/pg_hba.conf
 
-service postgresql restart
+service postgresql restart 12
 
 ./create_postgis_test_data.sh
