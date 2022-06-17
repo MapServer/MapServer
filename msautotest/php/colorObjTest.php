b9707fb8e867e89495606ea1547c5ca0128f1eb2
Jeff McKenna <jmckenna@gatewaygeomatics.com>
2022-06-17 15:46:50 -0300

    re-enable colorObj tests

--- msautotest/php/colorObjTest.php
+++ msautotest/php/colorObjTest.php
@@ -17,50 +17,48 @@ class colorObjTest extends \PHPUnit\Framework\TestCase
     public function test__getsetAlpha()
     {
         $this->assertEquals(50, $this->color->alpha=50);
-		$this->color->alpha=$this->originalAlpha;
+	$this->color->alpha=$this->originalAlpha;
     }
 
     public function test__toHex()
     {
-        # Test fails on ubuntu 20.04
+	# Test fails on ubuntu 20.04
         #$this->assertEquals("#00ff00", $this->color->toHex());
-		#$this->color->alpha=128;
-		#$this->assertEquals("#00ff0080", $this->color->toHex());
-		#$this->color->alpha = $this->originalAlpha;
-	}
+	#$this->color->alpha=128;
+	#$this->assertEquals("#00ff0080", $this->color->toHex());
+	#$this->color->alpha = $this->originalAlpha;
+    }
 
-        #causes random Segmentation Fault with PHPNG
-	#public function test__setHex()
-	#{
-		#$this->color->alpha = 80;
-		#$this->assertEquals(0, $this->color->red);
-		#$this->assertEquals(80, $this->color->alpha);
-		#$this->assertEquals(MS_SUCCESS, $this->color->setHex("#ff00ff"));
-		#$this->assertEquals(255, $this->color->red);
-		#$this->assertEquals(255, $this->color->alpha);
-		#$this->assertEquals(MS_SUCCESS, $this->color->setHex("#ff00ff80"));
-		#$this->assertEquals(128, $this->color->alpha);
-		#$this->color->setHex($this->original);
-	#}
+    public function test__setHex()
+    {
+        $this->color->alpha = 80;
+	$this->assertEquals(0, $this->color->red);
+	$this->assertEquals(80, $this->color->alpha);
+	$this->assertEquals(MS_SUCCESS, $this->color->setHex("#ff00ff"));
+	$this->assertEquals(255, $this->color->red);
+	$this->assertEquals(255, $this->color->alpha);
+	$this->assertEquals(MS_SUCCESS, $this->color->setHex("#ff00ff80"));
+	$this->assertEquals(128, $this->color->alpha);
+	$this->color->setHex($this->original);
+    }
 
-	#causes random Segmentation Fault with PHPNG
-	#public function test__setRGB()
-	#{
-		#$this->color->alpha = 80;
-		#$this->assertEquals(0, $this->color->red);
-		#$this->assertEquals(80, $this->color->alpha);
-		#$this->assertEquals(MS_SUCCESS, $this->color->setRGB(255, 0, 255));
-		#$this->assertEquals(255, $this->color->red);
-		#$this->assertEquals(255, $this->color->alpha);
-		#$this->color->setHex($this->original);
-		#$this->assertEquals(0, $this->color->red);
-		#$this->assertEquals(255, $this->color->alpha);
-		#$this->assertEquals(MS_SUCCESS, $this->color->setRGB(255, 0, 255, 80));
-		#$this->assertEquals(255, $this->color->red);
-		#$this->assertEquals(80, $this->color->alpha);
-		#$this->color->setHex($this->original);
-		#$this->color->alpha = $this->originalAlpha;
-	#}
+    public function test__setRGB()
+    {
+        $this->color->alpha = 80;
+	$this->assertEquals(0, $this->color->red);
+        $this->assertEquals(80, $this->color->alpha);
+        $this->assertEquals(MS_SUCCESS, $this->color->setRGB(255, 0, 255));
+        $this->assertEquals(255, $this->color->red);
+        $this->assertEquals(255, $this->color->alpha);
+        $this->color->setHex($this->original);
+        $this->assertEquals(0, $this->color->red);
+        $this->assertEquals(255, $this->color->alpha);
+        $this->assertEquals(MS_SUCCESS, $this->color->setRGB(255, 0, 255, 80));
+        $this->assertEquals(255, $this->color->red);
+        $this->assertEquals(80, $this->color->alpha);
+	$this->color->setHex($this->original);
+	$this->color->alpha = $this->originalAlpha;
+    }
     
     # destroy variables, if not can lead to segmentation fault
     public function tearDown(): void {
