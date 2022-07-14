ffdbfd00cbab242743685a867c8984984ed8df69
Björn Harrtell <bjorn@wololo.org>
2022-07-14 17:17:56 +0200

    Fix up buffer handling

--- mapskia.cpp
+++ mapskia.cpp
@@ -23,8 +23,9 @@ imageObj *skia2CreateImage(int width, int height, outputFormatObj *format, color
     image = (imageObj *) calloc(1, sizeof (imageObj));
     MS_CHECK_ALLOC(image, sizeof (imageObj), NULL);
     SkAlphaType alpha = format->transparent ? SkAlphaType::kPremul_SkAlphaType : SkAlphaType::kOpaque_SkAlphaType;
-    SkImageInfo imageInfo = SkImageInfo::MakeN32(width, height, alpha);
-    SkSurface *s = SkSurface::MakeRaster(imageInfo).get();
+    SkColorType ct = format->transparent ? SkColorType::kRGBA_8888_SkColorType : SkColorType::kRGB_888x_SkColorType;
+    SkImageInfo imageInfo = SkImageInfo::Make(width, height, ct, alpha);
+    SkSurface *s = SkSurface::MakeRaster(imageInfo).release();
     image->img.plugin = (void*) s;
   } else {
     msSetError(MS_RENDERERERR, "Cannot create Skia surface of size %dx%d.",
@@ -63,14 +64,64 @@ int skia2RenderPolygon(imageObj *image, shapeObj *p, colorObj * color)
   return MS_SUCCESS;
 }
 
+int skiaInitializeRasterBuffer(rasterBufferObj *rb, int width, int height, int mode)
+{
+  rb->type = MS_BUFFER_BYTE_RGBA;
+  rb->data.rgba.pixel_step = 4;
+  rb->data.rgba.row_step = rb->data.rgba.pixel_step * width;
+  rb->width = width;
+  rb->height = height;
+  int nBytes = rb->data.rgba.row_step * height;
+  rb->data.rgba.pixels = (unsigned char*)msSmallCalloc(nBytes,sizeof(unsigned char));
+  rb->data.rgba.r = &(rb->data.rgba.pixels[0]);
+  rb->data.rgba.g = &(rb->data.rgba.pixels[1]);
+  rb->data.rgba.b = &(rb->data.rgba.pixels[2]);
+  if(mode == MS_IMAGEMODE_RGBA)
+    rb->data.rgba.a = &(rb->data.rgba.pixels[3]);
+  return MS_SUCCESS;
+}
+
+int skiaGetRasterBufferHandle(imageObj *image, rasterBufferObj * rb)
+{
+  SkSurface *s = (SkSurface *) image->img.plugin;
+  SkPixmap p;
+  s->peekPixels(&p);
+  rb->type =MS_BUFFER_BYTE_RGBA;
+  rb->data.rgba.pixels = (unsigned char *) p.addr();
+  rb->data.rgba.row_step = p.rowBytes();
+  rb->data.rgba.pixel_step = 4;
+  rb->width = s->width();
+  rb->height = s->height();
+  rb->data.rgba.r = &(rb->data.rgba.pixels[0]);
+  rb->data.rgba.g = &(rb->data.rgba.pixels[1]);
+  rb->data.rgba.b = &(rb->data.rgba.pixels[2]);
+  if(!s->imageInfo().isOpaque())
+    rb->data.rgba.a = &(rb->data.rgba.pixels[3]);
+  else
+    rb->data.rgba.a = NULL;
+  return MS_SUCCESS;
+}
+
+int skiaGetRasterBufferCopy(imageObj *image, rasterBufferObj *rb)
+{
+  SkSurface *s = (SkSurface *) image->img.plugin;
+  SkPixmap p;
+  s->peekPixels(&p);
+  skiaInitializeRasterBuffer(rb, image->width, image->height, MS_IMAGEMODE_RGBA);
+  int nBytes = p.rowBytes() * p.height();
+  memcpy(rb->data.rgba.pixels, p.addr(), nBytes);
+  return MS_FAILURE;
+}
+
 int msPopulateRendererVTableSkia(rendererVTableObj * renderer)
 {
-  /*renderer->compositeRasterBuffer = &aggCompositeRasterBuffer;
   renderer->supports_pixel_buffer = 1;
   renderer->use_imagecache = 0;
   renderer->supports_clipping = 0;
   renderer->supports_svg = 0;
   renderer->default_transform_mode = MS_TRANSFORM_SIMPLIFY;
+
+  /*renderer->compositeRasterBuffer = &aggCompositeRasterBuffer;
   agg2InitCache(&(MS_RENDERER_CACHE(renderer)));
   renderer->cleanup = agg2Cleanup;
   renderer->renderLine = &agg2RenderLine;
@@ -103,6 +154,10 @@ int msPopulateRendererVTableSkia(rendererVTableObj * renderer)
 
   renderer->freeImage = &agg2FreeImage;
   renderer->freeSymbol = &agg2FreeSymbol;*/
+
+  renderer->getRasterBufferHandle = &skiaGetRasterBufferHandle;
+  renderer->getRasterBufferCopy = skiaGetRasterBufferCopy;
+  renderer->initializeRasterBuffer = skiaInitializeRasterBuffer;
 
   renderer->renderLine = &skia2RenderLine;
   renderer->renderPolygon = &skia2RenderPolygon;
