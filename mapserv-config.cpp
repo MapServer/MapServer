72a80273b86d17dfffc7d9cd929f6e0fea076bd3
Jeff McKenna <jmckenna@gatewaygeomatics.com>
2022-09-21 11:43:26 -0300

    correct CONFIG url message

--- mapserv-config.cpp
+++ mapserv-config.cpp
@@ -145,7 +145,7 @@ configObj *msLoadConfig(const char* ms_config_file)
 
   if((msyyin = fopen(ms_config_file, "r")) == NULL) {    
     msDebug("Cannot open configuration file %s.\n", ms_config_file);
-    msSetError(MS_IOERR, "See mapserver.org/config_file.html for more information.", "msLoadConfig()");
+    msSetError(MS_IOERR, "See mapserver.org/mapfile/config.html for more information.", "msLoadConfig()");
     msReleaseLock(TLOCK_PARSER);
     msFree(config);
     return NULL;
