553d4f5da883f00114daa3e5f97053fedd2a10f1
Jeff McKenna <jmckenna@gatewaygeomatics.com>
2023-04-16 08:49:38 -0300

    update CI builds

--- .github/workflows/start.sh
+++ .github/workflows/start.sh
@@ -4,16 +4,17 @@ set -e
 
 apt-get update -y
 
-export BUILD_NAME=PHP_7.4_WITH_PROJ8
+export BUILD_NAME=PHP_8.1_WITH_PROJ8
 #export PYTHON_VERSION=3.6
 export PYTHON_VERSION=system
 
 LANG=en_US.UTF-8
 export LANG
 DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends \
+    add-apt-repository -y ppa:ondrej/php \
     sudo locales tzdata software-properties-common python3-dev python3-pip python3-setuptools git curl \
     apt-transport-https ca-certificates gnupg software-properties-common wget \
-    php-dev php-xml php-mbstring && \
+    php8.1-dev php8.1-xml php8.1-mbstring && \
     echo "$LANG UTF-8" > /etc/locale.gen && \
     dpkg-reconfigure --frontend=noninteractive locales && \
     update-locale LANG=$LANG
