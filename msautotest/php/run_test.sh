aecc8cb5dc310a2489de069eb60847d201e4b4cf
Jeff McKenna <jmckenna@gatewaygeomatics.com>
2023-04-16 10:51:42 -0300

    update CI builds

--- msautotest/php/run_test.sh
+++ msautotest/php/run_test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if test -z $PHP_MAPSCRIPT_SO; then
-   php phpunit-9.5.phar --debug .
+   php phpunit-10.phar --debug .
    exit $?
 else
    php -d "extension=$PHP_MAPSCRIPT_SO" phpunit-9.5.phar --debug .
