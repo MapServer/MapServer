a22ec51adea68ea8cd02c41509839250cc34b192
Jeff McKenna <jmckenna@gatewaygeomatics.com>
2023-04-20 10:11:06 -0300

    upgrade Travis to Focal

--- ci/travis/before_install.sh
+++ ci/travis/before_install.sh
@@ -5,33 +5,94 @@ set -eu
 if ls /etc/apt/sources.list.d/pgdg* 2>/dev/null >/dev/null; then sudo mv /etc/apt/sources.list.d/pgdg* /tmp; fi
 dpkg -l | grep postgresql || /bin/true
 dpkg -l | grep postgis || /bin/true
-sudo apt-get remove --purge postgresql* libpq-dev libpq5 || /bin/true
+sudo apt-get remove --purge postgresql* libpq-dev libpq5 cmake || /bin/true
 
 sudo add-apt-repository -y ppa:ubuntugis/ubuntugis-unstable
 sudo apt-get update
-sudo apt-get install -y --allow-unauthenticated protobuf-c-compiler libprotobuf-c0-dev bison flex libfribidi-dev cmake librsvg2-dev colordiff libpq-dev libpng-dev libjpeg-dev libgif-dev libgeos-dev libfreetype6-dev libfcgi-dev libcurl4-gnutls-dev libcairo2-dev libgdal-dev libproj-dev libxml2-dev libexempi-dev lcov lftp postgis libharfbuzz-dev gdal-bin ccache curl postgresql-server-dev-10 postgresql-10-postgis-3 postgresql-10-postgis-3-scripts swig g++
+sudo apt-get install -y --allow-unauthenticated build-essential protobuf-c-compiler libprotobuf-c-dev bison flex libfribidi-dev librsvg2-dev colordiff libpq-dev libpng-dev libjpeg-dev libgif-dev libgeos-dev libfreetype6-dev libfcgi-dev libcurl4-gnutls-dev libcairo2-dev libgdal-dev libproj-dev libxml2-dev libexempi-dev lcov lftp postgis libharfbuzz-dev gdal-bin proj-bin ccache curl postgresql-server-dev-12 postgresql-12-postgis-3 postgresql-12-postgis-3-scripts g++ ca-certificates
 # following are already installed on Travis CI
 #sudo apt-get install --allow-unauthenticated php-dev python-dev python3-dev
 sudo apt-get install -y --allow-unauthenticated libmono-system-drawing4.0-cil mono-mcs
 sudo apt-get install -y --allow-unauthenticated libperl-dev
 sudo apt-get install -y --allow-unauthenticated openjdk-8-jdk
+sudo apt-get install -y --allow-unauthenticated libonig5
 
+#install recent cmake on GH build action
+if [ -z ${TRAVIS+x} ]; then
+    sudo apt-get install -y --allow-unauthenticated cmake
+    #sudo apt-get install -y --allow-unauthenticated php-xdebug
+    export MSBUILD_ENV="NOT_TRAVIS"
+else
+    # install recent CMake on Travis
+    DEPS_DIR="${TRAVIS_BUILD_DIR}/deps"
+    mkdir ${DEPS_DIR} && cd ${DEPS_DIR}
+    wget --no-check-certificate https://github.com/Kitware/CMake/releases/download/v3.26.3/cmake-3.26.3-linux-x86_64.tar.gz
+    tar -xvf cmake-3.26.3-linux-x86_64.tar.gz > /dev/null
+    mv cmake-3.26.3-linux-x86_64 cmake-install
+    export PATH=${DEPS_DIR}/cmake-install:${DEPS_DIR}/cmake-install/bin:${PATH}
+    cd ${TRAVIS_BUILD_DIR}
+    export MSBUILD_ENV="TRAVIS"
+    # check CMake version installed
+    cmake --version
+fi
+
+#upgrade to recent SWIG
+git clone https://github.com/swig/swig.git swig-git-master
+cd swig-git-master
+wget https://github.com/PhilipHazel/pcre2/releases/download/pcre2-10.39/pcre2-10.39.tar.gz
+./Tools/pcre-build.sh
+./autogen.sh
+./configure --prefix=/usr
+make
+sudo make install
+sudo ldconfig
+cd ../
+#check SWIG version
+swig -version
+
+eval "$(pyenv init --path)"
+eval "$(pyenv init -)"
+
+# list installed and available Python/PHP versions
+pyenv versions
+# echo $(pyenv root)
+# phpenv versions
+
+# set the global Python version
+pyenv global $PYTHON_VERSION
+
+# check we are using the correct versions
+pyenv which pip
+pyenv which python
+
+pip install --upgrade pip
+pip install cryptography==3.4.6 # avoid requiring rust compiler for the cryptography dependency
 pip install cpp-coveralls pyflakes lxml
-sudo pip install -U -r msautotest/requirements.txt
+pip install -r msautotest/requirements.txt
 
 export CC="ccache gcc"
 export CXX="ccache g++"
 
-sudo sed -i  's/md5/trust/' /etc/postgresql/10/main/pg_hba.conf
-sudo sed -i  's/peer/trust/' /etc/postgresql/10/main/pg_hba.conf
-sudo service postgresql restart 10
+sudo sed -i  's/md5/trust/' /etc/postgresql/12/main/pg_hba.conf
+sudo sed -i  's/peer/trust/' /etc/postgresql/12/main/pg_hba.conf
+sudo service postgresql restart 12
 
 cd msautotest
+#upgrade to recent PHPUnit
+cd php && curl -LO https://phar.phpunit.de/phpunit-10.phar
+cd ..
 python -m pyflakes .
 ./create_postgis_test_data.sh
 
-# py3
-python -m http.server &> /dev/null &
+# copy custom projection to the PROJ_DATA folder
+sudo cp ./wxs/data/epsg2 /usr/share/proj/
+
+if [ $PYTHON_VERSION = "2.7" ]; then
+    python -m SimpleHTTPServer &> /dev/null &
+else
+    # py3
+    python -m http.server &> /dev/null &
+fi
 
 cd ..
 touch maplexer.l
--- ci/travis/script.sh
+++ ci/travis/script.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 set -eu
 
-#if [ "$BUILD_NAME" = "PHP_7.3_WITH_ASAN" ]; then
+eval "$(pyenv init --path)"
+eval "$(pyenv init -)"
+
+#if [ "$BUILD_NAME" = "PHP_7.2_WITH_ASAN" ]; then
 #    export CC="ccache clang"
 #    export CXX="ccache clang++"
 #else
@@ -9,31 +12,40 @@ set -eu
     export CXX="ccache g++"
 #fi
 
-curl http://download.osgeo.org/proj/proj-6.1.1.tar.gz > proj-6.1.1.tar.gz
-tar xzf proj-6.1.1.tar.gz
-mv proj-6.1.1 proj
-(cd proj/data && curl http://download.osgeo.org/proj/proj-datumgrid-1.8.tar.gz > proj-datumgrid-1.8.tar.gz && tar xvzf proj-datumgrid-1.8.tar.gz)
-(cd proj; CFLAGS='-O2 -DPROJ_RENAME_SYMBOLS' CXXFLAGS='-O2 -DPROJ_RENAME_SYMBOLS' ./configure --disable-static --prefix=/usr/local && CCACHE_CPP2=yes make -j2 && sudo make -j3 install)
-sudo rm -f /usr/include/proj_api.h
+#make sure to use recent CMake, and the pyenv Python instance
+export PYTHONPREFIX="$(dirname $(realpath $(pyenv which python)))/.."
+if [ -z ${TRAVIS+x} ]; then
+    #not travis
+    export PATH=${PYTHONPREFIX}/bin:${PATH}
+else
+    #travis
+    export PATH=${TRAVIS_BUILD_DIR}/deps/cmake-install:${TRAVIS_BUILD_DIR}/deps/cmake-install/bin:${PYTHONPREFIX}/bin:${PATH}  
+fi
+cmake --version
 
-if [ "$BUILD_NAME" = "PHP_7.3_WITH_ASAN" ]; then
-    # Force use of PROJ 4 API
-    sudo rm /usr/local/include/proj.h
+# check we are using the correct versions
+pyenv which pip
+pyenv which python
+#pyenv which python-config
+# check for phpunit & xdebug
+php msautotest/php/phpunit-10.phar --version
+php -v
+
+if [ "$BUILD_NAME" = "PHP_8.1_WITH_ASAN" ]; then
     # -DNDEBUG to avoid issues with cairo cleanup
-    make cmakebuild MFLAGS="-j2" CMAKE_C_FLAGS="-g -fsanitize=address -DNDEBUG -DPROJ_RENAME_SYMBOLS -DACCEPT_USE_OF_DEPRECATED_PROJ_API_H" CMAKE_CXX_FLAGS="-g -fsanitize=address -DNDEBUG -DPROJ_RENAME_SYMBOLS -DACCEPT_USE_OF_DEPRECATED_PROJ_API_H" EXTRA_CMAKEFLAGS="-DCMAKE_BUILD_TYPE=None -DCMAKE_EXE_LINKER_FLAGS=-fsanitize=address -DPROJ_INCLUDE_DIR=/usr/local/include -DPROJ_LIBRARY=/usr/local/lib/libproj.so.15"
+    make cmakebuild MFLAGS="-j2" CMAKE_C_FLAGS="-g -fsanitize=address -DNDEBUG" CMAKE_CXX_FLAGS="-g -fsanitize=address -DNDEBUG" EXTRA_CMAKEFLAGS="-DCMAKE_BUILD_TYPE=None -DCMAKE_EXE_LINKER_FLAGS=-fsanitize=address"
     export AUTOTEST_OPTS="--strict --run_under_asan"
-    # Only run tests that only involve mapserv/shp2img binaries. mspython, etc would require LD_PREOLOAD'ing the asan shared object
+    # Only run tests that only involve mapserv/map2img binaries. mspython, etc would require LD_PREOLOAD'ing the asan shared object
     make -j4 asan_compatible_tests
-elif [ "$BUILD_NAME" = "PHP_7.4_WITH_PROJ6" ]; then
-    # Avoid any use of PROJ 4 API
-    sudo rm -f /usr/include/proj_api.h
-    make cmakebuild MFLAGS="-j2" CMAKE_C_FLAGS="-O2 -DPROJ_RENAME_SYMBOLS" CMAKE_CXX_FLAGS="-O2 -DPROJ_RENAME_SYMBOLS" EXTRA_CMAKEFLAGS="-DPROJ_INCLUDE_DIR=/usr/local/include -DPROJ_LIBRARY=/usr/local/lib/libproj.so.15"
+elif [ "$BUILD_NAME" = "PHP_8.1_WITH_PROJ8" ]; then
+    #runs through GitHub action
+    make cmakebuild MFLAGS="-j2" CMAKE_C_FLAGS="-O2" CMAKE_CXX_FLAGS="-O2" LIBMAPSERVER_EXTRA_FLAGS="-Wall -Werror -Wextra"
     make mspython-wheel
+    make phpng-build    
     make -j4 test
 else
-    # Force use of PROJ 4 API
-    sudo rm /usr/local/include/proj.h
-    make cmakebuild MFLAGS="-j2" CMAKE_C_FLAGS="-DPROJ_RENAME_SYMBOLS -DACCEPT_USE_OF_DEPRECATED_PROJ_API_H" CMAKE_CXX_FLAGS="-DPROJ_RENAME_SYMBOLS -DACCEPT_USE_OF_DEPRECATED_PROJ_API_H" EXTRA_CMAKEFLAGS="-DPROJ_INCLUDE_DIR=/usr/local/include -DPROJ_LIBRARY=/usr/local/lib/libproj.so.15"
+    make cmakebuild MFLAGS="-j2" CMAKE_C_FLAGS="-O2" CMAKE_CXX_FLAGS="-O2" LIBMAPSERVER_EXTRA_FLAGS="-Wall -Werror -Wextra"
     make mspython-wheel
+    make phpng-build    
     make -j4 test
 fi
