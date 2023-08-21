993836bf491717c18b52aa623e8e2392ae8d28c0
Jeff McKenna <jmckenna@gatewaygeomatics.com>
2023-08-21 08:36:54 -0300

    fix csharp test path

--- src/mapscript/csharp/run_test.sh
+++ src/mapscript/csharp/run_test.sh
@@ -1,12 +1,12 @@
 #!/bin/sh -ex
 cd ${CSHARP_MAPSCRIPT_SO}
 echo .: Testing shpdump.exe :.
-mono shpdump.exe ../../../tests/point.shp
+mono shpdump.exe ../../../../tests/point.shp
 echo .: Testing drawmap.exe :.
-mono drawmap.exe ../../../tests/test.map ./map.png
+mono drawmap.exe ../../../../tests/test.map ./map.png
 echo .: Testing shapeinfo.exe :.
-mono shapeinfo.exe ../../../tests/point.shp
+mono shapeinfo.exe ../../../../tests/point.shp
 echo .: Testing getbytes.exe :.
-mono getbytes.exe ../../../tests/test.map test_csharp2.png
+mono getbytes.exe ../../../../tests/test.map test_csharp2.png
 echo .: Testing RFC24.exe :.
-mono RFC24.exe ../../../tests/test.map
+mono RFC24.exe ../../../../tests/test.map
