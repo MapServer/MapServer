b6d1b9c0ec45cc77fa2c5e3ed23d34454bf3de76
Even Rouault <even.rouault@spatialys.com>
2024-03-10 01:44:09 +0100

    msRedactCredentials(): fix various issues (fixes #7046)
    * Avoid redacting too much stuff (including newline)
    * Improve detection of tokens

--- src/maperror.c
+++ src/maperror.c
@@ -340,12 +340,34 @@ char *msGetErrorString(const char *delimiter) {
   return (errstr);
 }
 
-void msRedactString(char *str, const char *keyword, const char delimeter) {
+static void msRedactString(char *str, const char *keyword) {
 
   char *password = strstr(str, keyword);
   if (password != NULL) {
+    const char chOptionDelimeter = password - str > 0 ? password[-1] : 0;
     char *ptr = password + strlen(keyword);
-    while (*ptr != '\0' && *ptr != delimeter) {
+    char chStringSep = *ptr;
+    if (chStringSep == '\'' || chStringSep == '"') {
+      ++ptr;
+    } else {
+      chStringSep = '\0';
+    }
+    /* Replace all characters from after equal sign to end of line, end of
+     * string, or end of quoted string.
+     */
+    while (*ptr != '\0' && *ptr != '\r' && *ptr != '\n') {
+      if (chStringSep == '\0') {
+        if (*ptr == chOptionDelimeter)
+          break;
+      } else {
+        if (*ptr == chStringSep) {
+          break;
+        }
+        if (*ptr == '\\' && ptr[1] == chStringSep) {
+          *ptr = '*';
+          ptr++;
+        }
+      }
       *ptr = '*';
       ptr++;
     }
@@ -354,12 +376,10 @@ void msRedactString(char *str, const char *keyword, const char delimeter) {
 
 void msRedactCredentials(char *str) {
 
-  // postgres formats
-  msRedactString(str, "password=", ' ');
-  // mssql use semi-colons as delimeters for parameters
-  msRedactString(str, "password=", ';');
+  // postgres or mssql formats
+  msRedactString(str, "password=");
   // ODBC connections can use pwd rather than password
-  msRedactString(str, "pwd=", ';');
+  msRedactString(str, "pwd=");
 }
 
 void msSetError(int code, const char *message_fmt, const char *routine, ...) {
