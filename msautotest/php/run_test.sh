817f1e7530496c08c8011a938a5e33a182da4042
Jeff McKenna <jmckenna@gatewaygeomatics.com>
2024-07-07 16:59:38 -0300

    upgrade to PHPUnit 11

--- msautotest/php/run_test.sh
+++ msautotest/php/run_test.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+PHPVersionMinor=$(php --version | head -n 1 | cut -d " " -f 2 | cut -c 1,3)
+if [ ${PHPVersionMinor} -gt 81 ]; then
+    PHPUnitVersion=11
+else
+    PHPUnitVersion=10
+fi
+
 if test -z $PHP_MAPSCRIPT_SO; then
    php phpunit-$PHPUnitVersion.phar .
    exit $?
