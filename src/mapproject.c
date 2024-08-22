0ffa3a7c1d76d367b3c399f9413b00b844cafc67
Roel Arents <roel.arents@kadaster.nl>
2024-08-22 12:46:08 +0200

    fix proj epsgaxis= stripping to also work for +epsgaxis=
    related to #7121

--- src/mapproject.c
+++ src/mapproject.c
@@ -762,7 +762,7 @@ int msProcessProjection(projectionObj *p) {
       // PROJ doesn't like extraneous parameters that it doesn't recognize
       // when initializing a CRS from a +init=epsg:xxxx string
       // Cf https://github.com/OSGeo/PROJ/issues/4203
-      if (strncmp(p->args[i], "epsgaxis=", strlen("epsgaxis=")) != 0) {
+      if (strstr(p->args[i], "epsgaxis=") == NULL) {
         args[numargs] = p->args[i];
         ++numargs;
       }
