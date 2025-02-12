5e2f124ff23feadd4dcfd13be61235fd782178ec
sethg <sethg@geographika.co.uk>
2025-02-12 21:49:52 +0100

    Update to jammy

--- ci/ubuntu/setup.sh
+++ ci/ubuntu/setup.sh
@@ -22,7 +22,7 @@ sudo apt-get install -y --allow-unauthenticated libonig5
 
 #install recent cmake
 wget -O - https://apt.kitware.com/keys/kitware-archive-latest.asc 2>/dev/null | gpg --dearmor - | sudo tee /etc/apt/trusted.gpg.d/kitware.gpg >/dev/null
-sudo apt-add-repository 'deb https://apt.kitware.com/ubuntu/ focal main'
+sudo apt-add-repository 'deb https://apt.kitware.com/ubuntu/ jammy main'
 sudo apt-get install -y --allow-unauthenticated cmake
 
 echo "cmake version"
