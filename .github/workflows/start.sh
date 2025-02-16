0e3cdc747710c6a6efb093b57a647d489f345afb
sethg <sethg@geographika.co.uk>
2025-02-16 12:53:11 +0100

    Fix PHP warnings

--- .github/workflows/start.sh
+++ .github/workflows/start.sh
@@ -5,19 +5,23 @@ set -e
 apt-get update -y
 
 export PYTHON_VERSION=system
+export LANG=en_US.UTF-8
+DEBIAN_FRONTEND=noninteractive
 
-LANG=en_US.UTF-8
-export LANG
-DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends \
+apt-get install -y --no-install-recommends \
     sudo locales tzdata software-properties-common python3-dev python3-venv python3-pip python3-setuptools git curl \
     apt-transport-https ca-certificates gnupg software-properties-common wget
-#install PHP 8.1
-DEBIAN_FRONTEND=noninteractive add-apt-repository ppa:ondrej/php -y
-DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends \
-    sudo php8.1-dev php8.1-xml php8.1-mbstring && \
-    echo "$LANG UTF-8" > /etc/locale.gen && \
-    dpkg-reconfigure --frontend=noninteractive locales && \
-    update-locale LANG=$LANG
+# install PHP 8.1
+# https://github.com/oerdnj/deb.sury.org/issues/56
+add-apt-repository ppa:ondrej/php -y
+LC_ALL=C.UTF-8 add-apt-repository ppa:ondrej/php -y
+
+apt-get install -y --no-install-recommends \
+    sudo php8.1-dev php8.1-xml php8.1-mbstring
+
+echo "$LANG UTF-8" > /etc/locale.gen && \
+dpkg-reconfigure --frontend=noninteractive locales && \
+update-locale LANG=$LANG
 
 USER=root
 export USER
