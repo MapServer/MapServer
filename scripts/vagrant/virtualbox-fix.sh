7d4cc24dd007ce26759a1e4a2b063508f8cfe62b
sethg <sethg@geographika.co.uk>
2025-02-20 22:54:07 +0100

    Remove any previous symbolic link

--- scripts/vagrant/virtualbox-fix.sh
+++ scripts/vagrant/virtualbox-fix.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
-if [ ! -e "/usr/lib/VBoxGuestAdditions" ]; then
-    ln -s /opt/VBoxGuestAdditions-4.3.10/lib/VBoxGuestAdditions /usr/lib/VBoxGuestAdditions
+if [ -e "/usr/lib/VBoxGuestAdditions" ] || [ -L "/usr/lib/VBoxGuestAdditions" ]; then
+    rm -rf /usr/lib/VBoxGuestAdditions
 fi
+
+ln -s /opt/VBoxGuestAdditions-4.3.10/lib/VBoxGuestAdditions /usr/lib/VBoxGuestAdditions
