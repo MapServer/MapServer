c99ec80b3c9e709126061fe2a18e56292ce9a4a1
sethg <sethg@geographika.co.uk>
2025-02-21 00:52:49 +0100

    Update to ubuntugis-stable

--- ci/ubuntu/setup.sh
+++ ci/ubuntu/setup.sh
@@ -12,7 +12,7 @@ curl -fsSL https://apt.kitware.com/keys/kitware-archive-latest.asc | sudo tee /e
 echo 'deb [signed-by=/etc/apt/keyrings/kitware-archive-latest.asc] https://apt.kitware.com/ubuntu focal main' | sudo tee /etc/apt/sources.list.d/kitware.list > /dev/null
 
 # Add required repositories
-sudo add-apt-repository -y ppa:ubuntugis/ubuntugis-unstable
+sudo add-apt-repository -y ppa:ubuntugis/ppa # stable
 sudo apt-add-repository 'deb https://apt.kitware.com/ubuntu/ focal main'
 
 sudo apt-get update
