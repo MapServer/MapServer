MapServer/MapServer

commit 64a706f6f598e8bc912a2c0713cc4228e8805f44

Move PHP MapScript builds to Docker and simplify Python MapScript builds

msautotest/pymod/mstestlib.py
@@ -596,15 +596,6 @@ def _run(map, out_file, command, extra_args):
     else:
         command = command.replace("[RENDERER]", "")
 
-    # Used in msautotest/wxs/wcs_netcdf_input_output.map as for some unknown
-    # reason comparison fails on Travis-CI but not in the github action tests
-    # when using Docker. Likely due to a pre-installed library on the test runners
-    ignore_comparison_result = False
-    if "[IGNORE_COMPARISON_RESULT_ON_CI]" in command:
-        command = command.replace("[IGNORE_COMPARISON_RESULT_ON_CI]", "")
-        if "IGNORE_COMPARISON_RESULT_ON_CI" in os.environ:
-            ignore_comparison_result = True
-
     os.environ["MS_PDF_CREATION_DATE"] = "dummy date"
 
     # support for environment variable of type [ENV foo=bar]
@@ -731,7 +722,7 @@ def _run(map, out_file, command, extra_args):
 
     cmp = compare_result(out_file)
 
-    if cmp != "match" and ignore_comparison_result:
+    if cmp != "match":
         if not keep_pass:
             os.remove("result/" + out_file)
         if not quiet:

.github/workflows/build-mapscript-php.yml
@@ -21,35 +21,18 @@ permissions:
 
 jobs:
   build:
-    runs-on: ubuntu-20.04
+    runs-on: ubuntu-24.04
     strategy:
       matrix:
         php-version: [8.4, 8.3]
-    env:
-      IGNORE_COMPARISON_RESULT_ON_CI: 'true'
 
     steps:
       - name: Checkout code
         uses: actions/checkout@v4
 
-      - name: Set up Python
-        uses: actions/setup-python@v5
-        with:
-          python-version: 3.8
-
-      - name: Setup PHP
+      - name: Set up PHP and Install Dependencies
         run: |
-          # for pre-installed software see
-          # https://github.com/actions/runner-images/blob/main/images/ubuntu/Ubuntu2004-Readme.md
-          # both swig and php are included, so removed these
-          sudo apt-get -qq remove "php*-cli" "php*-dev"
-          sudo apt-get -qq remove "swig*"
-          sudo add-apt-repository ppa:ondrej/php -y
-          sudo apt-get -qq update
-          sudo apt-get -qq install php${{ matrix.php-version }}-cli php${{ matrix.php-version }}-dev php${{ matrix.php-version }}-mbstring php${{ matrix.php-version }}-xml php${{ matrix.php-version }}-pcov php${{ matrix.php-version }}-xdebug
-
-      - name: Install dependencies
-        run: ./ci/ubuntu/setup.sh
-
-      - name: Build
-        run: ./ci/ubuntu/build.sh
+            docker run \
+            -e WORK_DIR="$PWD" \
+            -e PHP_VERSION="${{ matrix.php-version }}" \
+            -v $PWD:$PWD ubuntu:20.04 $PWD/scripts/build-mapscript-php.sh

Makefile
@@ -10,12 +10,23 @@ YACC=yacc
 CMAKEFLAGS_MAPSCRIPT_PYTHON=-DWITH_CLIENT_WMS=1 \
 			  -DWITH_CLIENT_WFS=1 -DWITH_KML=1 -DWITH_SOS=1 \
 			  -DWITH_PYTHON=1 -DWITH_THREAD_SAFETY=1 -DWITH_FRIBIDI=1 -DWITH_FCGI=1 -DWITH_EXEMPI=1 \
-			  -DCMAKE_BUILD_TYPE=Release -DWITH_RSVG=1 -DWITH_CURL=1 -DWITH_HARFBUZZ=1 ${EXTRA_CMAKEFLAGS} -DLIBMAPSERVER_EXTRA_FLAGS="${LIBMAPSERVER_EXTRA_FLAGS}" -DCMAKE_INSTALL_PREFIX=/tmp/install-mapserver
+			  -DCMAKE_BUILD_TYPE=Release -DWITH_RSVG=1 -DWITH_CURL=1 -DWITH_HARFBUZZ=1 \
+			  ${EXTRA_CMAKEFLAGS} -DLIBMAPSERVER_EXTRA_FLAGS="${LIBMAPSERVER_EXTRA_FLAGS}" \
+			  -DCMAKE_INSTALL_PREFIX=/tmp/install-mapserver
+
+CMAKEFLAGS_MAPSCRIPT_PHP=-DWITH_CLIENT_WMS=1 \
+			  -DWITH_CLIENT_WFS=1 -DWITH_KML=1 -DWITH_SOS=1 -DWITH_PHPNG=1 \
+			  -DWITH_PYTHON=0 -DWITH_THREAD_SAFETY=1 -DWITH_FRIBIDI=1 -DWITH_FCGI=1 -DWITH_EXEMPI=1 \
+			  -DCMAKE_BUILD_TYPE=Release -DWITH_RSVG=1 -DWITH_CURL=1 -DWITH_HARFBUZZ=1 \
+			  ${EXTRA_CMAKEFLAGS} -DLIBMAPSERVER_EXTRA_FLAGS="${LIBMAPSERVER_EXTRA_FLAGS}" \
+			  -DCMAKE_INSTALL_PREFIX=/tmp/install-mapserver
 
 CMAKEFLAGS_NOCOVERAGE=-DWITH_CLIENT_WMS=1 \
 			  -DWITH_CLIENT_WFS=1 -DWITH_KML=1 -DWITH_SOS=1 -DWITH_CSHARP=1 -DWITH_PHPNG=1 -DWITH_PERL=1 \
 			  -DWITH_PYTHON=1 -DWITH_JAVA=1 -DWITH_THREAD_SAFETY=1 -DWITH_FRIBIDI=1 -DWITH_FCGI=1 -DWITH_EXEMPI=1 \
-			  -DCMAKE_BUILD_TYPE=Release -DWITH_RSVG=1 -DWITH_CURL=1 -DWITH_HARFBUZZ=1 -DWITH_MSSQL2008=ON ${EXTRA_CMAKEFLAGS} -DLIBMAPSERVER_EXTRA_FLAGS="${LIBMAPSERVER_EXTRA_FLAGS}" -DCMAKE_INSTALL_PREFIX=/tmp/install-mapserver
+			  -DCMAKE_BUILD_TYPE=Release -DWITH_RSVG=1 -DWITH_CURL=1 -DWITH_HARFBUZZ=1 -DWITH_MSSQL2008=ON \
+			  ${EXTRA_CMAKEFLAGS} -DLIBMAPSERVER_EXTRA_FLAGS="${LIBMAPSERVER_EXTRA_FLAGS}" \
+			  -DCMAKE_INSTALL_PREFIX=/tmp/install-mapserver
 
 CMAKEFLAGS=-DCMAKE_C_FLAGS="--coverage ${CMAKE_C_FLAGS}" -DCMAKE_CXX_FLAGS="--coverage ${CMAKE_CXX_FLAGS}" \
 			  -DCMAKE_SHARED_LINKER_FLAGS="-lgcov" ${CMAKEFLAGS_NOCOVERAGE}
@@ -33,7 +44,11 @@ cmakebuild_nocoverage: lexer parser
 cmakebuild_mapscript_python: lexer parser
 	if test ! -s build/Makefile; then  mkdir -p build ; cd build ; cmake .. $(CMAKEFLAGS_MAPSCRIPT_PYTHON); fi
 	cd build && $(MAKE) $(MFLAGS)
-    
+
+cmakebuild_mapscript_php: lexer parser
+	if test ! -s build/Makefile; then  mkdir -p build ; cd build ; cmake .. $(CMAKEFLAGS_MAPSCRIPT_PHP); fi
+	cd build && $(MAKE) $(MFLAGS)
+
 warning:
 	$(error "This Makefile is used to run the \"test\" target")
 
@@ -69,9 +84,6 @@ mspython-wheel:
 
 php-testcase:
 	test -f "$(PHP_MAPSCRIPT)" && (export PHP_MAPSCRIPT_SO="../../$(PHP_MAPSCRIPT)" && cd msautotest/php && ./run_test.sh)
-
-phpng-build:
-	cd build && cmake --build . --config Release
 
 java-testcase:
 	test -d "$(JAVA_MAPSCRIPT_PATH)" && (export JAVA_MAPSCRIPT_SO="../../../$(JAVA_MAPSCRIPT_PATH)" && cd src/mapscript/java && ./run_test.sh)

ci/ubuntu/build.sh
@@ -4,14 +4,6 @@ set -eu
 export CC="ccache gcc"
 export CXX="ccache g++"
 
-if [ "${MAPSCRIPT_PYTHON_ONLY:-}" = "true" ]; then
-    # only build MapServer with the Python MapScript and not PHP, Perl etc.
-    make cmakebuild_mapscript_python MFLAGS="-j$(nproc)" CMAKE_C_FLAGS="-O2" CMAKE_CXX_FLAGS="-O2" LIBMAPSERVER_EXTRA_FLAGS="-Wall -Werror -Wextra"
-    # build the wheel and run the Python MapScript test suite
-    make mspython-wheel
-    exit
-fi
-
 # Turn CMake warnings as errors
 EXTRA_CMAKEFLAGS="-Werror=dev"
 
@@ -30,7 +22,7 @@ else
         LIBMAPSERVER_EXTRA_FLAGS="-Wall -Werror -Wextra" \
         EXTRA_CMAKEFLAGS="${EXTRA_CMAKEFLAGS} -DWITH_PCRE2=ON"
     make mspython-wheel
-    make phpng-build
+    make cmakebuild_mapscript_php
 fi
 
 # msautotests setup

ci/ubuntu/setup.sh
@@ -34,9 +34,15 @@ echo "cmake version"
 cmake --version
 
 # upgrade to recent SWIG
-git clone https://github.com/swig/swig.git swig-git-master
+if [ ! -d "swig-git-master" ]; then
+    echo "Cloning SWIG repository..."
+    git clone https://github.com/swig/swig.git swig-git-master
+else
+    echo "swig-git-master already exists, skipping clone."
+fi
+
 cd swig-git-master
-wget https://github.com/PCRE2Project/pcre2/releases/download/pcre2-10.44/pcre2-10.44.tar.gz
+wget -nc https://github.com/PCRE2Project/pcre2/releases/download/pcre2-10.44/pcre2-10.44.tar.gz
 ./Tools/pcre-build.sh
 ./autogen.sh
 ./configure --prefix=/usr
@@ -57,8 +63,5 @@ which python
 which pip
 
 # install Python dependencies (required for msautotests)
-pip install --upgrade pip
-# pip install cryptography==3.4.6 # avoid requiring rust compiler for the cryptography dependency
-pip install pyflakes
+export PIP_BREAK_SYSTEM_PACKAGES=1
 pip install -r msautotest/requirements.txt
-

msautotest/wxs/wcs_netcdf_3d_input_output.map
@@ -3,7 +3,7 @@
 #
 # REQUIRES: INPUT=GDAL OUTPUT=PNG SUPPORTS=WCS
 
-# RUN_PARMS: wcs_netcdf_3d_input_output.nc [MAPSERV] QUERY_STRING="map=[MAPFILE]&SERVICE=WCS&VERSION=2.0.1&REQUEST=GetCoverage&COVERAGEID=multi&FORMAT=application/x-netCDF&RANGESUBSET=2,3" > [RESULT_DEMIME][IGNORE_COMPARISON_RESULT_ON_CI]
+# RUN_PARMS: wcs_netcdf_3d_input_output.nc [MAPSERV] QUERY_STRING="map=[MAPFILE]&SERVICE=WCS&VERSION=2.0.1&REQUEST=GetCoverage&COVERAGEID=multi&FORMAT=application/x-netCDF&RANGESUBSET=2,3" > [RESULT_DEMIME]
 
 MAP
 

msautotest/wxs/wcs_netcdf_3d_output.map
@@ -3,7 +3,7 @@
 #
 # REQUIRES: INPUT=GDAL OUTPUT=PNG SUPPORTS=WCS
 
-# RUN_PARMS: wcs_netcdf_3d_output.nc [MAPSERV] QUERY_STRING="map=[MAPFILE]&SERVICE=WCS&VERSION=2.0.1&REQUEST=GetCoverage&COVERAGEID=multi&FORMAT=application/x-netCDF&RANGESUBSET=1,5,9" > [RESULT_DEMIME][IGNORE_COMPARISON_RESULT_ON_CI]
+# RUN_PARMS: wcs_netcdf_3d_output.nc [MAPSERV] QUERY_STRING="map=[MAPFILE]&SERVICE=WCS&VERSION=2.0.1&REQUEST=GetCoverage&COVERAGEID=multi&FORMAT=application/x-netCDF&RANGESUBSET=1,5,9" > [RESULT_DEMIME]
 
 MAP
 

msautotest/wxs/wcs_netcdf_input_output.map
@@ -3,9 +3,9 @@
 #
 # REQUIRES: INPUT=GDAL OUTPUT=PNG SUPPORTS=WCS
 
-# RUN_PARMS: wcs_netcdf_input_output_wcs10_get_coverage.nc [MAPSERV] QUERY_STRING="map=[MAPFILE]&SERVICE=WCS&REQUEST=GetCoverage&VERSION=1.0.0&COVERAGE=precipitation&CRS=EPSG:4326&BBOX=-80,-20,-79.5,-19.5&RESX=0.25&RESY=0.25&FORMAT=netCDF" > [RESULT_DEMIME][IGNORE_COMPARISON_RESULT_ON_CI]
+# RUN_PARMS: wcs_netcdf_input_output_wcs10_get_coverage.nc [MAPSERV] QUERY_STRING="map=[MAPFILE]&SERVICE=WCS&REQUEST=GetCoverage&VERSION=1.0.0&COVERAGE=precipitation&CRS=EPSG:4326&BBOX=-80,-20,-79.5,-19.5&RESX=0.25&RESY=0.25&FORMAT=netCDF" > [RESULT_DEMIME]
 
-# RUN_PARMS: wcs_netcdf_input_output_wcs20_get_coverage.nc [MAPSERV] QUERY_STRING="map=[MAPFILE]&SERVICE=WCS&REQUEST=GetCoverage&VERSION=2.0.1&COVERAGEID=precipitation&FORMAT=application/x-netcdf&SUBSET=long(-80,-79.5)&SUBSETTINGCRS=http://www.opengis.net/def/crs/EPSG/0/4326" > [RESULT_DEMIME][IGNORE_COMPARISON_RESULT_ON_CI]
+# RUN_PARMS: wcs_netcdf_input_output_wcs20_get_coverage.nc [MAPSERV] QUERY_STRING="map=[MAPFILE]&SERVICE=WCS&REQUEST=GetCoverage&VERSION=2.0.1&COVERAGEID=precipitation&FORMAT=application/x-netcdf&SUBSET=long(-80,-79.5)&SUBSETTINGCRS=http://www.opengis.net/def/crs/EPSG/0/4326" > [RESULT_DEMIME]
 
 MAP
 

commit fea7167245f60808bbcc7bd6def6121977e2cf00

msautotest: Fix interpretation of test output comparison in mstestlib
This fixes a regression from 64a706f6f598e8bc912a2c0713cc4228e8805f44 were a
feature to skip some differences in test output was incompletely removed.
Before this fix it will skip all non-matching results and report them as success.

msautotest/pymod/mstestlib.py
@@ -722,16 +722,6 @@ def _run(map, out_file, command, extra_args):
 
     cmp = compare_result(out_file)
 
-    if cmp != "match":
-        if not keep_pass:
-            os.remove("result/" + out_file)
-        if not quiet:
-            print("     results do not match, but ignored.")
-        else:
-            sys.stdout.write(".")
-            sys.stdout.flush()
-        return True, None
-
     if cmp == "match":
         if not keep_pass:
             os.remove("result/" + out_file)