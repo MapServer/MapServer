9a126009e02925b6ef635b27aa9ce45ef5d20573
Jeff McKenna <jmckenna@gatewaygeomatics.com>
2025-03-13 11:50:50 -0300

    upgrade PHPUnit to version 12

--- msautotest/php/run_test.sh
+++ msautotest/php/run_test.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
+cd "$WORK_DIR"
+
+# get PHPUnit
+cd msautotest
+echo "PHP version"
+php -v
 PHPVersionMinor=$(php --version | head -n 1 | cut -d " " -f 2 | cut -c 1,3)
 if [ ${PHPVersionMinor} -ge 83 ]; then
-    PHPUnitVersion=12
+    cd php && curl -LO https://phar.phpunit.de/phpunit-12.phar
+    echo "PHPUnit version"
+    php phpunit-12.phar --version
 else
-    PHPUnitVersion=11
+    cd php && curl -LO https://phar.phpunit.de/phpunit-10.phar
+    echo "PHPUnit version"
+    php phpunit-10.phar --version
 fi
 
 if test -z $PHP_MAPSCRIPT_SO; then
--- scripts/build-mapscript-php.sh
+++ scripts/build-mapscript-php.sh
@@ -40,23 +40,6 @@ export CXX="ccache g++"
 
 make cmakebuild_mapscript_php
 
-cd "$WORK_DIR"
-
-# get PHPUnit
-cd msautotest
-echo "PHP version"
-php -v
-PHPVersionMinor=$(php --version | head -n 1 | cut -d " " -f 2 | cut -c 1,3)
-if [ ${PHPVersionMinor} -ge 83 ]; then
-    cd php && curl -LO https://phar.phpunit.de/phpunit-12.phar
-    echo "PHPUnit version"
-    php phpunit-12.phar --version
-else
-    cd php && curl -LO https://phar.phpunit.de/phpunit-11.phar
-    echo "PHPUnit version"
-    php phpunit-11.phar --version
-fi
-
 echo "PHP includes"
 
 php-config --includes
