530f2bfdd99f27541ea9d08dc043d7e3c1e8f38c
Jeff McKenna <jmckenna@gatewaygeomatics.com>
2025-03-17 08:42:15 -0300

    CI: upgrade PHPUnit to version 12 (#7254)

--- msautotest/php/run_test.sh
+++ msautotest/php/run_test.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 
+cd "$WORK_DIR"
+
+# get PHPUnit
+cd msautotest
+echo "PHP version"
+php -v
 PHPVersionMinor=$(php --version | head -n 1 | cut -d " " -f 2 | cut -c 1,3)
-if [ ${PHPVersionMinor} -gt 81 ]; then
-    PHPUnitVersion=11
+if [ ${PHPVersionMinor} -ge 83 ]; then
+    PHPUnitVersion=12
 else
     PHPUnitVersion=10
 fi
+cd php && curl -LO https://phar.phpunit.de/phpunit-$PHPUnitVersion.phar
+echo "PHPUnit version"
+php phpunit-$PHPUnitVersion.phar --version
 
 if test -z $PHP_MAPSCRIPT_SO; then
    php phpunit-$PHPUnitVersion.phar .
