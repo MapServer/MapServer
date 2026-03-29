5d7bbfae3509f55f7d5acc8ebd44bd7ac3b8f455
Jeff McKenna <jmckenna@gatewaygeomatics.com>
2026-03-29 16:59:59 -0300

    CI: fix coverity-scan script

--- scripts/coverity-scan.sh
+++ scripts/coverity-scan.sh
@@ -9,7 +9,7 @@ fi
 
 # first install necessary packages
 apt-get update -y
-apt-get install -y software-properties-common
+apt-get install software-properties-common -y
 
 # Install libraries
 add-apt-repository -y ppa:ubuntugis/ubuntugis-unstable
